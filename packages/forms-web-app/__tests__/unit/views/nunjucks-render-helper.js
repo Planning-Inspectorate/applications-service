8f7da99ba30aac1ffe1b55c8524084e68eaf6d12
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-10-21 12:19:50 +0100

    APS-443 initial unit test framework

---
+++ packages/forms-web-app/__tests__/unit/views/nunjucks-render-helper.js
@@ -0,0 +1,26 @@
+const nunjucks = require('nunjucks');
+const path = require('path');
+const dateFilter = require('nunjucks-date-filter');
+const filterByKey = require('../../../src/lib/filter-by-key');
+const addKeyValuePair = require('../../../src/lib/add-key-value-pair');
+const render = require('../../../src/lib/render-template-filter');
+
+const viewPaths = [
+  path.join(__dirname, '../../..', 'node_modules', 'govuk-frontend'),
+  path.join(__dirname, '../../..', 'node_modules', '@ministryofjustice', 'frontend'),
+  path.join(__dirname, '../../..', 'node_modules', '@pins', 'common', 'src', 'frontend'),
+  path.join(__dirname, '../../..', 'src', 'views'),
+];
+
+const nunjucksConfig = {
+  noCache: true,
+};
+
+const env = nunjucks.configure(viewPaths, nunjucksConfig);
+
+env.addFilter('filterByKey', filterByKey);
+env.addFilter('addKeyValuePair', addKeyValuePair);
+env.addFilter('date', dateFilter);
+env.addFilter('render', render);
+
+module.exports = env;
---
+++ packages/forms-web-app/src/lib/empty-application.js
@@ -0,0 +1,4 @@
+module.exports.APPLICATION_DOCUMENT = {
+  empty: {
+  },
+};
