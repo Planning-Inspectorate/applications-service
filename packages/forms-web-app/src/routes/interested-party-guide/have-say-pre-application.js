3a79bf51052fe263d9d93604fb27f5275f2ce22a
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-10-25 13:53:02 +0100

    feat: add new page for registration start

--- packages/forms-web-app/src/routes/interested-party-guide/have-say-pre-application.js
+++ packages/forms-web-app/src/routes/interested-party-guide/have-say-pre-application.js
@@ -1,9 +1,22 @@
-const { VIEW } = require('../../lib/views');
 
 const express = require('express');
+
+const haveSayPreApplicationController = require('../../controllers/interested-party-guide/have-say-pre-application');
+const { validationErrorHandler } = require('../../validators/validation-error-handler');
+
+const {
+    rules: haveSayPreApplicationRules,
+  } = require('../../validators/interested-party-guide/have-say-pre-application');
+
 const router = express.Router();
 
-router.get('/have-say-pre-application', (req, res) => {
-  res.render(VIEW.INTERESTED_PARTY_GUIDE.HAVE_SAY_PRE_APPLICATION);
-});
+router.get('/have-say-pre-application', haveSayPreApplicationController.getHaveSayPreApplication);
+
+router.post(
+    '/have-say-pre-application',
+    haveSayPreApplicationRules(),
+    validationErrorHandler,
+    haveSayPreApplicationController.postHaveSayPreApplication
+  );
+
 module.exports = router;
