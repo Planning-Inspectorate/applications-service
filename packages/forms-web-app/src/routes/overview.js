ecfdd094227551a018631918466dd9f9498fd70b
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-10-27 10:14:13 +0100

    test: add tets for overview page

--- packages/forms-web-app/src/routes/overview.js
+++ packages/forms-web-app/src/routes/overview.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const { VIEW } = require('../lib/views');
 const overviewController = require('../controllers/overview');
-const logger = require('../lib/logger');
-
-
 
 const router = express.Router();
 
