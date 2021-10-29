71682bf3850c9cc0eee5780c9381dc1e0baa4096
elvin <ali.elvin@gmail.com>
2021-10-29 18:11:37 +0100

    Clean up

--- packages/applications-service-api/src/routes/index.js
+++ packages/applications-service-api/src/routes/index.js
@@ -8,9 +8,7 @@ const express = require('express');
 const router = express.Router();
 
 const apiDocsRouter = require('./api-docs');
-const dbConnectionTestRouter = require('./db-connection-test');
 
 router.use('/api-docs', apiDocsRouter);
-router.use('/test', dbConnectionTestRouter);
 
 module.exports = router;
