d0812627da718c9fa741fd54949ba1c960cc778a
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-01 10:00:35 +0000

    test: modify test for register page title changes

--- packages/forms-web-app/__tests__/unit/controllers/register/start.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/register/start.test.js
@@ -1,5 +1,6 @@
 const startController = require('../../../../src/controllers/register/start');
 const { VIEW } = require('../../../../src/lib/views');
+const config = require('../../../../src/config');
 const { mockReq, mockRes } = require('../../mocks');
 
 jest.mock('../../../../src/lib/logger');
@@ -17,7 +18,7 @@ describe('controllers/register/start', () => {
     describe('getStart', () => {
         it('should call the correct template', () => {
             startController.getStart(req, res);
-            expect(res.render).toHaveBeenCalledWith('register/start');
+            expect(res.render).toHaveBeenCalledWith('register/start', {"serviceName": config.serviceName});
         });
     });
 });
