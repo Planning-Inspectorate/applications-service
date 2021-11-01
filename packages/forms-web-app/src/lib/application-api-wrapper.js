adcb798cfdac5fa9d9e42baaa8f10e4b818164e8
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-01 16:02:24 +0000

    add page for project search

---
+++ packages/forms-web-app/src/lib/application-api-wrapper.js
@@ -0,0 +1,67 @@
+const fetch = require('node-fetch');
+const uuid = require('uuid');
+const { utils } = require('@pins/common');
+
+const config = require('../config');
+const parentLogger = require('./logger');
+
+async function handler(path, method = 'GET', opts = {}, headers = {}) {
+  const correlationId = uuid.v4();
+  const url = `${config.appeals.url}${path}`;
+
+  const logger = parentLogger.child({
+    correlationId,
+    service: 'Application Service API',
+  });
+
+  try {
+    logger.debug({ url, method, opts, headers }, 'New call');
+
+    return await utils.promiseTimeout(
+      config.appeals.timeout,
+      Promise.resolve().then(async () => {
+        const apiResponse = await fetch(url, {
+          method,
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Correlation-ID': correlationId,
+            ...headers,
+          },
+          ...opts,
+        });
+
+        if (!apiResponse.ok) {
+          logger.debug(apiResponse, 'API Response not OK');
+          try {
+            const errorResponse = await apiResponse.json();
+            /* istanbul ignore else */
+            if (errorResponse.errors && errorResponse.errors.length) {
+              throw new Error(errorResponse.errors.join('\n'));
+            }
+
+            /* istanbul ignore next */
+            throw new Error(apiResponse.statusText);
+          } catch (e) {
+            throw new Error(e.message);
+          }
+        }
+
+        logger.debug('Successfully called');
+
+        const data = await apiResponse.json();
+
+        logger.debug('Successfully parsed to JSON');
+
+        return data;
+      })
+    );
+  } catch (err) {
+    logger.error({ err }, 'Error');
+    throw err;
+  }
+}
+
+
+exports.getAllProjectList = async (sessionId) => {
+  return handler(`/api/v1/appeals/${sessionId}`);
+};
--- packages/forms-web-app/src/routes/index.js
+++ packages/forms-web-app/src/routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const homeRouter = require('./home');
 const cookieRouter = require('./cookies');
 const overviewRouter = require('./overview');
+const projectSearchRouter = require('./project-search');
 const guidancePagesRouter = require('./guidance-pages');
 const registerRouter = require('./register');
 const interestedPartyRouter = require('./interested-party-guide');
@@ -13,6 +14,7 @@ router.use('/', homeRouter);
 router.use('/', guidancePagesRouter);
 router.use('/cookies', cookieRouter);
 router.use('/overview', overviewRouter);
+router.use('/project-search', projectSearchRouter);
 router.use('/register', registerRouter);
 router.use(interestedPartyRouter);
 
