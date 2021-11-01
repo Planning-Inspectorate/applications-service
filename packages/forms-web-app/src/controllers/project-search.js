39cdecd4e779767a2d8bd61a9c6b3ff700ab05fd
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-01 16:31:05 +0000

    modify function name

--- packages/forms-web-app/src/controllers/project-search.js
+++ packages/forms-web-app/src/controllers/project-search.js
@@ -2,6 +2,6 @@ const config = require('../config');
 const { VIEW } = require('../lib/views');
 const logger = require('../lib/logger');
 
-exports.getOverview = async (req, res) => {
+exports.getProjectList = async (req, res) => {
   res.render(VIEW.PROJECT_SEARCH);
 };
