3f6b1fd80b974dbf6a436012b2e13990a24ce12b
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-05 11:44:21 +0000

    test: add test for application-api-wrapper

--- packages/forms-web-app/__tests__/unit/lib/application-api-wrapper.test.js
+++ packages/forms-web-app/__tests__/unit/lib/application-api-wrapper.test.js
@@ -21,7 +21,7 @@ jest.mock('../../../src/lib/logger', () => ({
 
 config.applications.url = 'http://fake.url';
 
-describe('lib/appeals-api-wrapper', () => {
+describe('lib/application-api-wrapper', () => {
   beforeEach(() => {
     fetch.resetMocks();
     fetch.doMock();
@@ -32,91 +32,20 @@ describe('lib/appeals-api-wrapper', () => {
     jest.useRealTimers();
   });
 
-  describe('createOrUpdateAppeal', () => {
-    [
-      {
-        title: 'POST when a id is missing',
-        given: () => {
-          fetch.mockResponseOnce(JSON.stringify({ good: 'data' }));
-
-          return {
-            a: 'b',
-            id: undefined,
-          };
-        },
-        expected: (appealsApiResponse) => {
-          expect(fetch).toHaveBeenCalledWith(`${config.appeals.url}/api/v1/appeals`, {
-            body: '{"a":"b"}',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Correlation-ID': uuid.v4(),
-            },
-            method: 'POST',
-          });
-          expect(appealsApiResponse).toEqual({ good: 'data' });
-        },
-      },
-      {
-        title: 'PUT when a id is provided',
-        given: () => {
-          fetch.mockResponseOnce(JSON.stringify({ shouldBe: 'valid' }));
-
-          return {
-            c: 'd',
-            id: '123-abc',
-          };
-        },
-        expected: (appealsApiResponse) => {
-          expect(fetch).toHaveBeenCalledWith(`${config.appeals.url}/api/v1/appeals/123-abc`, {
-            body: '{"c":"d","id":"123-abc"}',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Correlation-ID': uuid.v4(),
-            },
-            method: 'PUT',
-          });
-          expect(appealsApiResponse).toEqual({ shouldBe: 'valid' });
-        },
-      },
-    ].forEach(({ title, given, expected }) => {
-      it(`should ${title}`, async () => {
-        const appealsApiResponse = await createOrUpdateAppeal(given());
-        expected(appealsApiResponse);
-      });
-    });
-
-    it('should gracefully handle a fetch failure', async () => {
-      fetch.mockResponseOnce(JSON.stringify({ errors: ['something went wrong'] }), {
-        status: 400,
-      });
-
-      /**
-       * Non-standard way to handle functions that throw in Jest.
-       * I believe this is because of `utils.promiseTimout`.
-       */
-      try {
-        await createOrUpdateAppeal({
-          a: 'b',
-        });
-      } catch (e) {
-        expect(e.toString()).toEqual('Error: something went wrong');
-      }
+  
+  describe('getAllProjectList', () => {
+    it(`should call the expected URL`, async () => {
+      fetch.mockResponseOnce(JSON.stringify({ shouldBe: 'valid' }));
+      await getAllProjectList();
+      expect(fetch.mock.calls[0][0]).toEqual('http://fake.url/api/v1/applications');
     });
   });
 
-  describe('getExistingAppeal', () => {
+  describe('getProjectData', () => {
     it(`should call the expected URL`, async () => {
       fetch.mockResponseOnce(JSON.stringify({ shouldBe: 'valid' }));
-      await getExistingAppeal('123');
-      expect(fetch.mock.calls[0][0]).toEqual('http://fake.url/api/v1/appeals/123');
-    });
-  });
-
-  describe('getLPAList', () => {
-    it(`should call the expected URL`, async () => {
-      fetch.mockResponseOnce(JSON.stringify({ shouldBe: 'valid' }));
-      await getLPAList();
-      expect(fetch.mock.calls[0][0]).toEqual('http://fake.url/api/v1/local-planning-authorities');
+      await getProjectData('ABC123');
+      expect(fetch.mock.calls[0][0]).toEqual('http://fake.url/api/v1/applications/ABC123');
     });
   });
 });
