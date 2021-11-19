8beca5401826eb19914cfc10b564d79227c9d177
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-19 10:51:52 +0000

    add check-your-answer page component

---
+++ packages/forms-web-app/src/controllers/register/check-your-answers.js
@@ -0,0 +1,8 @@
+const logger = require('../../lib/logger');
+const { VIEW } = require('../../lib/views');
+const config = require('../../config');
+
+exports.getCheckYourAnswers = async (req, res) => {
+  logger.info('-----------------------'+JSON.stringify(req.session.registrationData));
+  res.render(VIEW.REGISTER.CHECK_YOUR_ANSWERS, {data: req.session.registrationData});
+};
