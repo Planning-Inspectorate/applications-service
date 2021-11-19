e589d206c48047bb025600f248e43f873536796e
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-19 10:49:58 +0000

    add over-18 page component

---
+++ packages/forms-web-app/src/controllers/register/over-18.js
@@ -0,0 +1,23 @@
+const logger = require('../../lib/logger');
+const { VIEW } = require('../../lib/views');
+
+exports.getOver18 = async (req, res) => {
+  res.render(VIEW.REGISTER.OVER_18);
+};
+
+exports.postOver18 = async (req, res) => {
+  const { body } = req;
+  const over18 = body['over-18'];
+  const { errors = {}, errorSummary = [] } = body;
+  if (errors['full-name'] || Object.keys(errors).length > 0) {
+    res.render(VIEW.REGISTER.OVER_18, {
+      errors,
+      errorSummary,
+    });
+    return;
+  }
+
+  req.session.registrationData['over-18'] = over18;
+  logger.info('-----------------------'+JSON.stringify(req.session));
+  res.redirect(`/${VIEW.REGISTER.ADDRESS}`);
+};
