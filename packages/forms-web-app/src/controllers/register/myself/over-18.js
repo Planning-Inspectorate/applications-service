1283aa10340ce54d01e41b695348a7620f65e010
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-19 13:13:12 +0000

    add tests for over-18 controller

--- packages/forms-web-app/src/controllers/register/myself/over-18.js
+++ packages/forms-web-app/src/controllers/register/myself/over-18.js
@@ -2,7 +2,7 @@ const logger = require('../../../lib/logger');
 const { VIEW } = require('../../../lib/views');
 
 exports.getOver18 = async (req, res) => {
-  res.render(VIEW.REGISTER.OVER_18);
+  res.render(VIEW.REGISTER.MYSELF.OVER_18);
 };
 
 exports.postOver18 = async (req, res) => {
