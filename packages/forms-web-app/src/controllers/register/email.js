f760cb3309f65d83821d0357c3441416e29c1a8e
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-19 10:50:33 +0000

    add email page component

---
+++ packages/forms-web-app/src/controllers/register/email.js
@@ -0,0 +1,22 @@
+const logger = require('../../lib/logger');
+const { VIEW } = require('../../lib/views');
+
+exports.getEmail = async (req, res) => {
+  res.render(VIEW.REGISTER.EMAIL);
+};
+
+exports.postEmail = async (req, res) => {
+  const { body } = req;
+  const { errors = {}, errorSummary = [] } = body;
+  if (errors['email'] || Object.keys(errors).length > 0) {
+    res.render(VIEW.REGISTER.EMAIL, {
+      errors,
+      errorSummary,
+    });
+    return;
+  }
+
+  req.session.registrationData['email'] = body['email'];
+  logger.info('-----------------------'+JSON.stringify(req.session));
+  res.redirect(`/${VIEW.REGISTER.TELEPHONE}`);
+};
---
+++ packages/forms-web-app/src/routes/register/email.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const emailController = require('../../controllers/register/email');
+const { validationErrorHandler } = require('../../validators/validation-error-handler');
+const {
+  rules: emailValidationRules,
+} = require('../../validators/register/email');
+
+
+const router = express.Router();
+
+router.get('/email', emailController.getEmail);
+
+router.post(
+  '/email',
+  emailValidationRules(),
+  validationErrorHandler,
+  emailController.postEmail
+);
+
+module.exports = router;
