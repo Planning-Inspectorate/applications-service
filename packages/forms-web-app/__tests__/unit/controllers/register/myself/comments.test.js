c6fe3d28888b7b62e68c6040593fdb7107bf33bd
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-11-19 13:35:03 +0000

    add tests for comments controller

---
+++ packages/forms-web-app/__tests__/unit/controllers/register/myself/comments.test.js
@@ -0,0 +1,66 @@
+const commentsController = require('../../../../../src/controllers/register/myself/comments');
+const { VIEW } = require('../../../../../src/lib/views');
+const { mockReq, mockRes } = require('../../../mocks');
+
+jest.mock('../../../../../src/lib/logger');
+
+describe('controllers/register/myself/comments', () => {
+    let req;
+    let res;
+
+    beforeEach(() => {
+        req = {
+            ...mockReq(),
+            session: {
+                registrationData: {
+                    'comments': 'test'
+                }
+            },
+        };
+        res = mockRes();
+        jest.resetAllMocks();
+    });
+
+    describe('getComments', () => {
+        it('should call the correct template', () => {
+            commentsController.getComments(req, res);
+            expect(res.render).toHaveBeenCalledWith('register/myself/comments');
+        });
+    });
+
+    describe('postComments', () => {
+        it(`'should post data and redirect to '/${VIEW.REGISTER.MYSELF.CHECK_YOUR_ANSWERS}' if comments is provided`, async () => {
+            const mockRequest = {
+                ...req,
+                body: {
+                    'comments': 'test',
+                },
+            };
+            await commentsController.postComments(
+                mockRequest,
+                res
+            );
+
+            expect(res.redirect).toHaveBeenCalledWith(`/${VIEW.REGISTER.MYSELF.CHECK_YOUR_ANSWERS}`);
+        });
+        it('should re-render the template with errors if there is any validation error', async () => {
+            const mockRequest = {
+                ...req,
+                body: {
+                    errorSummary: [{ text: 'There were errors here', href: '#' }],
+                    errors: { a: 'b' }
+                },
+            };
+            await commentsController.postComments(
+                mockRequest,
+                res
+            );
+            expect(res.redirect).not.toHaveBeenCalled();
+
+            expect(res.render).toHaveBeenCalledWith(VIEW.REGISTER.MYSELF.COMMENTS, {
+                errorSummary: [{ text: 'There were errors here', href: '#' }],
+                errors: { a: 'b' }
+            });
+        });
+    });
+});
