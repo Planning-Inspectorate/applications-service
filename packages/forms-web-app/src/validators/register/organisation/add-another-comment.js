0fdef1ba643f782cce500f434feaec40c70fcbed
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-12-03 12:47:10 +0000

    modifu add-another-comment validator

--- packages/forms-web-app/src/validators/register/organisation/add-another-comment.js
+++ packages/forms-web-app/src/validators/register/organisation/add-another-comment.js
@@ -1,14 +1,14 @@
 const { body } = require('express-validator');
 
-
 const rules = () => {
   return [
     body('add-another-comment')
-    .notEmpty()
-    .withMessage('Select yes if you want to add another comment'),
-    // body('length')
-    // .isInt({ min:1, max: 16})
-    // .withMessage('Enter atleast one comment'),
+     .notEmpty()
+     .withMessage('Select yes if you want to add another comment'),
+    body('length')
+     .if(body('add-another-comment').matches('no'))
+     .isInt({ min:1})
+     .withMessage('You should add atleast one comment'),
   ];
 };
 
