494431205512c334203d6ca7dcf78b18a99f9ee9
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2021-12-23 14:54:37 +0000

    add project timeline page placeholder

--- packages/forms-web-app/src/routes/overview.js
+++ packages/forms-web-app/src/routes/overview.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const overviewController = require('../controllers/overview');
+const projectTimeLineController = require('../controllers/project-timeline');
 
 const router = express.Router();
 
+router.get('/project-timeline', projectTimeLineController.getProjectTimeLine);
 router.get('/:case_ref', overviewController.getOverview);
 
 module.exports = router;
