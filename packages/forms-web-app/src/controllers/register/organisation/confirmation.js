6203bf45a40e6809195442bc0d77a38b1f7454a1
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-01-14 12:02:01 +0000

    update organisation comments page

--- packages/forms-web-app/src/controllers/register/organisation/confirmation.js
+++ packages/forms-web-app/src/controllers/register/organisation/confirmation.js
@@ -1,20 +1,28 @@
 const logger = require('../../../lib/logger');
 const { VIEW } = require('../../../lib/views');
-const { postRegistrationData, postCommentsData } = require('../../../services/registration.service');
+const {
+  postRegistrationData,
+  postCommentsData,
+} = require('../../../services/registration.service');
 
 exports.getConfirmation = async (req, res) => {
   req.session.orgRegdata['case_ref'] = req.session.caseRef;
   const registrationData = JSON.stringify(req.session.orgRegdata);
   const response = await postRegistrationData(registrationData);
   const ipRefNo = response.data;
-  const commentsData = JSON.stringify({comments: req.session.comments, mode: req.session.mode});
+  const commentsData = JSON.stringify({ comments: req.session.comment, mode: req.session.mode });
   await postCommentsData(ipRefNo, commentsData);
   const email = req.session.orgRegdata.email;
   delete req.session.comments;
   delete req.session.orgRegdata;
   if (req.session.mode === 'draft') {
-    res.render(VIEW.REGISTER.SAVE_CONFIRMATION, {ipRefNo: ipRefNo, email: email});
+    res.render(VIEW.REGISTER.SAVE_CONFIRMATION, { ipRefNo: ipRefNo, email: email });
   } else {
-    res.render(VIEW.REGISTER.ORGANISATION.CONFIRMATION, {ipRefNo: ipRefNo, email: email, projectName: req.session.projectName, caseRef: req.session.caseRef});
+    res.render(VIEW.REGISTER.ORGANISATION.CONFIRMATION, {
+      ipRefNo: ipRefNo,
+      email: email,
+      projectName: req.session.projectName,
+      caseRef: req.session.caseRef,
+    });
   }
 };
