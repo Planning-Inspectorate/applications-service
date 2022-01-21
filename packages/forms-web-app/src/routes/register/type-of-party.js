3606ce3632a2c953baf46432b3b1fe4683bb5dc7
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-01-21 13:39:34 +0000

    change urls

--- packages/forms-web-app/src/routes/register/type-of-party.js
+++ packages/forms-web-app/src/routes/register/type-of-party.js
@@ -3,19 +3,17 @@ const express = require('express');
 const typeOfPartyController = require('../../controllers/register/type-of-party');
 const { validationErrorHandler } = require('../../validators/validation-error-handler');
 
-const {
-    rules: typeOfPartyRules,
-  } = require('../../validators/register/type-of-party');
+const { rules: typeOfPartyRules } = require('../../validators/register/type-of-party');
 
 const router = express.Router();
 
-router.get('/type-of-party', typeOfPartyController.getTypeOfParty);
+router.get('/who-registering-for', typeOfPartyController.getTypeOfParty);
 
 router.post(
-    '/type-of-party',
-    typeOfPartyRules(),
-    validationErrorHandler,
-    typeOfPartyController.postTypeOfParty
-  );
+  '/who-registering-for',
+  typeOfPartyRules(),
+  validationErrorHandler,
+  typeOfPartyController.postTypeOfParty
+);
 
 module.exports = router;
