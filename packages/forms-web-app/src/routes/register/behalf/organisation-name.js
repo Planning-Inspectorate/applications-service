862fb8695ef6fcb6729f9d022236ce6c9ea81ead
elvin <ali.elvin@gmail.com>
2022-01-24 12:09:42 +0000

    Validate organisation name

--- packages/forms-web-app/src/routes/register/behalf/organisation-name.js
+++ packages/forms-web-app/src/routes/register/behalf/organisation-name.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 const organisationNameController = require('../../../controllers/register/behalf/organisation-name');
+const { validationErrorHandler } = require('../../../validators/validation-error-handler');
+const {
+  rules: organisationNameValidationRules,
+} = require('../../../validators/register/behalf/organisation-name');
 
 const router = express.Router();
 
 router.get('/organisation-name', organisationNameController.getOrganisationName);
 
-router.post('/organisation-name', organisationNameController.postOrganisationName);
+router.post(
+  '/organisation-name',
+  organisationNameValidationRules(),
+  validationErrorHandler,
+  organisationNameController.postOrganisationName
+);
 
 module.exports = router;
