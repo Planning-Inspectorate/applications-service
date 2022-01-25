50ea5f2065ac599d806f1553d53666ed06baecae
elvin <ali.elvin@gmail.com>
2022-01-25 15:22:54 +0000

    Fix validation message

--- packages/forms-web-app/src/validators/register/behalf/organisation-name.js
+++ packages/forms-web-app/src/validators/register/behalf/organisation-name.js
@@ -1,7 +1,11 @@
 const { body } = require('express-validator');
 
 const rules = () => {
-  return [body('organisation-name').notEmpty().withMessage('Enter your organisation name')];
+  return [
+    body('organisation-name')
+      .notEmpty()
+      .withMessage('Enter the name of the organisation you work for'),
+  ];
 };
 
 module.exports = {
