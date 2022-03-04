719964f017dcc9cd41e09c1c8371ad9ade669d47
elvin <ali.elvin@gmail.com>
2022-03-04 15:40:24 +0000

    Fix typo

--- packages/forms-web-app/src/validators/register/myself/tell-us-about-project.js
+++ packages/forms-web-app/src/validators/register/myself/tell-us-about-project.js
@@ -5,7 +5,7 @@ const rules = () => {
     body('comment')
       .notEmpty()
       .withMessage('Enter what you want to tell us about this proposed project'),
-    body('country')
+    body('comment')
       .isLength({ min: 1, max: 65535 })
       .withMessage('What you want to tell us must be 65535 characters or less'),
   ];
