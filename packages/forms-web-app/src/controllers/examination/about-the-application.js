98155a1ba1ddadddf793010b41beb90ec9be101f
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-03-04 16:44:57 +0000

    remove logs

--- packages/forms-web-app/src/controllers/examination/about-the-application.js
+++ packages/forms-web-app/src/controllers/examination/about-the-application.js
@@ -40,7 +40,6 @@ function renderData(req, res, caseRef, response) {
 }
 
 exports.getAboutTheApplication = async (req, res) => {
-  console.log('---------------------->' + JSON.stringify(req.params));
   const caseRef = req.params.case_ref;
   const pageNumber = req.params.page;
   let search = req.session.document_search ? req.session.document_search : '';
@@ -55,7 +54,6 @@ exports.getAboutTheApplication = async (req, res) => {
 };
 
 exports.postSearchDocument = async (req, res) => {
-  console.log('=========================>' + JSON.stringify(req.params));
   const caseRef = req.params.case_ref;
   const pageNumber = req.params.page;
   const { body } = req;
