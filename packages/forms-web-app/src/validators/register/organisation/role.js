c840e62dc52576fab4c3aebe6b283fdc9b3db59b
Chris Cundill <chrisc@diligentsoft.co>
2022-03-09 17:13:17 +0000

    ASB-172: Added validation rules and error messages as per story spec.  Also made improvement to sanitise telephone number input to remove spaces.

--- packages/forms-web-app/src/validators/register/organisation/role.js
+++ packages/forms-web-app/src/validators/register/organisation/role.js
@@ -1,7 +1,12 @@
 const { body } = require('express-validator');
 
 const rules = () => {
-  return [body('role').notEmpty().withMessage('Enter your job title or volunteer role')];
+  return [
+    body('role').notEmpty().withMessage('Enter your job title or volunteer role'),
+    body('role')
+      .isLength({ min: 3, max: 64 })
+      .withMessage('Your job title or volunteer role must be 64 characters or less'),
+  ];
 };
 
 module.exports = {
