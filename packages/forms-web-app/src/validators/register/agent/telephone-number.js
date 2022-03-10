4b6c1f71b14b56d6941171e7fe568f341f2eb568
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-03-10 14:30:40 +0000

    update field level validiation for agents journey

--- packages/forms-web-app/src/validators/register/agent/telephone-number.js
+++ packages/forms-web-app/src/validators/register/agent/telephone-number.js
@@ -1,7 +1,17 @@
 const { body } = require('express-validator');
 
 const rules = () => {
-  return [body('telephone').notEmpty().withMessage('Enter your telephone number')];
+  return [
+    body('telephone').notEmpty().withMessage('Enter your telephone number'),
+    body('telephone')
+      .isLength({ min: 1, max: 255 })
+      .withMessage('Telephone number must be 255 characters or less'),
+    body('telephone')
+      .isNumeric()
+      .withMessage(
+        'Enter a telephone number, like 01632 960 001, 07700 900 982 or 44 808 157 0192'
+      ),
+  ];
 };
 
 module.exports = {
