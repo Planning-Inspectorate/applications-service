87782c509f1cfee098148406f4237b452bdb5a8c
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-03-15 12:31:42 +0000

    fix telephone validation for agents journey

--- packages/forms-web-app/src/validators/register/agent/telephone-number.js
+++ packages/forms-web-app/src/validators/register/agent/telephone-number.js
@@ -7,6 +7,7 @@ const rules = () => {
       .isLength({ min: 1, max: 255 })
       .withMessage('Telephone number must be 255 characters or less'),
     body('telephone')
+      .blacklist('\\s')
       .isNumeric()
       .withMessage(
         'Enter a telephone number, like 01632 960 001, 07700 900 982 or 44 808 157 0192'
