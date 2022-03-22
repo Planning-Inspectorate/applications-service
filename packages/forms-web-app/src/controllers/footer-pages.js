6b2ef255cf4c461b3a65b09c7a9ddaeccc0e238b
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-03-22 14:23:18 +0000

    update links on header footer

--- packages/forms-web-app/src/controllers/footer-pages.js
+++ packages/forms-web-app/src/controllers/footer-pages.js
@@ -1,13 +1,5 @@
 const { VIEW } = require('../lib/views');
 
-exports.getTermsAndConditions = (_, res) => {
-  res.render(VIEW.FOOTER_PAGES.TERMS_AND_CONDITIONS);
-};
-
-exports.getSitemap = (_, res) => {
-  res.render(VIEW.FOOTER_PAGES.SITEMAP);
-};
-
 exports.getAccessibility = (_, res) => {
   res.render(VIEW.FOOTER_PAGES.ACCESSIBILITY);
 };
