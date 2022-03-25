16a4315874a59160d24dd62eab16d38009bf9108
Chris Cundill <chrisc@diligentsoft.co>
2022-03-25 11:56:37 +0000

    ASB-138: Changed route for accessibility statement and aligned content with prototype

--- packages/forms-web-app/src/routes/footer-pages.js
+++ packages/forms-web-app/src/routes/footer-pages.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getAccessibility, getCookiesInfo } = require('../controllers/footer-pages');
 
 const router = express.Router();
-router.get('/accessibility', getAccessibility);
+router.get('/accessibility-statement', getAccessibility);
 router.get('/cookies-info', getCookiesInfo);
 
 module.exports = router;
