a043400ce725b8d319872b8e5dc704a0e7b3ba4c
Chris Cundill <chrisc@diligentsoft.co>
2022-04-12 09:51:56 +0100

    ASB-234: Removed remnants of before-you-start placeholder page

--- packages/forms-web-app/src/routes/guidance-pages.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getBeforeApply } = require('../controllers/guidance-pages');
-
-const router = express.Router();
-
-router.get('/before-you-apply', getBeforeApply);
-
-module.exports = router;
