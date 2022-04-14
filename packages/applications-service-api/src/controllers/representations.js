80614d88c445d7521c5f0edad3576d966590a082
Chris Cundill <chrisc@diligentsoft.co>
2022-04-14 13:31:23 +0100

    ASB-230: Added endpoint to prove retrieval of redacted representations for a project

---
+++ packages/applications-service-api/src/controllers/representations.js
@@ -0,0 +1,31 @@
+const { StatusCodes } = require('http-status-codes');
+
+const logger = require('../lib/logger');
+
+const { getRepresentationsForApplication } = require('../services/representation.service');
+
+const ApiError = require('../error/apiError');
+
+module.exports = {
+  async getRepresentationsForApplication(req, res) {
+    const { applicationId } = req.query;
+    logger.debug(`Retrieving representations for application ref ${applicationId}`);
+    try {
+      const representations = await getRepresentationsForApplication(applicationId);
+
+      if (!representations.length) {
+        throw ApiError.noRepresentationsFound();
+      }
+
+      res.status(StatusCodes.OK).send(representations);
+    } catch (e) {
+      if (e instanceof ApiError) {
+        logger.debug(e.message);
+        res.status(e.code).send({ code: e.code, errors: e.message.errors });
+        return;
+      }
+      logger.error(e.message);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(`Problem getting representations \n ${e}`);
+    }
+  },
+};
--- packages/applications-service-api/src/routes/index.js
+++ packages/applications-service-api/src/routes/index.js
@@ -9,11 +9,13 @@ const interestedPartyRouter = require('./interested-party');
 const apiDocsRouter = require('./api-docs');
 const dbConnectionTestRouter = require('./db-connection-test');
 const documentRouter = require('./documents');
+const representationsRouter = require('./representations');
 
 router.use('/api/v1/applications', applicationsRouter);
 router.use('/api/v1/interested-party', interestedPartyRouter);
 router.use('/api-docs', apiDocsRouter);
 router.use('/test', dbConnectionTestRouter);
 router.use('/api/v1/documents', documentRouter);
+router.use('/api/v1/representations', representationsRouter);
 
 module.exports = router;
