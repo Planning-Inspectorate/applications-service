4c8b67fab2bcb4410dbc695ab3bc66c79f85db24
elvin <ali.elvin@gmail.com>
2022-04-21 11:07:39 +0100

    ASb-271 Add endpoint /api/v2/documents?caseRef=TR040009

--- packages/applications-service-api/src/routes/documents.js
+++ packages/applications-service-api/src/routes/documents.js
@@ -5,5 +5,6 @@ const documentsController = require('../controllers/documents');
 const router = express.Router();
 
 router.post('/:caseRef', documentsController.getDocuments);
+router.post('/', documentsController.getV2Documents);
 
 module.exports = router;
