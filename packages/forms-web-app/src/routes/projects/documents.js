31b8592ba5a844c6c5e2022f60cf7a70b78fe29d
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-04-25 16:14:58 +0100

    Implement search box and search submission

--- packages/forms-web-app/src/routes/projects/documents.js
+++ packages/forms-web-app/src/routes/projects/documents.js
@@ -6,7 +6,7 @@ const aboutTheApplicationController = require('../../controllers/projects/docume
 
 router.get('/:case_ref', projectsController.getExamination);
 router.get('/:case_ref/documents/:page', aboutTheApplicationController.getAboutTheApplication);
-router.post('/:case_ref/documents/search/:page', aboutTheApplicationController.postSearchDocument);
-router.post('/:case_ref/documents/filter/:page', aboutTheApplicationController.postFilterDocument);
+router.post('/:case_ref/documents/:page', aboutTheApplicationController.postSearchDocument);
+router.post('/:case_ref/documents/:page', aboutTheApplicationController.postFilterDocument);
 
 module.exports = router;
