90be4bc400442c7b86e60fc720d8455c60c6b1bb
Chris Cundill <chrisc@diligentsoft.co>
2022-04-27 16:52:25 +0100

    ASB-270 Fixed prettier violation

--- packages/forms-web-app/src/controllers/projects/documents.js
+++ packages/forms-web-app/src/controllers/projects/documents.js
@@ -29,7 +29,7 @@ function renderData(req, res, params, response) {
     logger.debug(`Document data received:  ${JSON.stringify(documents)} `);
     const pageData = getPageData(respData);
     const paginationData = generatePagination(pageData.currentPage, pageData.totalPages);
-    console.log(documents)
+    console.log(documents);
     res.render(VIEW.PROJECTS.DOCUMENTS, {
       documents,
       projectName,
