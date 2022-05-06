43ccfd1833fd5310a4aad424ea0086b303e0f26a
Chris Cundill <christophercundill@gmail.com>
2022-05-06 12:15:52 +0100

    ASB-312 Created paginationBar macro so that pagination template code from documents can be re-used for registration comments.  Also extracted out function from controller used to derive pagination data from paginated results. (#251)
    Co-authored-by: Chris Cundill <chrisc@diligentsoft.co>

--- packages/forms-web-app/src/lib/pagination.js
+++ packages/forms-web-app/src/lib/pagination.js
@@ -1,4 +1,31 @@
-const generatePageNumbers = (currentPage, totalPages) => {
+/**
+ * Derives pagination data from a given set of paginated results
+ * @param paginatedResults paginated results returned from a Applications API call
+ * @returns an object containing pagination data for the result set which includes the start and end range that is
+ * represented by the current page of results
+ */
+const getPaginationData = (paginatedResults) => {
+  const paginationData = {};
+  paginationData.totalItems = paginatedResults.totalItems;
+  paginationData.itemsPerPage = paginatedResults.itemsPerPage;
+  paginationData.totalPages = paginatedResults.totalPages;
+  paginationData.currentPage = parseInt(paginatedResults.currentPage, 10);
+  paginationData.fromRange = paginatedResults.itemsPerPage * (paginatedResults.currentPage - 1) + 1;
+  paginationData.toRange =
+    paginationData.currentPage === paginatedResults.totalPages
+      ? paginatedResults.totalItems
+      : paginatedResults.itemsPerPage * paginatedResults.currentPage;
+  return paginationData;
+};
+
+/**
+ * Calculates the page numbers which should be offered to users for a given amount of pages, taking into account
+ * the current page of the user
+ * @param currentPage current page of the user
+ * @param totalPages total number of result pages available
+ * @returns an array of page options to be displayed
+ */
+const calculatePageNumbers = (currentPage, totalPages) => {
   let delta = 1;
   if (currentPage === 1 || totalPages === currentPage) {
     delta = 2;
@@ -21,16 +48,23 @@ const generatePageNumbers = (currentPage, totalPages) => {
   }, []);
 };
 
-const generatePagination = (currentPage, totalPages) => {
-  let pageNumbers = generatePageNumbers(currentPage, totalPages);
-  if (currentPage !== 1) {
+/**
+ * Calculates the page options which should be offered to users for a given set of pagination data
+ * @param paginationData
+ * @returns an array of page options to be displayed
+ */
+const calculatePageOptions = (paginationData) => {
+  let pageNumbers = calculatePageNumbers(paginationData.currentPage, paginationData.totalPages);
+  if (paginationData.currentPage !== 1) {
     pageNumbers = ['prev', ...pageNumbers];
   }
-  if (currentPage !== totalPages) {
+  if (paginationData.currentPage !== paginationData.totalPages) {
     pageNumbers = [...pageNumbers, 'next'];
   }
   return pageNumbers;
 };
+
 module.exports = {
-  generatePagination,
+  calculatePageOptions,
+  getPaginationData,
 };
