c22fb738961124e16b0e0c5d84324c0895711c4f
Pankaj K <pankaj.khare@planninginspectorate.gov.uk>
2022-05-13 12:20:51 +0100

    ASB-300 add search box for represenatation

--- packages/forms-web-app/src/controllers/projects/representations.js
+++ packages/forms-web-app/src/controllers/projects/representations.js
@@ -4,12 +4,15 @@ const { searchRepresentations } = require('../../lib/application-api-wrapper');
 const { getPaginationData, calculatePageOptions } = require('../../lib/pagination');
 
 exports.getRepresentations = async (req, res) => {
+  const { searchTerm } = req.query;
   const applicationResponse = await getAppData(req.params.case_ref);
+  const params = {
+    applicationId: req.params.case_ref,
+    page: '1',
+    ...req.query,
+  };
   if (applicationResponse.resp_code === 200) {
-    const representationsResponse = await searchRepresentations(
-      applicationResponse.data.CaseReference,
-      req.query.page
-    );
+    const representationsResponse = await searchRepresentations(params);
     const paginationData = getPaginationData(representationsResponse.data);
     const pageOptions = calculatePageOptions(paginationData);
     res.render(VIEW.PROJECTS.REPRESENTATIONS, {
@@ -18,6 +21,7 @@ exports.getRepresentations = async (req, res) => {
       representations: representationsResponse.data.representations,
       paginationData,
       pageOptions,
+      searchTerm,
     });
   }
 };
