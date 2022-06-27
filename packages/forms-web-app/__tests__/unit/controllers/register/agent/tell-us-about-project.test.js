705cb9bcf8ad9fc877e15b645ea4fda72504efc2
christopher-semmens <christopher.semmens@kinandcarta.com>
2022-06-27 12:34:20 +0100

    refactor: add a feature flag to hide save and exit registration journey

--- packages/forms-web-app/__tests__/unit/controllers/register/agent/tell-us-about-project.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/register/agent/tell-us-about-project.test.js
@@ -2,9 +2,11 @@ const commentsController = require('../../../../../src/controllers/register/agen
 const { postRegistration, putComments } = require('../../../../../src/lib/application-api-wrapper');
 const { VIEW } = require('../../../../../src/lib/views');
 const { mockReq, mockRes } = require('../../../mocks');
+const config = require('../../../../../src/config');
 
 jest.mock('../../../../../src/lib/application-api-wrapper');
 jest.mock('../../../../../src/lib/logger');
+jest.mock('../../../../../src/config');
 
 describe('controllers/register/agent/tell-us-about-project', () => {
 	let req;
@@ -23,6 +25,8 @@ describe('controllers/register/agent/tell-us-about-project', () => {
 		);
 
 		putComments.mockImplementation(() => Promise.resolve({ resp_code: 200, data: {} }));
+
+		config.featureFlag.allowSaveAndExitOption = true;
 	});
 
 	describe('getComments', () => {
--- packages/forms-web-app/src/controllers/register/myself/tell-us-about-project.js
+++ packages/forms-web-app/src/controllers/register/myself/tell-us-about-project.js
@@ -3,6 +3,7 @@ const {
 	postRegistrationData,
 	postCommentsData
 } = require('../../../services/registration.service');
+const config = require('../../../config');
 
 exports.getComments = async (req, res) => {
 	const { comment } = req.session;
@@ -11,8 +12,8 @@ exports.getComments = async (req, res) => {
 
 exports.postComments = async (req, res) => {
 	const { body } = req;
-
 	const { errors = {}, errorSummary = [], comment } = body;
+
 	if (errors.comment || Object.keys(errors).length > 0) {
 		res.render(VIEW.REGISTER.MYSELF.TELL_US_ABOUT_PROJECT, {
 			errors,
@@ -23,33 +24,42 @@ exports.postComments = async (req, res) => {
 	}
 
 	const mode = req.body.mode ? req.body.mode : req.query.mode;
+
 	if (mode === 'edit') {
 		req.session.comment = comment;
+
 		res.redirect(`/${VIEW.REGISTER.MYSELF.CHECK_YOUR_ANSWERS}`);
 	} else {
 		delete body.mode;
 
 		req.session.comment = comment;
-		if (mode === 'draft') {
+
+		if (mode === 'draft' && config.featureFlag.allowSaveAndExitOption) {
 			req.session.mode = 'draft';
 
 			let { ipRefNo } = req.session.mySelfRegdata;
 
 			if (!req.session.mySelfRegdata.ipRefNo) {
 				req.session.mySelfRegdata.case_ref = req.session.caseRef;
+
 				const registrationData = JSON.stringify(req.session.mySelfRegdata);
 				const response = await postRegistrationData(registrationData);
+
 				ipRefNo = response.data;
 				req.session.mySelfRegdata.ipRefNo = ipRefNo;
 			}
+
 			const commentsData = JSON.stringify({
 				comments: req.session.comment,
 				mode: req.session.mode
 			});
+
 			if (commentsData) await postCommentsData(ipRefNo, commentsData);
+
 			res.redirect(`/${VIEW.REGISTER.MYSELF.REGISTRATION_COMPLETE}`);
 		} else {
 			req.session.mode = 'final';
+
 			res.redirect(`/${VIEW.REGISTER.MYSELF.CHECK_YOUR_ANSWERS}`);
 		}
 	}
