c8f978272beecee768dceec0d9e2d2d25d18c9c8
mauriciokin <mauricio.deoliveira@kinandcarta.com>
2022-07-19 16:00:17 +0100

    refactor(revert file changes): revert File Changes
    revert file changes

--- packages/forms-web-app/src/validators/register/tell-us-about-project.js
+++ packages/forms-web-app/src/validators/register/tell-us-about-project.js
@@ -3,11 +3,11 @@ const { body } = require('express-validator');
 const validate = () => {
 	return [
 		body('comment')
-			.isLength({ max: 65234 })
-			.withMessage('What you want to tell us must be 65234 characters or less'),
-		body('comment')
 			.notEmpty()
-			.withMessage('Enter what you want to tell us about this proposed project')
+			.withMessage('Enter what you want to tell us about this proposed project'),
+		body('comment')
+			.isLength({ min: 1, max: 65234 })
+			.withMessage('What you want to tell us must be 65234 characters or less')
 	];
 };
 
