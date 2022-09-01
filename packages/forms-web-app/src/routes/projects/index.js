959a53b7b2a900d8ee5a8d1ec8283f67513a7bea
mauriciokin <mauricio.deoliveira@kinandcarta.com>
2022-09-01 12:52:20 +0100

    feat(activate relevant reps and document library with feature flags in production): add flag to prod
    Activate relevant reps and document library with feature flags in production

--- packages/forms-web-app/src/routes/projects/index.js
+++ packages/forms-web-app/src/routes/projects/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const config = require('../../config');
 
 const {
-	featureFlag: { hideProjectTimelineLink }
+	featureFlag: {
+		hideProjectTimelineLink,
+		allowDocumentLibrary,
+		allowRepresentation,
+		usePrivateBetaV1RoutesOnly
+	}
 } = config;
 
 const router = express.Router();
@@ -15,15 +20,24 @@ const timetableController = require('../../controllers/projects/timetable');
 const recommendationsController = require('../../controllers/projects/recommendations');
 const allExaminationDocsController = require('../../controllers/projects/all-examination-documents');
 
-router.get('/', projectSearchController.getProjectList);
-router.get('/all-examination-documents', allExaminationDocsController.getAllExaminationDocuments);
-router.get('/recommendations', recommendationsController.getRecommendations);
-router.get('/timetable', timetableController.getTimetable);
-router.get('/:case_ref/representations/:id', representationsController.getRepresentation);
-router.get('/:case_ref/representations', representationsController.getRepresentations);
-hideProjectTimelineLink &&
-	router.get('/project-timeline', projectTimeLineController.getProjectTimeLine);
+if (!usePrivateBetaV1RoutesOnly) {
+	router.get('/', projectSearchController.getProjectList);
+	router.get('/all-examination-documents', allExaminationDocsController.getAllExaminationDocuments);
+	router.get('/recommendations', recommendationsController.getRecommendations);
+	router.get('/timetable', timetableController.getTimetable);
+}
 
-router.use(documentsRouter);
+if (hideProjectTimelineLink) {
+	router.get('/project-timeline', projectTimeLineController.getProjectTimeLine);
+}
+
+if (allowDocumentLibrary) {
+	router.use(documentsRouter);
+}
+
+if (allowRepresentation) {
+	router.get('/:case_ref/representations/:id', representationsController.getRepresentation);
+	router.get('/:case_ref/representations', representationsController.getRepresentations);
+}
 
 module.exports = router;
