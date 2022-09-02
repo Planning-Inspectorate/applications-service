ccb10a5f40549076006ddb363af28b0431ca2ec8
mauriciokin <mauricio.deoliveira@kinandcarta.com>
2022-09-02 16:16:43 +0100

    fix(fix tests): fix tests
    fix tests

--- packages/forms-web-app/__tests__/unit/controllers/examination/have-your-say.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/examination/have-your-say.test.js
@@ -1,5 +1,4 @@
 const { getHaveYourSay } = require('../../../../src/controllers/examination/have-your-say');
-const { getAppData } = require('../../../../src/services/application.service');
 const { mockReq, mockRes } = require('../../mocks');
 
 jest.mock('../../../../src/lib/logger');
@@ -19,54 +18,42 @@ describe('controllers/register/start', () => {
 
 	describe('Have Your Say', () => {
 		it('should load project data and return register have your say view', async () => {
-			getAppData.mockImplementation(() =>
-				Promise.resolve({
-					resp_code: 200
-				})
-			);
 			const req = {
 				...mockReq(),
-				params: {
-					case_ref: 'ABC123'
+				session: {
+					examination: {
+						caseRef: 'ABC123'
+					}
 				}
 			};
 			await getHaveYourSay(req, res);
 			expect(res.render).toHaveBeenCalledWith('pages/examination/have-your-say', {
-				backLinkUrl: '/projects/timetable',
-				startNowUrl: '#',
+				backLinkUrl: '/projects/ABC123/examination-timetable',
+				pageTitle: 'Have your say during the Examination of the application',
+				startNowUrl: '/examination/tbc',
 				title: 'Have your say during the Examination of the application'
 			});
 		});
 
 		it('should load project data and return have your say view even when existing project in session  ', async () => {
-			getAppData.mockImplementation(() =>
-				Promise.resolve({
-					resp_code: 200
-				})
-			);
 			const req = {
 				...mockReq(),
-				params: {
-					case_ref: 'ABC123'
-				},
 				session: {
-					caseRef: 'ABC120'
+					examination: {
+						caseRef: 'ABC120'
+					}
 				}
 			};
 			await getHaveYourSay(req, res);
 			expect(res.render).toHaveBeenCalledWith('pages/examination/have-your-say', {
-				backLinkUrl: '/projects/timetable',
-				startNowUrl: '#',
+				backLinkUrl: '/projects/ABC120/examination-timetable',
+				pageTitle: 'Have your say during the Examination of the application',
+				startNowUrl: '/examination/tbc',
 				title: 'Have your say during the Examination of the application'
 			});
 		});
 
 		it('should redirect to not found route if project not found for caseRef provided', async () => {
-			getAppData.mockImplementation(() =>
-				Promise.resolve({
-					resp_code: 404
-				})
-			);
 			const req = {
 				...mockReq(),
 				params: {
@@ -79,11 +66,6 @@ describe('controllers/register/start', () => {
 		});
 
 		it('should redirect to not found route if caseRef not provided', async () => {
-			getAppData.mockImplementation(() =>
-				Promise.resolve({
-					resp_code: 404
-				})
-			);
 			const req = {
 				...mockReq(),
 				session: {}
