6102bcdce985bbb30a6254075311b130d33ac0b7
mauriciokin <mauricio.deoliveira@kinandcarta.com>
2022-09-21 16:42:38 +0100

    feat(add email address view, controller): add email address view, controller
    Add email address view, controller

--- packages/forms-web-app/src/controllers/examination/email.js
+++ packages/forms-web-app/src/controllers/examination/email.js
@@ -2,24 +2,59 @@ const {
 	routesConfig: {
 		examination: {
 			directory: examinationDirectory,
-			pages: { nameMyself, email }
+			pages: { email }
 		}
 	}
 } = require('../../routes/config');
 
+const config = require('../../config');
+const examinationSessionStorage = config?.sessionStorage?.examination;
+
 const pageData = {
-	backLinkUrl: `${examinationDirectory + nameMyself.route}`,
 	id: email.id,
 	pageTitle: email.name,
 	title: email.name
 };
 
 const getEmail = async (req, res) => {
+	const requestSession = req.session;
+	const userEmail = requestSession?.userEmail;
+	const currentView = requestSession?.currentView;
+	userEmail && (pageData.email = userEmail);
+
+	console.log({ requestSession });
+
+	if (!requestSession || !currentView) return res.status(404).render('error/not-found');
+
+	pageData.backLinkUrl = `${examinationDirectory + currentView.route}`;
 	res.render(email.view, pageData);
 };
 
 const postEmail = async (req, res) => {
-	res.render(email.view, pageData);
+	const { body = {} } = req;
+	const { errors = {}, errorSummary = [] } = body;
+	const requestSession = req.session;
+	const currentView = requestSession?.currentView;
+	requestSession?.userEmail && (pageData.email = requestSession?.userEmail);
+
+	if (errors[email.id] || Object.keys(errors).length > 0) {
+		return res.render(email.view, {
+			...pageData,
+			errors,
+			errorSummary
+		});
+	}
+
+	if (!requestSession || !currentView) return res.status(404).render('error/not-found');
+
+	requestSession.userEmail = examinationSessionStorage.property.email = body[email.id];
+	pageData.backLinkUrl = `${examinationDirectory + currentView.route}`;
+
+	if (req.query?.mode === 'edit') {
+		res.render(email.view, pageData);
+	} else {
+		res.redirect(`${examinationDirectory}/select-deadline-item`);
+	}
 };
 
 module.exports = {
