620f830b605b0a59e57e436d9429108d1962e914
Philip Philippou <philip@philips-mbp-3.lan>
2022-09-22 11:40:30 +0100

    fix(declaration.js): handle internal exception when posting declaration
    Add exception handling to relevant reps post to avoid uncaught exception forcing a web server
    re-start.

--- packages/forms-web-app/__tests__/unit/controllers/register/agent/declaration.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/register/agent/declaration.test.js
@@ -1,7 +1,7 @@
 const declarationController = require('../../../../../src/controllers/register/agent/declaration');
 const { postRegistration, putComments } = require('../../../../../src/lib/application-api-wrapper');
 const { VIEW } = require('../../../../../src/lib/views');
-const { mockReq, mockRes } = require('../../../mocks');
+const { mockReq, mockRes, mockResponse } = require('../../../mocks');
 
 jest.mock('../../../../../src/lib/application-api-wrapper');
 jest.mock('../../../../../src/lib/logger');
@@ -9,11 +9,26 @@ jest.mock('../../../../../src/lib/logger');
 describe('controllers/register/agent/declaration', () => {
 	let req;
 	let res;
+	let mockRequest;
 
 	beforeEach(() => {
 		req = mockReq();
 		res = mockRes();
 		jest.resetAllMocks();
+
+		mockRequest = {
+			...req,
+			session: {
+				behalfRegdata: {
+					email: 'anc@test.com',
+					ipRefNo: 'ABC123'
+				},
+				projectName: 'ABC',
+				caseRef: 'ABC123',
+				mode: 'final',
+				comment: 'comment'
+			}
+		};
 
 		postRegistration.mockImplementation(() =>
 			Promise.resolve({ resp_code: 200, data: '30020010' })
@@ -31,37 +46,21 @@ describe('controllers/register/agent/declaration', () => {
 
 	describe('postDeclaration', () => {
 		it(`'should post data and redirect to '/${VIEW.REGISTER.AGENT.REGISTRATION_COMPLETE}'`, async () => {
-			const mockRequest = {
-				...req,
-				session: {
-					behalfRegdata: {
-						email: 'anc@test.com',
-						ipRefNo: 'ABC123'
-					},
-					projectName: 'ABC',
-					caseRef: 'ABC123'
-				}
-			};
 			await declarationController.postDeclaration(mockRequest, res);
-
 			expect(res.redirect).toHaveBeenCalledWith(`/${VIEW.REGISTER.AGENT.REGISTRATION_COMPLETE}`);
 		});
 		it(`'should create session data and post, redirect to '/${VIEW.REGISTER.AGENT.REGISTRATION_COMPLETE}' if no ipRef exists in session`, async () => {
-			const mockRequest = {
-				...req,
-				session: {
-					behalfRegdata: {
-						email: 'anc@test.com'
-					},
-					projectName: 'ABC',
-					caseRef: 'ABC123',
-					mode: 'final',
-					comment: 'comment'
-				}
-			};
 			await declarationController.postDeclaration(mockRequest, res);
-
 			expect(res.redirect).toHaveBeenCalledWith(`/${VIEW.REGISTER.AGENT.REGISTRATION_COMPLETE}`);
+		});
+
+		it('handle exception thrown by API when writing representations', async () => {
+			res = mockResponse();
+			putComments.mockImplementation(() => {
+				throw new Error();
+			});
+			await declarationController.postDeclaration(mockRequest, res);
+			expect(res.render).toHaveBeenCalledWith('error/unhandled-exception');
 		});
 	});
 });
--- packages/forms-web-app/src/controllers/register/myself/declaration.js
+++ packages/forms-web-app/src/controllers/register/myself/declaration.js
@@ -10,19 +10,23 @@ exports.getDeclaration = async (req, res) => {
 
 exports.postDeclaration = async (req, res) => {
 	let { ipRefNo } = req.session.mySelfRegdata;
+	try {
+		if (!ipRefNo) {
+			req.session.mySelfRegdata.case_ref = req.session.caseRef;
+			req.session.mySelfRegdata.mode = req.session.mode;
+			const registrationData = JSON.stringify(req.session.mySelfRegdata);
+			const response = await postRegistrationData(registrationData);
+			ipRefNo = response.data;
+			req.session.mySelfRegdata.ipRefNo = ipRefNo;
+		}
 
-	if (!ipRefNo) {
-		req.session.mySelfRegdata.case_ref = req.session.caseRef;
-		req.session.mySelfRegdata.mode = req.session.mode;
-		const registrationData = JSON.stringify(req.session.mySelfRegdata);
-		const response = await postRegistrationData(registrationData);
-		ipRefNo = response.data;
-		req.session.mySelfRegdata.ipRefNo = ipRefNo;
-	}
-
-	const commentsData = JSON.stringify({ comments: req.session.comment, mode: req.session.mode });
-	if (commentsData && Object.keys(JSON.parse(commentsData)).length) {
-		await postCommentsData(ipRefNo, commentsData);
+		const commentsData = JSON.stringify({ comments: req.session.comment, mode: req.session.mode });
+		if (commentsData && Object.keys(JSON.parse(commentsData)).length) {
+			await postCommentsData(ipRefNo, commentsData);
+		}
+	} catch (e) {
+		req.log.error(e, 'Could not Post declaration, internal error occurred');
+		return res.status(500).render('error/unhandled-exception');
 	}
 
 	res.redirect(`/${VIEW.REGISTER.MYSELF.REGISTRATION_COMPLETE}`);
