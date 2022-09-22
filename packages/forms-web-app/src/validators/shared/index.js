d07911c8260e04309fbf5cfeca146a25a66b16f9
mauriciokin <mauricio.deoliveira@kinandcarta.com>
2022-09-22 18:02:32 +0100

    perf(refactor address validation to have 1 file instead of 3): refactor address validation to have 1
    refactor address validation to have 1 file instead of 3

--- packages/forms-web-app/src/validators/shared/index.js
+++ packages/forms-web-app/src/validators/shared/index.js
@@ -1,7 +1,9 @@
 const { validateNotEmpty } = require('./not-empty');
 const { validateNotEmptyAndLength } = require('./not-empty-and-length');
+const { addressValidationRules } = require('./address');
 
 module.exports = {
 	validateNotEmpty,
-	validateNotEmptyAndLength
+	validateNotEmptyAndLength,
+	addressValidationRules
 };
