dcb288b5b3fc77ba14a3ada8e7832981d102b459
James Paterson <james.paterson@kinandcarta.com>
2022-09-23 15:15:30 +0100

    chore: rename submission to submissions, add some more logic to dummy response

--- packages/applications-service-api/src/routes/submissions.js
+++ packages/applications-service-api/src/routes/submissions.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 
-const submissionController = require('../controllers/submission');
+const submissionsController = require('../controllers/submissions');
 const config = require('../lib/config');
 
 const router = express.Router();
 const upload = multer({ dest: config.uploads.path });
 
-router.post('/:caseRef', upload.single('file'), submissionController.createSubmission);
+router.post('/:caseRef', upload.single('file'), submissionsController.createSubmission);
 
 module.exports = router;
