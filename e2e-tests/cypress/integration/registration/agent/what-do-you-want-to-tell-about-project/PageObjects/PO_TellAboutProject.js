c9cc9c59b48218666ac291ed5b24210685e7813e
Philip Philippou <philip@philips-mbp-3.lan>
2022-09-23 09:07:02 +0100

    feat(tell-us-about-project.njk): changes to text around having your say instructions

--- e2e-tests/cypress/integration/registration/agent/what-do-you-want-to-tell-about-project/PageObjects/PO_TellAboutProject.js
+++ e2e-tests/cypress/integration/registration/agent/what-do-you-want-to-tell-about-project/PageObjects/PO_TellAboutProject.js
@@ -14,10 +14,7 @@ class PO_TellAboutProject extends PageObject {
 	}
 
 	assertDoNotIncludePersonalDetailsPresent() {
-		cy.get('.govuk-details__summary-text').should(
-			'contain.text',
-			'Do not include any personal details.'
-		);
+		cy.get('.govuk-details__summary-text').should('contain.text', 'Do not include these details.');
 	}
 }
 export default PO_TellAboutProject;
