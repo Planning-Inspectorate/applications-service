c1bb221b4e9faf2b4bdea51d1bd5f4ccaa79936c
Philip Philippou <philip@philips-mbp-3.lan>
2022-09-26 13:35:11 +0100

    fix(clickprojectlink.js): improve the fix to clickProjects link

--- e2e-tests/cypress/support/common-methods/clickProjectLink.js
+++ e2e-tests/cypress/support/common-methods/clickProjectLink.js
@@ -1,8 +1,3 @@
 module.exports = (projectName) => {
-	cy.get('td:nth-child(1)').each(($e1, index) => {
-		const text = $e1.text();
-		if (text.includes(projectName)) {
-			cy.get('td:nth-child(1)').eq(index).click(15, 20);
-		}
-	});
+	cy.get('td:nth-child(1)').contains(projectName).click();
 };
