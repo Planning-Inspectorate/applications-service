dd473e32a2d9c405b163f618db8beda73ece6b26
Philip Philippou <philip@philips-mbp-3.lan>
2022-09-26 10:20:05 +0100

    style(clickprojectlink.js): remove unecessary trim()

--- e2e-tests/cypress/support/common-methods/clickProjectLink.js
+++ e2e-tests/cypress/support/common-methods/clickProjectLink.js
@@ -1,6 +1,6 @@
 module.exports = (projectName) => {
 	cy.get('td:nth-child(1)').each(($e1, index) => {
-		const text = $e1.text().trim();
+		const text = $e1.text();
 		if (text.includes(projectName)) {
 			cy.get('td:nth-child(1)').eq(index).click(15, 20);
 		}
