51091f584953af28edd9283da6b5be818ef5f7e4
James Paterson <james.paterson@kinandcarta.com>
2022-10-05 12:58:25 +0100

    feat(api): integrate submission endpoint with ni

--- packages/applications-service-api/src/routes/submissions.js
+++ packages/applications-service-api/src/routes/submissions.js
@@ -2,11 +2,10 @@ const express = require('express');
 const multer = require('multer');
 
 const submissionsController = require('../controllers/submissions');
-const config = require('../lib/config');
 
 const router = express.Router();
-const upload = multer({ dest: config.uploads.path });
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/:caseRef', upload.single('file'), submissionsController.createSubmission);
+router.post('/:caseReference', upload.single('file'), submissionsController.createSubmission);
 
 module.exports = router;
