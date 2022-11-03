df33db5b31ca82835da98505a69d8f2ca477edf0
christopher-semmens <christopher.semmens@kinandcarta.com>
2022-11-03 14:35:40 +0000

    feat: add form sanitisation to register/myself/full-name

--- packages/forms-web-app/src/routes/register/myself/full-name.js
+++ packages/forms-web-app/src/routes/register/myself/full-name.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const decodeUri = require('../../../middleware/decode-uri');
 const fullNameController = require('../../../controllers/register/myself/full-name');
 const { validationErrorHandler } = require('../../../validators/validation-error-handler');
 const { rules: fullNameValidationRules } = require('../../../validators/shared/full-name');
@@ -10,6 +11,7 @@ router.get('/full-name', fullNameController.getFullName);
 
 router.post(
 	'/full-name',
+	decodeUri('body', ['full-name']),
 	fullNameValidationRules(),
 	validationErrorHandler,
 	fullNameController.postFullName
