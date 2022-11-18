8de65458ae97b5265489891e581430b5546871d4
Jonathan Goulding <jonathangoulding21@gmail.com>
2022-11-18 16:08:39 +0000

    fix(examination closing date logic): add conditions to render text for closing and open examinations dates

--- packages/forms-web-app/src/utils/get-now.js
+++
@@ -1,3 +0,0 @@
-const getNow = () => new Date();
-
-module.exports = { getNow };
--- packages/forms-web-app/src/utils/is-before-or-after-date.js
+++ packages/forms-web-app/src/utils/is-before-or-after-date.js
@@ -1,29 +1,50 @@
-const { getNow } = require('./get-now');
 const { formatDate } = require('./date-utils');
+const moment = require('moment');
 
 const isBeforeNowUTC = (date) => {
 	if (!date || typeof date !== 'string') return;
 
-	return getNow() < new Date(date);
+	return new Date() < new Date(date);
 };
 
-const isBeforeOrAfterDate = (date, optionsArray) => {
-	if (
-		!date ||
-		typeof date !== 'string' ||
-		!Array.isArray(optionsArray) ||
-		!optionsArray.length ||
-		optionsArray.length !== 2
-	)
-		return;
+const getConfirmedStartOfExamination = (date) => {
+	const formattedDate = formatDate(date);
+
+	if (!formattedDate || formattedDate === 'NaNvalid date') return;
+
+	const toHappen = `The examination opens on ${formattedDate}`;
+	const happened = `The examination opened on ${formattedDate}`;
+
+	return isBeforeNowUTC(date) ? toHappen : happened;
+};
+
+const handleGrantedExtension = (extensionCloseData) =>
+	`The deadline for the close of the Examination has been extended to ${formatDate(
+		extensionCloseData
+	)}`;
+
+const handleNoDatesExtensionAddSixMonthsToStart = (startDate) =>
+	`The examination is expected to close on ${formatDate(
+		moment(startDate).add(6, 'M').toISOString()
+	)}`;
+
+const getDateTimeExaminationEnds = (date, extensionCloseData, startDate) => {
+	if (!date && !extensionCloseData) return handleNoDatesExtensionAddSixMonthsToStart(startDate);
+
+	if (!date && extensionCloseData) return handleGrantedExtension(extensionCloseData);
 
 	const formattedDate = formatDate(date);
 
 	if (!formattedDate || formattedDate === 'NaNvalid date') return;
 
-	const [toHappen, happened] = optionsArray.map((str) => `${str} ${formattedDate}`);
+	const toHappen = `The examination is expected to close on ${formattedDate}`;
+	const happened = `The examination closed on ${formattedDate}`;
 
 	return isBeforeNowUTC(date) ? toHappen : happened;
 };
 
-module.exports = { isBeforeNowUTC, isBeforeOrAfterDate };
+module.exports = {
+	isBeforeNowUTC,
+	getConfirmedStartOfExamination,
+	getDateTimeExaminationEnds
+};
