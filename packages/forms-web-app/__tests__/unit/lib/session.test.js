2aa56c32419151293ff151ffa59a1966dbb5a0a2
James Paterson <james.paterson@kinandcarta.com>
2023-01-13 17:06:14 +0000

    feat(forms-web-app): redis cache config set up

--- packages/forms-web-app/__tests__/unit/lib/session.test.js
+++ packages/forms-web-app/__tests__/unit/lib/session.test.js
@@ -1,34 +1,82 @@
-const session = require('../../../src/lib/session');
-const config = require('../../../src/config');
+const { configureSessionStore } = require('../../../src/lib/session');
+
+const { createClient } = require('redis');
+const session = require('express-session');
 
 jest.mock('express-session');
-jest.mock('../../../src/lib/logger');
+jest.mock('redis');
+
+let config;
+
+const mockCreateClientOnFn = jest.fn();
+const mockCreateClientConnectFn = jest.fn();
 
 describe('lib/session', () => {
-	it('should throw if unable to find the session secret', () => {
-		expect(() => session()).toThrow('Session secret must be set');
-	});
+	describe('configureSessionStore', () => {
+		beforeEach(() => {
+			config = require('../../../src/config');
 
-	it('should configure with the expected config', () => {
-		config.server.sessionSecret = 'a fake session secret';
+			createClient.mockImplementation(() => ({
+				// eslint-disable-next-line no-unused-vars
+				on: (event, callback) => mockCreateClientOnFn(event, callback),
+				connect: () => mockCreateClientConnectFn()
+			}));
+		});
 
-		const configuredSession = session();
+		it('should throw if unable to find the session secret', () => {
+			expect(() => configureSessionStore(session)).toThrow('Session secret must be set');
+		});
 
-		expect(configuredSession.cookie).toEqual({});
-		expect(configuredSession.resave).toEqual(false);
-		expect(configuredSession.saveUninitialized).toEqual(true);
-		expect(configuredSession.secret).toEqual(config.server.sessionSecret);
-	});
+		it('should configure a redis store if useRedisSessionStore is true', () => {
+			config.server.sessionSecret = 'a fake session secret';
+			config.featureFlag.useRedisSessionStore = true;
+			config.db.session.redis = {
+				host: 'example.org',
+				port: 1234,
+				ssl: false,
+				password: 'some_password'
+			};
 
-	it('should configure with the expected config when useSecureSessionCookie', () => {
-		config.server.sessionSecret = 'a fake session secret';
-		config.server.useSecureSessionCookie = true;
+			configureSessionStore(session);
 
-		const configuredSession = session();
+			expect(createClient).toHaveBeenLastCalledWith({
+				socket: {
+					host: 'example.org',
+					port: 1234,
+					tls: false
+				},
+				password: 'some_password',
+				legacyMode: true
+			});
+			expect(mockCreateClientOnFn).toBeCalledWith('connect', expect.any(Function));
+			expect(mockCreateClientOnFn).toBeCalledWith('ready', expect.any(Function));
+			expect(mockCreateClientOnFn).toBeCalledWith('end', expect.any(Function));
+			expect(mockCreateClientOnFn).toBeCalledWith('error', expect.any(Function));
+			expect(mockCreateClientOnFn).toBeCalledWith('reconnecting', expect.any(Function));
+			expect(mockCreateClientConnectFn).toBeCalled();
+		});
 
-		expect(configuredSession.cookie.secure).toEqual(true);
-		expect(configuredSession.resave).toEqual(false);
-		expect(configuredSession.saveUninitialized).toEqual(true);
-		expect(configuredSession.secret).toEqual(config.server.sessionSecret);
+		it('should configure with the expected config', () => {
+			config.server.sessionSecret = 'a fake session secret';
+
+			const configuredSession = configureSessionStore(session);
+
+			expect(configuredSession.cookie.maxAge).toEqual(14400000);
+			expect(configuredSession.resave).toEqual(false);
+			expect(configuredSession.saveUninitialized).toEqual(true);
+			expect(configuredSession.secret).toEqual(config.server.sessionSecret);
+		});
+
+		it('should configure with the expected config when useSecureSessionCookie', () => {
+			config.server.sessionSecret = 'a fake session secret';
+			config.server.useSecureSessionCookie = true;
+
+			const configuredSession = configureSessionStore(session);
+
+			expect(configuredSession.cookie.secure).toEqual(true);
+			expect(configuredSession.resave).toEqual(false);
+			expect(configuredSession.saveUninitialized).toEqual(true);
+			expect(configuredSession.secret).toEqual(config.server.sessionSecret);
+		});
 	});
 });
