4e7baaf94faf9184e710528c1c4f704c1b23721a
christopher-semmens <christopher.semmens@kinandcarta.com>
2023-01-18 09:28:27 +0000

    feat: move examination library to top of documents page

--- packages/forms-web-app/__tests__/unit/controllers/projects/documents/documents/search-document.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/projects/documents/documents/search-document.test.js
@@ -3,100 +3,74 @@ const {
 } = require('../../../../../../src/controllers/projects/documents/utils/documents/searchDocuments');
 const { searchDocumentsV3 } = require('../../../../../../src/services/document.service');
 const {
+	searchExaminationLibraryDocument
+} = require('../../../../../../src/controllers/projects/documents/utils/documents/search-examination-library-document');
+const {
 	getBody
 } = require('../../../../../../src/controllers/projects/documents/utils/documents/body/getBody');
-const {
-	documentsWithExaminationLibraryAtTheTop
-} = require('../../../../../../src/controllers/projects/documents/utils/documents/documents-with-examination-library-at-the-top');
 
 jest.mock('../../../../../../src/services/document.service', () => ({
 	searchDocumentsV3: jest.fn()
 }));
-jest.mock(
-	'../../../../../../src/controllers/projects/documents/utils/documents/documents-with-examination-library-at-the-top',
-	() => ({
-		documentsWithExaminationLibraryAtTheTop: jest.fn()
-	})
-);
 jest.mock(
 	'../../../../../../src/controllers/projects/documents/utils/documents/body/getBody',
 	() => ({
 		getBody: jest.fn()
 	})
 );
+jest.mock(
+	'../../../../../../src/controllers/projects/documents/utils/documents/search-examination-library-document',
+	() => ({
+		searchExaminationLibraryDocument: jest.fn()
+	})
+);
+
 describe('#searchDocuments', () => {
 	describe('When searching for documents', () => {
-		describe('and it is the first page', () => {
-			const mockCaseRef = 'mock case ref';
-			const mockQuery = {};
-			let response;
-			beforeEach(async () => {
-				documentsWithExaminationLibraryAtTheTop.mockReturnValue([
-					'mock documents with examination'
-				]);
-				getBody.mockReturnValue({ text: 'mock body' });
-				searchDocumentsV3.mockReturnValue({
-					data: {
-						documents: ['mock search response'],
-						filters: ['mock filters'],
-						totalItems: '100',
-						itemsPerPage: '20',
-						totalPages: '5',
-						currentPage: '2'
-					}
-				});
-				response = await searchDocuments(mockCaseRef, mockQuery);
-			});
-
-			it('should call the examination library document mapper', () => {
-				expect(documentsWithExaminationLibraryAtTheTop).toHaveBeenCalledWith(
-					{ text: 'mock body' },
-					['mock search response']
-				);
-			});
-
-			it('should call the search documents service', () => {
-				expect(searchDocumentsV3).toHaveBeenCalledWith({ text: 'mock body' });
-			});
-			it('should return the response mapped to documents, filters and pagination', () => {
-				expect(response).toEqual({
-					documents: ['mock documents with examination'],
+		const mockCaseRef = 'mock case ref';
+		const mockQuery = {};
+		beforeEach(async () => {
+			getBody.mockReturnValue({ text: 'mock body' });
+			searchDocumentsV3.mockReturnValue({
+				data: {
+					documents: ['mock documents'],
 					filters: ['mock filters'],
-					pagination: {
-						totalItems: '100',
-						itemsPerPage: '20',
-						totalPages: '5',
-						currentPage: '2'
-					}
-				});
+					totalItems: '100',
+					itemsPerPage: '20',
+					totalPages: '5',
+					currentPage: '2'
+				}
 			});
 		});
-
-		describe('and it is not the first page', () => {
-			const mockCaseRef = 'mock case ref';
-			const mockQuery = { page: 2 };
+		describe('and there is no an examination library document', () => {
 			let response;
 			beforeEach(async () => {
-				getBody.mockReturnValue({ text: 'mock body' });
-				searchDocumentsV3.mockReturnValue({
-					data: {
-						documents: ['mock documents'],
-						filters: ['mock filters'],
-						totalItems: '100',
-						itemsPerPage: '20',
-						totalPages: '5',
-						currentPage: '2'
-					}
+				response = await searchDocuments(mockCaseRef, mockQuery);
+			});
+
+			it('should not get the examination library document html', () => {
+				expect(response.examinationLibraryDocument).not.toBeDefined();
+			});
+		});
+		describe('and there is an examination library document', () => {
+			let response;
+			beforeEach(async () => {
+				searchExaminationLibraryDocument.mockReturnValue({
+					name: 'mock examionation library document'
 				});
 				response = await searchDocuments(mockCaseRef, mockQuery);
 			});
 
 			it('should call the search documents service', () => {
 				expect(searchDocumentsV3).toHaveBeenCalledWith({ text: 'mock body' });
+			});
+			it('should call the examination library document', () => {
+				expect(searchExaminationLibraryDocument).toHaveBeenCalledWith({ text: 'mock body' });
 			});
 			it('should return the response mapped to documents, filters and pagination', () => {
 				expect(response).toEqual({
 					documents: ['mock documents'],
+					examinationLibraryDocument: { name: 'mock examionation library document' },
 					filters: ['mock filters'],
 					pagination: {
 						totalItems: '100',
--- packages/forms-web-app/src/controllers/projects/documents/utils/documents/getDocuments.js
+++ packages/forms-web-app/src/controllers/projects/documents/utils/documents/getDocuments.js
@@ -1,6 +1,10 @@
 const { mapDocumentsToViewModel } = require('./documents-view-model');
+const { getExaminationLibraryDocumentHtml } = require('./get-examination-library-document-html');
 
-const getDocuments = (documents) => mapDocumentsToViewModel(documents);
+const getDocuments = (documents, examinationLibraryDocument) => ({
+	documents: mapDocumentsToViewModel(documents),
+	examinationLibraryDocumentHtml: getExaminationLibraryDocumentHtml(examinationLibraryDocument)
+});
 
 module.exports = {
 	getDocuments
