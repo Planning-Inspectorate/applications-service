4fce8a76d9937efcc3cd6b1cf30ab309eea0a39b
David Beale <david.beale@kinandcarta.com>
2023-01-30 11:14:42 +0000

    chore: update existing tests

--- packages/forms-web-app/src/lib/base64.js
+++ packages/forms-web-app/src/lib/base64.js
@@ -1,8 +1,8 @@
 module.exports = {
-	toBase64(text) {
+	toBase64(text = '') {
 		return Buffer.from(text, 'utf-8').toString('base64');
 	},
-	fromBase64(base64text) {
+	fromBase64(base64text = '') {
 		return Buffer.from(base64text, 'base64').toString('utf-8');
 	}
 };
