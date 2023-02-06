d8fb7b87ec570030be35b6091c9aca7b421f9347
akan <akan_antia@live.co.uk>
2023-02-06 14:43:29 +0000

    test: addtional test

---
+++ e2e_scenario_tests/cypress/e2e/features/RegistrationPage/4_User-report-abuse.feature
@@ -0,0 +1,9 @@
+Feature: User reports an abuse
+    After user has completed the have your say journey and reports an abuse
+
+    Background: User start the registration process
+        Given I navigate to the registration start page
+
+    Scenario: User reports an abuse
+       And The user has completed the have your say journey
+       Then User reports an abuse
---
+++ e2e_scenario_tests/cypress/e2e/step_definitions/Register-to-have-your-say/TC_08_User-reports-an-abuse.cy.js
@@ -0,0 +1,35 @@
+import { Then, And } from 'cypress-cucumber-preprocessor/steps';
+import PO_HaveYourSay from '../../../pageObject/Register-to-have-your-say/PO_HaveYourSay';
+import PO_CheckBox from '../../../pageObject/Register-to-have-your-say/PO_CheckBox';
+const haveYourSay = new PO_HaveYourSay();
+const checkBox = new PO_CheckBox();
+
+And('The user has completed the have your say journey', () => {
+	checkBox.mySelfCheckBox(); //Who are you registering for? Myself
+	haveYourSay.continueButton();
+	haveYourSay.fullNameField().type('John Doe');
+	haveYourSay.continueButton();
+	checkBox.yesCheckBox(); //Are you 18 or over? Yes
+	haveYourSay.continueButton();
+	haveYourSay.emailField().type('john-doe@aol.co.uk');
+	haveYourSay.continueButton();
+	cy.addressFields(); // Address fields
+	haveYourSay.continueButton();
+	haveYourSay.phoneField().type('01265789456');
+	haveYourSay.continueButton();
+	haveYourSay.commentField().type('Lorem ipsum dolor sit amet, consectetur adipiscing elit');
+	haveYourSay.continueButton();
+	haveYourSay.acceptContinueBtn();
+	haveYourSay.acceptRegister();
+});
+
+Then('User reports an abuse', () => {
+	cy.get(':nth-child(13) > a').click();
+	cy.get('.office-form-notice-report').click();
+	cy.get('#Phishing').click();
+	cy.get('.office-form-reportabuse-input').type(
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+	);
+	cy.get('.button-content').click();
+	cy.get('.thank-you-page-container').should('be.visible', 'Your report was submitted.');
+});
