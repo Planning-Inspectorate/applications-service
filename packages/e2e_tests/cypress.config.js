dd6d0bb8151ca0f51fe89de0c9a0ffbb70a5aeff
akano-baba <akan_antia@live.co.uk>
2023-02-20 10:22:34 +0000

    test: resolve conflict

--- packages/e2e_tests/cypress.config.js
+++ packages/e2e_tests/cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
 		},
 
 		specPattern: 'cypress/e2e/**/*.feature',
-		baseUrl: 'https://applications-service-test.planninginspectorate.gov.uk',
+		baseUrl: 'http://localhost:9004',
 		experimentalRunAllSpecs: true,
 		chromeWebSecurity: false,
 		pageLoadTimeout: 20000,
