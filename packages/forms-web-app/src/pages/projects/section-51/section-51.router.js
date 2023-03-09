ba5a48ad4e69a131dc8d47ac7b1c66b46ff61cfb
Jonathan Goulding <jonathangoulding21@gmail.com>
2023-03-09 10:13:42 +0000

    feat(section 51): add section 51 initial page

--- packages/forms-web-app/src/pages/projects/section-51/section-51.router.js
+++ packages/forms-web-app/src/pages/projects/section-51/section-51.router.js
@@ -3,7 +3,6 @@ const { getSection51 } = require('./section-51.controller');
 const { verticalTabs } = require('../middleware');
 const router = express.Router();
 
-//  param here not being passed through when in top router
 router.get('/:case_ref/s51advice', verticalTabs, getSection51);
 
 module.exports = router;
