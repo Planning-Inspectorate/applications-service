03922c1fbdbfdaa9f076b6df6c114fe9cd0b08a9
David Beale <david.beale@kinandcarta.com>
2023-03-15 12:11:06 +0000

    feat: form post override

---
+++ packages/forms-web-app/__tests__/unit/scripts/sw.test.js
@@ -0,0 +1,87 @@
+/**
+ * @jest-environment jsdom
+ */
+const listeners = {};
+const mockAddEventListener = jest.fn().mockImplementation((event, func) => {
+	listeners[event] = func;
+});
+const mockSkipWaiting = jest.fn();
+Object.defineProperty(global, 'self', {
+	value: {
+		addEventListener: mockAddEventListener,
+		skipWaiting: mockSkipWaiting,
+		clients: {
+			claim: () => 'value'
+		},
+		location: 'http://example.com/example'
+	}
+});
+
+const mockFetch = jest.fn();
+Object.defineProperty(global, 'fetch', {
+	value: mockFetch
+});
+
+describe('scripts/sw', () => {
+	it('adds event listeners', async () => {
+		require('../../../src/public/scripts/sw.script.js');
+		await delay(100); // Give async code time to run
+
+		expect(mockAddEventListener).toHaveBeenNthCalledWith(1, 'install', listeners.install);
+		expect(mockAddEventListener).toHaveBeenNthCalledWith(2, 'active', listeners.active);
+		expect(mockAddEventListener).toHaveBeenNthCalledWith(3, 'fetch', listeners.fetch);
+	});
+
+	it('handles install event ', async () => {
+		require('../../../src/public/scripts/sw.script.js');
+		await delay(100); // Give async code time to run
+
+		listeners.install();
+		expect(mockSkipWaiting).toHaveBeenCalled();
+	});
+
+	it('handles active event ', async () => {
+		require('../../../src/public/scripts/sw.script.js');
+		await delay(100); // Give async code time to run
+
+		const mockWaitUntil = jest.fn();
+		listeners.active({
+			waitUntil: mockWaitUntil
+		});
+		expect(mockWaitUntil).toHaveBeenCalledWith('value');
+	});
+
+	it('handles fetch event for form post ', async () => {
+		require('../../../src/public/scripts/sw.script.js');
+		await delay(100); // Give async code time to run
+
+		const mockResponseWith = jest.fn();
+		listeners.fetch({
+			request: {
+				method: 'POST',
+				url: 'http://example.com/example',
+				headers: {
+					get: () => 'application/x-www-form-urlencoded'
+				},
+				clone: () => ({
+					url: 'http://example.com/example',
+					body: 'first=one&second=two'
+				})
+			},
+			respondWith: mockResponseWith
+		});
+		await delay(100); // Give async code time to run
+
+		expect(mockResponseWith).toBeCalled();
+		expect(mockFetch).toBeCalledWith('http://example.com/example', {
+			method: 'POST',
+			redirect: 'manual',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: '{"first":"one","second":"two"}'
+		});
+	});
+});
+
+const delay = (ms) => new Promise((r) => setTimeout(r, ms));
