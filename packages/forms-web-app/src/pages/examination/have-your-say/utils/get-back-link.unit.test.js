39f7a3a367bae95ac6121a53944c0ecb7dc75c9a
Jonathan Goulding <jonathangoulding21@gmail.com>
2023-03-27 15:13:22 +0100

    fix(examination): have your say back link

--- packages/forms-web-app/src/pages/examination/have-your-say/utils/get-back-link.unit.test.js
+++ packages/forms-web-app/src/pages/examination/have-your-say/utils/get-back-link.unit.test.js
@@ -2,15 +2,15 @@ const { getBackLink } = require('./get-back-link');
 
 describe('have your say #getBackLink', () => {
 	describe('When getting the back link for the have your say page', () => {
-		describe('and the user have come from the projects examination route', () => {
-			const response = getBackLink('mock case ref', '/projects');
-			it('should return the page data', () => {
-				expect(response).toEqual('/projects/mock case ref/examination-timetable');
+		describe('and the user have come from the server host', () => {
+			const response = getBackLink('mock case ref', `http://localhost:3000/own-path`);
+			it('should return the referrer link', () => {
+				expect(response).toEqual('http://localhost:3000/own-path');
 			});
 		});
-		describe('and the user have come from the projects examination route', () => {
+		describe('and the user has not come from the server host', () => {
 			const response = getBackLink('mock case ref', '');
-			it('should return the page data', () => {
+			it('should return empty', () => {
 				expect(response).toEqual('');
 			});
 		});
