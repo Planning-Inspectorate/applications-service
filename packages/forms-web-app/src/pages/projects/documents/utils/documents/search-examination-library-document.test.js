3ff4be381331d14edef6549ce7fa65b1bb660ebb
James Paterson <james.paterson@kinandcarta.com>
2023-07-04 12:34:57 +0100

    fix(forms-web-app): add additional filtering for exam library doc selection

--- packages/forms-web-app/src/pages/projects/documents/utils/documents/search-examination-library-document.test.js
+++ packages/forms-web-app/src/pages/projects/documents/utils/documents/search-examination-library-document.test.js
@@ -9,34 +9,55 @@ jest.mock('../../../../../services/document.service', () => ({
 describe('controllers/projects/documents/utils/documents/search-examination-library-document', () => {
 	describe('#searchExaminationLibraryDocument', () => {
 		describe('When searching for the examination library document', () => {
-			const body = { text: 'mock body' };
+			const mockBody = { text: 'mock body' };
 			describe('and there is an examination library document returned in an array', () => {
-				let result;
-				const mockBody = { ...body };
-				beforeEach(async () => {
-					searchDocumentsV3.mockReturnValue({
-						data: {
-							documents: [{ name: 'mock examination library document' }]
-						}
-					});
-					result = await searchExaminationLibraryDocument(mockBody);
+				const mockExamLibraryDocument = {
+					name: 'mock examination library document',
+					type: 'Examination Library'
+				};
+
+				searchDocumentsV3.mockReturnValueOnce({
+					data: {
+						documents: [
+							mockExamLibraryDocument,
+							{ name: 'some other examination library document' }
+						]
+					}
 				});
-				it('should return the examination library document object', () => {
-					expect(result).toEqual({ name: 'mock examination library document' });
+
+				it('should return the examination library document object', async () => {
+					const result = await searchExaminationLibraryDocument(mockBody);
+
+					expect(result).toEqual(mockExamLibraryDocument);
 				});
 			});
+
 			describe('and there is no an examination library document returned in an array', () => {
-				let result;
-				const mockBody = { ...body };
-				beforeEach(async () => {
-					searchDocumentsV3.mockReturnValue({
-						data: {
-							documents: []
-						}
-					});
-					result = await searchExaminationLibraryDocument(mockBody);
+				searchDocumentsV3.mockReturnValueOnce({
+					data: {
+						documents: []
+					}
 				});
-				it('should return undefined', () => {
+
+				it('should return undefined', async () => {
+					const result = await searchExaminationLibraryDocument(mockBody);
+
+					expect(result).not.toBeDefined();
+				});
+			});
+
+			describe('and there is no an document with type of examination library returned in an array', () => {
+				searchDocumentsV3.mockReturnValueOnce({
+					data: {
+						documents: [
+							{ name: 'some examination library document', type: 'not examination library' }
+						]
+					}
+				});
+
+				it('should return undefined', async () => {
+					const result = await searchExaminationLibraryDocument(mockBody);
+
 					expect(result).not.toBeDefined();
 				});
 			});
