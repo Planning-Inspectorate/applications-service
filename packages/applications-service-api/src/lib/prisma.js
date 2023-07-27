6189f18e1161d1accc858bc6113aa7833584e367
James Paterson <james.paterson@kinandcarta.com>
2023-07-27 14:38:16 +0100

    feat(applications-service-api): verbose query logging in dev

--- packages/applications-service-api/src/lib/prisma.js
+++ packages/applications-service-api/src/lib/prisma.js
@@ -1,16 +1,39 @@
 const { PrismaClient } = require('@prisma/client');
+const { isProduction } = require('./config');
+const logger = require('./logger');
 
 let prismaClientInstance;
 
 const createPrismaClient = () => {
+	const logOption = isProduction
+		? ['query', 'info', 'warn', 'error']
+		: [{ emit: 'event', level: 'query' }, 'info', 'warn', 'error'];
+
 	if (!prismaClientInstance) {
 		prismaClientInstance = new PrismaClient({
-			log: ['query', 'info', 'warn', 'error']
+			log: logOption
 		});
-
 	}
 
+	if (!isProduction) registerQueryLogger(prismaClientInstance);
+
 	return prismaClientInstance;
+};
+
+const registerQueryLogger = (prismaClient) => {
+	prismaClient.$on('query', async (e) => {
+		try {
+			const params = JSON.parse(e.params);
+			let sql = `${e.query}`;
+			params.forEach((param, index) => {
+				let value = typeof param === 'string' ? `'${param}'` : param;
+				sql = sql.replace(`@P${index + 1}`, value);
+			});
+			logger.info(sql);
+		} catch (e) {
+			logger.error(e);
+		}
+	});
 };
 
 module.exports = { prismaClient: createPrismaClient() };
