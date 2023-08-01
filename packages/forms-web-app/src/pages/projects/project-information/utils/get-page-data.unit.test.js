c4042543752b1099d1ef17f5d49b67e6b4559434
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-08-01 13:48:38 +0100

    feat(projects): address pr notes

--- packages/forms-web-app/src/pages/projects/project-information/utils/get-page-data.unit.test.js
+++ packages/forms-web-app/src/pages/projects/project-information/utils/get-page-data.unit.test.js
@@ -2,6 +2,16 @@ const { stripPrefixFromProposalType } = require('./get-page-data');
 
 describe('projects/project-information/utils/get-page-data', () => {
 	describe('#stripPrefixFromProposalType', () => {
+		it("should return unchanged string if prefix doesn't match the pattern", async () => {
+			const test1 = stripPrefixFromProposalType('TRA04 - Railways');
+			const test2 = stripPrefixFromProposalType('BC033 - An Industrial Process or Processes');
+			const test3 = stripPrefixFromProposalType('EN06 Gas Transporter Pipe-lines');
+
+			expect(test1).toBe('TRA04 - Railways');
+			expect(test2).toBe('BC033 - An Industrial Process or Processes');
+			expect(test3).toBe('EN06 Gas Transporter Pipe-lines');
+		});
+
 		it('should remove the case prefix from proposal string', async () => {
 			const test1 = stripPrefixFromProposalType('TR04 - Railways');
 			const test2 = stripPrefixFromProposalType('BC03 - An Industrial Process or Processes');
