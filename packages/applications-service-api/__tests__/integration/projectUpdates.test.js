d79004ac63cdacbb4646e0ae4f31d069dbc2749e
James Paterson <james.paterson@kinandcarta.com>
2023-08-07 12:31:37 +0100

    fix(applications-service-api): return 200 when no project updates for case

--- packages/applications-service-api/__tests__/integration/projectUpdates.test.js
+++ packages/applications-service-api/__tests__/integration/projectUpdates.test.js
@@ -2,21 +2,16 @@ const { request } = require('../__data__/supertest');
 const { PROJECT_UPDATE_DB, PROJECT_UPDATE_RESPONSE } = require('../__data__/projectUpdates');
 
 const mockProjectUpdateFindMany = jest.fn();
-const mockProjectFindUnique = jest.fn();
 jest.mock('../../src/lib/prisma', () => ({
 	prismaClient: {
 		projectUpdate: {
 			findMany: (query) => mockProjectUpdateFindMany(query)
-		},
-		project: {
-			findUnique: (query) => mockProjectFindUnique(query)
 		}
 	}
 }));
 
 describe('/api/v1/project-updates/{{caseReference}}', () => {
 	it('returns project updates when case has some', async () => {
-		mockProjectFindUnique.mockResolvedValueOnce({ id: 1 });
 		mockProjectUpdateFindMany.mockResolvedValueOnce([PROJECT_UPDATE_DB]);
 
 		const response = await request.get('/api/v1/project-updates/BC0110001');
@@ -26,7 +21,6 @@ describe('/api/v1/project-updates/{{caseReference}}', () => {
 	});
 
 	it('returns no project updates when case has none', async () => {
-		mockProjectFindUnique.mockResolvedValueOnce({ id: 1 });
 		mockProjectUpdateFindMany.mockResolvedValueOnce([]);
 
 		const response = await request.get('/api/v1/project-updates/BC0110001');
@@ -41,17 +35,7 @@ describe('/api/v1/project-updates/{{caseReference}}', () => {
 		expect(response.status).toEqual(400);
 	});
 
-	it('returns 404 when case not found', async () => {
-		mockProjectFindUnique.mockResolvedValueOnce(null);
-
-		const response = await request.get('/api/v1/project-updates/BC0110001');
-
-		expect(response.status).toEqual(404);
-	});
-
 	it('returns 500 when unhandled error occurs', async () => {
-		mockProjectFindUnique.mockRejectedValueOnce(new Error('some error'));
-
 		const response = await request.get('/api/v1/project-updates/BC0110001');
 
 		expect(response.status).toEqual(500);
