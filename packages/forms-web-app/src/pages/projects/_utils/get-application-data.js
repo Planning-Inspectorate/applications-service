d101f1333dd41c7b428213e77e80ede9135f4e4c
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-08-09 12:15:29 +0100

    feat(project-information): add pre-application and pre-acceptance stages

---
+++ packages/forms-web-app/src/pages/projects/_utils/get-application-data.js
@@ -0,0 +1,33 @@
+const { getAppData } = require('../../../services/application.service');
+const { projectInfoProjectStages } = require('../../../utils/project-stages');
+const dayjs = require('dayjs');
+const getApplicationData = async (case_ref) => {
+	const { data, resp_code } = await getAppData(case_ref);
+	if (resp_code !== 200) throw new Error('Application response status not 200');
+
+	const status = {
+		number: data.Stage,
+		text: projectInfoProjectStages[data.Stage]
+	};
+
+	const DateOfDCOSubmission = data.DateOfDCOSubmission
+		? dayjs(data.DateOfDCOSubmission).add(28, 'days').toISOString()
+		: null;
+
+	return {
+		projectName: data.ProjectName,
+		caseRef: data.CaseReference,
+		proposal: data.Proposal,
+		summary: data.Summary,
+		webAddress: data.WebAddress,
+		dateOfNonAcceptance: data.dateOfNonAcceptance,
+		status,
+		anticipatedDateOfSubmission: data.AnticipatedDateOfSubmission,
+		contactEmailAddress: data.ProjectEmailAddress,
+		DateOfDCOSubmission
+	};
+};
+
+module.exports = {
+	getApplicationData
+};
