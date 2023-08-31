b2d95c126db9646787da2fe0190dde6279283f92
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-08-31 11:52:06 +0100

    feat: get applications api sorting

--- packages/applications-service-api/src/services/application.service.js
+++ packages/applications-service-api/src/services/application.service.js
@@ -4,14 +4,43 @@ const getApplication = async (id) => {
 	return await db.Project.findOne({ where: { CaseReference: id } });
 };
 
-const getAllApplications = async (query) => {
+const createQueryFilters = (query) => {
+	// Pagination
 	const pageNo = parseInt(query?.page) || 1;
-	const size = Math.min(parseInt(query?.size) || 25, 100);
+	const defaultSize = 25;
+	const maxSize = 100;
+	const size = Math.min(parseInt(query?.size) || defaultSize, maxSize);
+
+	// Sorting
+	const allowedSortFieldsWithDirection = [
+		'ProjectName',
+		'PromoterName',
+		'DateOfDCOSubmission',
+		'ConfirmedDateOfDecision',
+		'Stage'
+	].flatMap((field) => [field, `+${field}`, `-${field}`]);
+
+	const defaultSort = '+ProjectName';
+	const sort = allowedSortFieldsWithDirection.includes(query?.sort) ? query?.sort : defaultSort;
+	const sortDirection = sort?.startsWith('-') ? 'DESC' : 'ASC';
+	const sortFieldName = sort?.replace(/^[+-]/, '');
+
+	return {
+		pageNo,
+		size,
+		offset: size * (pageNo - 1),
+		order: [[sortFieldName, sortDirection]]
+	};
+};
+
+const getAllApplications = async (query) => {
+	const { pageNo, size, offset, order } = createQueryFilters(query);
 
 	const count = await db.Project.count();
 	const applications = await db.Project.findAll({
-		offset: size * (pageNo - 1),
-		limit: size
+		offset,
+		limit: size,
+		order
 	});
 
 	return {
