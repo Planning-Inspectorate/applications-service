9cf5e0ef4ebde0039eddc324499206696cbf3a78
James Paterson <james.paterson@kinandcarta.com>
2023-09-05 16:52:30 +0100

    fix(applications-service-api): remove default lng/lat value for project

--- packages/applications-service-api/src/utils/add-map-zoom-and-longlat.js
+++ packages/applications-service-api/src/utils/add-map-zoom-and-longlat.js
@@ -1,23 +1,24 @@
 const addMapZoomLvlAndLongLat = (document) => {
 	const area = ['COUNTRY', 'REGION', 'COUNTY', 'BOROUGH', 'DISTRICT', 'CITY', 'TOWN', 'JUNCTION'];
-	const MAPZOOMLVL_OFFSET = 5;
-	const DEFAULT_MAPZOOMLVL = 9;
-	const DEFAULT_LONGLAT = ['53.8033666', '-2.7044637'];
-	const mapZoomLevel = document.MapZoomLevel ? document.MapZoomLevel : 'COUNTRY';
-	let LongLat = [...DEFAULT_LONGLAT];
+	const ZOOM_LEVEL_OFFSET = 5;
+
+	const zoomLevelArea = document.MapZoomLevel || 'COUNTRY';
+	const MapZoomLevel = ZOOM_LEVEL_OFFSET + area.indexOf(zoomLevelArea.toUpperCase());
+
+	let LongLat;
 	if (document.LatLong) {
-		const latLong = document.LatLong.split(',');
+		const latLong = document.LatLong.split(',').map((s) => s.trim());
 		LongLat = [latLong[1], latLong[0]];
 	}
 
 	const application = {
 		...document,
-		MapZoomLevel: mapZoomLevel
-			? MAPZOOMLVL_OFFSET + area.indexOf(mapZoomLevel.toUpperCase())
-			: MAPZOOMLVL_OFFSET + DEFAULT_MAPZOOMLVL,
+		MapZoomLevel,
 		LongLat
 	};
+
 	delete application.LatLong;
+
 	return application;
 };
 
