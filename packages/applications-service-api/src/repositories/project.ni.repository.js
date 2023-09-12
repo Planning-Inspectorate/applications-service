6afd0636bdd8e16826a3122df79ab64daea3295b
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-09-12 11:58:57 +0100

    feat: added searching on applications API endpoint

--- packages/applications-service-api/src/repositories/project.ni.repository.js
+++ packages/applications-service-api/src/repositories/project.ni.repository.js
@@ -44,21 +44,34 @@ const GET_ALL_APPLICATIONS_DEFAULT_ATTRIBUTES = [
 const getApplication = async (id) => db.Project.findOne({ where: { CaseReference: id } });
 
 const getAllApplications = async (options) => {
-	const { filters } = options;
+	const { filters, searchTerm } = options;
+	let findAllOptions = pick(options, ['offset', 'limit', 'order']);
+	findAllOptions.attributes = options.attributes || GET_ALL_APPLICATIONS_DEFAULT_ATTRIBUTES;
+	findAllOptions.where = {};
 
-	const statements = [];
-	if (filters?.region) statements.push({ Region: { [Op.in]: filters.region } });
-	if (filters?.stage) statements.push({ Stage: { [Op.in]: filters.stage } });
+	// filters
+	const filterStatements = [];
+	if (filters?.region) filterStatements.push({ Region: { [Op.in]: filters.region } });
+	if (filters?.stage) filterStatements.push({ Stage: { [Op.in]: filters.stage } });
 	if (filters?.sector) {
 		const sectorStatements = filters.sector.map((sector) => ({
 			Proposal: { [Op.like]: `${sector}%` }
 		}));
-		statements.push({ [Op.or]: sectorStatements });
+		filterStatements.push({ [Op.or]: sectorStatements });
 	}
 
-	let findAllOptions = pick(options, ['offset', 'limit', 'order']);
-	if (statements.length > 0) findAllOptions.where = { [Op.and]: statements };
-	findAllOptions.attributes = options.attributes || GET_ALL_APPLICATIONS_DEFAULT_ATTRIBUTES;
+	// search
+	const searchTermStatements = [];
+	if (searchTerm) {
+		searchTermStatements.push({ ProjectName: { [Op.like]: `%${searchTerm}%` } });
+		searchTermStatements.push({ PromoterName: { [Op.like]: `%${searchTerm}%` } });
+	}
+
+	// build where clause
+	if (filterStatements.length > 0)
+		findAllOptions.where = { ...findAllOptions.where, [Op.and]: filterStatements };
+	if (searchTermStatements.length > 0)
+		findAllOptions.where = { ...findAllOptions.where, [Op.or]: searchTermStatements };
 
 	return db.Project.findAll({
 		...findAllOptions,
