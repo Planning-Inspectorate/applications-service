23aefcd53004ab1da9ca528db008cbeb8070d3a9
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-09-18 16:17:06 +0100

    feat(examination/submitting-for): update template
    render hint text if there is no interestedPartyNumber present in the session data

--- packages/forms-web-app/src/pages/examination/submitting-for/utils/get-page-data.js
+++ packages/forms-web-app/src/pages/examination/submitting-for/utils/get-page-data.js
@@ -1,5 +1,7 @@
 const { getBackLinkUrl } = require('./get-back-link-url');
 const { getSubmittingForOptions } = require('./get-submitting-for-options');
+const { getDeadlineDetailsInterestedPartyNumberOrDefault } = require('../../_session/deadline');
+
 const {
 	routesConfig: {
 		examination: {
@@ -11,6 +13,7 @@ const {
 const getPageData = (query, session) => ({
 	backLinkUrl: getBackLinkUrl(query, session),
 	id: submittingFor.id,
+	interestedPartyNumber: getDeadlineDetailsInterestedPartyNumberOrDefault(session),
 	options: getSubmittingForOptions(session),
 	pageTitle: submittingFor.name,
 	title: submittingFor.name
