dadcb65f80fe34fa93f38671d73a0b5e12ea200c
James Paterson <james.paterson@kinandcarta.com>
2023-09-27 19:04:44 +0100

    fix(back-office-subscribers): nsip-project property updates and positive selection

--- packages/back-office-subscribers/nsip-project/index.js
+++ packages/back-office-subscribers/nsip-project/index.js
@@ -1,32 +1,70 @@
-const excludedProperties = [
-	'operationsLeadId',
-	'operationsManagerId',
-	'caseManagerId',
-	'nsipOfficerIds',
-	'nsipAdministrationOfficerIds',
-	'leadInspectorId',
-	'inspectorIds',
-	'environmentalServicesOfficerId',
-	'legalOfficerId',
-	'applicantIds',
-	'interestedPartyIds'
-];
+const pick = require('lodash.pick');
 
 module.exports = async (context, message) => {
 	context.log(`invoking nsip-project function with message: ${JSON.stringify(message)}`);
-	context.bindings.project = parseMessage(message);
-};
 
-const parseMessage = (message) => {
-	let output = {
+	const project = pick(message, [
+		'caseId',
+		'caseReference',
+		'projectName',
+		'projectDescription',
+		'publishStatus',
+		'sector',
+		'projectType',
+		'sourceSystem',
+		'stage',
+		'projectLocation',
+		'projectEmailAddress',
+		'regions',
+		'transboundary',
+		'easting',
+		'northing',
+		'welshLanguage',
+		'mapZoomLevel',
+		'secretaryOfState',
+		'dateProjectAppearsOnWebsite',
+		'dateOfDCOAcceptance',
+		'dateOfNonAcceptance',
+		'dateIAPIDue',
+		'notificationDateForEventsDeveloper',
+		'notificationDateForPMAndEventsDirectlyFollowingPM',
+		'rule6LetterPublishDate',
+		'rule8LetterPublishDate',
+		'anticipatedDateOfSubmission',
+		'anticipatedSubmissionDateNonSpecific',
+		'dateOfDCOSubmission',
+		'dateOfRepresentationPeriodOpen',
+		'dateOfRelevantRepresentationClose',
+		'dateRRepAppearOnWebsite',
+		'confirmedStartOfExamination',
+		'dateTimeExaminationEnds',
+		'stage4ExtensionToExamCloseDate',
+		'stage5ExtensionToRecommendationDeadline',
+		'dateOfRecommendations',
+		'confirmedDateOfDecision',
+		'stage5ExtensionToDecisionDeadline',
+		'dateProjectWithdrawn',
+		'section46Notification',
+		'datePINSFirstNotifiedOfProject',
+		'screeningOpinionSought',
+		'screeningOpinionIssued',
+		'scopingOpinionSought',
+		'scopingOpinionIssued',
+		'deadlineForAcceptanceDecision',
+		'dateSection58NoticeReceived',
+		'preliminaryMeetingStartDate',
+		'deadlineForCloseOfExamination',
+		'deadlineForSubmissionOfRecommendation',
+		'deadlineForDecision',
+		'jRPeriodEndDate',
+		'extensionToDateRelevantRepresentationsClose',
+		'examinationTimetableId'
+	]);
+
+	context.bindings.project = {
+		...project,
+		// TODO: store regions in separate table
+		regions: Array.isArray(project.regions) ? JSON.stringify(project.regions) : null,
 		modifiedAt: new Date()
 	};
-
-	for (const [key, value] of Object.entries(message)) {
-		if (!excludedProperties.includes(key)) {
-			output[key] = Array.isArray(value) ? value.join(',') : value;
-		}
-	}
-
-	return output;
 };
