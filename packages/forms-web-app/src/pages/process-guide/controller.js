bf47ac9324d32fe915f1eb2075a8b68dda0a6162
christopher-semmens <christopher.semmens@kinandcarta.com>
2023-10-09 17:28:37 +0100

    fix: process guide issues

--- packages/forms-web-app/src/pages/process-guide/controller.js
+++ packages/forms-web-app/src/pages/process-guide/controller.js
@@ -2,6 +2,10 @@ const { getPageData } = require('./_utils/get-page-data');
 
 const view = 'process-guide/view.njk';
 
-const getProcessGuideController = (req, res) => res.render(view, getPageData());
+const getProcessGuideController = (req, res) => {
+	const { session } = req;
+	const refUrl = req.get('Referrer');
+	return res.render(view, getPageData(refUrl, session));
+};
 
 module.exports = { getProcessGuideController };
