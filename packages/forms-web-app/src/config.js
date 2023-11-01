11f1461fed715a881a6d51fe558ca8dc9bedfb64
James Paterson <james.paterson@kinandcarta.com>
2023-11-01 16:55:49 +0000

    feat: add app insights sdk and feature toggle

--- packages/forms-web-app/src/config.js
+++ packages/forms-web-app/src/config.js
@@ -60,7 +60,9 @@ module.exports = {
 		projectMigrationCaseReferences: splitStringToArray(
 			process.env.PROJECT_MIGRATION_CASE_REFERENCES
 		),
-		generalisedFormSanitisation: process.env.FEATURE_ENABLE_GENERALISED_FORM_SANITISATION === 'true'
+		generalisedFormSanitisation:
+			process.env.FEATURE_ENABLE_GENERALISED_FORM_SANITISATION === 'true',
+		useApplicationInsights: process.env.FEATURE_APPLICATION_INSIGHTS === 'true'
 	},
 	featureHideLink: {
 		hideAllExaminationDocumentsLink: true
