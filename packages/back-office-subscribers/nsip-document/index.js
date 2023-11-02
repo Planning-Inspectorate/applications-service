fa78e49ce53cc4845dd0c184e58a3914990efee0
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-11-02 15:24:13 +0000

    feat: bo func docs wip

--- packages/back-office-subscribers/nsip-document/index.js
+++ packages/back-office-subscribers/nsip-document/index.js
@@ -1,45 +1,77 @@
 const pick = require('lodash.pick');
+const { prismaClient } = require('../lib/prisma');
 
 module.exports = async (context, message) => {
-	context.log(`invoking nsip-document function with message: ${JSON.stringify(message)}`);
+	context.log(`invoking nsip-document function`);
+	const documentId = message.documentId;
 
-	const document = pick(message, [
-		'documentId',
-		'caseId',
-		'caseRef',
-		'documentReference',
-		'version',
-		'examinationRefNo',
-		'filename',
-		'originalFilename',
-		'size',
-		'mime',
-		'documentURI',
-		'publishedDocumentURI',
-		'virusCheckStatus',
-		'fileMD5',
-		'dateCreated',
-		'lastModified',
-		'caseType',
-		'documentStatus',
-		'redactedStatus',
-		'publishedStatus',
-		'datePublished',
-		'documentType',
-		'securityClassification',
-		'sourceSystem',
-		'origin',
-		'owner',
-		'author',
-		'representative',
-		'description',
-		'stage',
-		'filter1',
-		'filter2'
-	]);
+	if (!documentId) {
+		context.log(`skipping update as documentId is missing`);
+		return;
+	}
 
-	context.bindings.document = {
-		...document,
-		modifiedAt: new Date()
-	};
+	return await prismaClient.$transaction(async (tx) => {
+		const existingDocument = await tx.document.findUnique({
+			where: {
+				documentId
+			}
+		});
+
+		const shouldUpdate =
+			!existingDocument ||
+			new Date(context.bindingData.enqueuedTimeUtc) >
+				new Date(existingDocument.modifiedAt.toUTCString());
+
+		if (shouldUpdate) {
+			const document = {
+				...pick(message, documentPropertiesFromMessage),
+				modifiedAt: new Date()
+			};
+			await tx.document.upsert({
+				where: {
+					documentId
+				},
+				update: document,
+				create: document
+			});
+			context.log(`upserted document with documentId: ${documentId}`);
+		} else {
+			context.log(`skipping update of document with documentId: ${documentId}`);
+		}
+	});
 };
+
+const documentPropertiesFromMessage = [
+	'documentId',
+	'caseId',
+	'caseRef',
+	'documentReference',
+	'version',
+	'examinationRefNo',
+	'filename',
+	'originalFilename',
+	'size',
+	'mime',
+	'documentURI',
+	'publishedDocumentURI',
+	'virusCheckStatus',
+	'fileMD5',
+	'dateCreated',
+	'lastModified',
+	'caseType',
+	'documentStatus',
+	'redactedStatus',
+	'publishedStatus',
+	'datePublished',
+	'documentType',
+	'securityClassification',
+	'sourceSystem',
+	'origin',
+	'owner',
+	'author',
+	'representative',
+	'description',
+	'stage',
+	'filter1',
+	'filter2'
+];
