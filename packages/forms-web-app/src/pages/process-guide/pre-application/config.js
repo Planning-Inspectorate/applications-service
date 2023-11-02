4e00a7f710b5d002ac78a04b29510a0a99518a75
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-11-02 09:01:24 +0000

    fix(web/process-guide): update pre-application process guide text

--- packages/forms-web-app/src/pages/process-guide/pre-application/config.js
+++ packages/forms-web-app/src/pages/process-guide/pre-application/config.js
@@ -8,7 +8,7 @@ const preApplicationTitle = 'Pre-application';
 
 const preApplicationContent = [
 	'This is where the applicant starts to create their application. The applicant is required to run a consultation and engage with people and organisations in the area. They must also create detailed documents about the impact the project could have on the environment.',
-	'You can get involved at this stage to influence the application before the applicant sends it to the Planning Inspectorate.'
+	'It is important to get involved at this stage to influence the application before the applicant sends it to the Planning Inspectorate.'
 ];
 
 const preApplicationLinkText =
