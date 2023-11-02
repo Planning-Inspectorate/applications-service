efc91632593c564dd638a89c62ebf2cd5b416aba
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-11-02 11:04:09 +0000

    feat: back-office project subscriber function with prisma

--- packages/back-office-subscribers/nsip-project/index.js
+++ packages/back-office-subscribers/nsip-project/index.js
@@ -1,70 +1,102 @@
 const pick = require('lodash.pick');
+const { prismaClient } = require('../lib/prisma');
 
 module.exports = async (context, message) => {
-	context.log(`invoking nsip-project function with message: ${JSON.stringify(message)}`);
+	context.log(`invoking nsip-project function`);
+	const caseReference = message.caseReference;
 
-	const project = pick(message, [
-		'caseId',
-		'caseReference',
-		'projectName',
-		'projectDescription',
-		'publishStatus',
-		'sector',
-		'projectType',
-		'sourceSystem',
-		'stage',
-		'projectLocation',
-		'projectEmailAddress',
-		'regions',
-		'transboundary',
-		'easting',
-		'northing',
-		'welshLanguage',
-		'mapZoomLevel',
-		'secretaryOfState',
-		'dateProjectAppearsOnWebsite',
-		'dateOfDCOAcceptance',
-		'dateOfNonAcceptance',
-		'dateIAPIDue',
-		'notificationDateForEventsDeveloper',
-		'notificationDateForPMAndEventsDirectlyFollowingPM',
-		'rule6LetterPublishDate',
-		'rule8LetterPublishDate',
-		'anticipatedDateOfSubmission',
-		'anticipatedSubmissionDateNonSpecific',
-		'dateOfDCOSubmission',
-		'dateOfRepresentationPeriodOpen',
-		'dateOfRelevantRepresentationClose',
-		'dateRRepAppearOnWebsite',
-		'confirmedStartOfExamination',
-		'dateTimeExaminationEnds',
-		'stage4ExtensionToExamCloseDate',
-		'stage5ExtensionToRecommendationDeadline',
-		'dateOfRecommendations',
-		'confirmedDateOfDecision',
-		'stage5ExtensionToDecisionDeadline',
-		'dateProjectWithdrawn',
-		'section46Notification',
-		'datePINSFirstNotifiedOfProject',
-		'screeningOpinionSought',
-		'screeningOpinionIssued',
-		'scopingOpinionSought',
-		'scopingOpinionIssued',
-		'deadlineForAcceptanceDecision',
-		'dateSection58NoticeReceived',
-		'preliminaryMeetingStartDate',
-		'deadlineForCloseOfExamination',
-		'deadlineForSubmissionOfRecommendation',
-		'deadlineForDecision',
-		'jRPeriodEndDate',
-		'extensionToDateRelevantRepresentationsClose',
-		'examinationTimetableId'
-	]);
+	if (!caseReference) {
+		context.log(`skipping update of events as caseReference is missing`);
+		return;
+	}
 
-	context.bindings.project = {
-		...project,
-		// TODO: store regions in separate table
-		regions: Array.isArray(project.regions) ? JSON.stringify(project.regions) : null,
-		modifiedAt: new Date()
-	};
+	return await prismaClient.$transaction(async (tx) => {
+		const existingProject = await tx.project.findUnique({
+			where: {
+				caseReference
+			}
+		});
+
+		const shouldUpdate =
+			!existingProject ||
+			new Date(context.bindingData.enqueuedTimeUtc) >
+				new Date(existingProject.modifiedAt.toUTCString());
+
+		if (shouldUpdate) {
+			let project = pick(message, projectPropertiesFromMessage);
+			project = {
+				...project,
+				regions: Array.isArray(project.regions) ? JSON.stringify(project.regions) : null,
+				modifiedAt: new Date()
+			};
+			await tx.project.upsert({
+				where: {
+					caseReference
+				},
+				update: project,
+				create: project
+			});
+			context.log(`upserted project with caseReference: ${caseReference}`);
+		} else {
+			context.log(`skipping update of project with caseReference: ${caseReference}`);
+		}
+	});
 };
+
+const projectPropertiesFromMessage = [
+	'caseId',
+	'caseReference',
+	'projectName',
+	'projectDescription',
+	'publishStatus',
+	'sector',
+	'projectType',
+	'sourceSystem',
+	'stage',
+	'projectLocation',
+	'projectEmailAddress',
+	'regions',
+	'transboundary',
+	'easting',
+	'northing',
+	'welshLanguage',
+	'mapZoomLevel',
+	'secretaryOfState',
+	'dateProjectAppearsOnWebsite',
+	'dateOfDCOAcceptance',
+	'dateOfNonAcceptance',
+	'dateIAPIDue',
+	'notificationDateForEventsDeveloper',
+	'notificationDateForPMAndEventsDirectlyFollowingPM',
+	'rule6LetterPublishDate',
+	'rule8LetterPublishDate',
+	'anticipatedDateOfSubmission',
+	'anticipatedSubmissionDateNonSpecific',
+	'dateOfDCOSubmission',
+	'dateOfRepresentationPeriodOpen',
+	'dateOfRelevantRepresentationClose',
+	'dateRRepAppearOnWebsite',
+	'confirmedStartOfExamination',
+	'dateTimeExaminationEnds',
+	'stage4ExtensionToExamCloseDate',
+	'stage5ExtensionToRecommendationDeadline',
+	'dateOfRecommendations',
+	'confirmedDateOfDecision',
+	'stage5ExtensionToDecisionDeadline',
+	'dateProjectWithdrawn',
+	'section46Notification',
+	'datePINSFirstNotifiedOfProject',
+	'screeningOpinionSought',
+	'screeningOpinionIssued',
+	'scopingOpinionSought',
+	'scopingOpinionIssued',
+	'deadlineForAcceptanceDecision',
+	'dateSection58NoticeReceived',
+	'preliminaryMeetingStartDate',
+	'deadlineForCloseOfExamination',
+	'deadlineForSubmissionOfRecommendation',
+	'deadlineForDecision',
+	'jRPeriodEndDate',
+	'extensionToDateRelevantRepresentationsClose',
+	'examinationTimetableId'
+];
