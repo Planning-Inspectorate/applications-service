3b239c28b90f3aa3733aa322e2940dccb72ff587
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-11-17 18:13:09 +0000

    refactor: get updates subscribed page to use new route configuration

---
+++ packages/forms-web-app/src/pages/projects/get-updates/subscribed/config.js
@@ -0,0 +1,5 @@
+const getSubscribedRoute = 'subscribed';
+
+module.exports = {
+	getSubscribedRoute
+};
--- packages/forms-web-app/src/pages/projects/get-updates/subscribed/utils/get-titles.js
+++ packages/forms-web-app/src/pages/projects/get-updates/subscribed/utils/get-titles.js
@@ -1,7 +1,7 @@
 const { mapTitles } = require('../../../../_utils/map-titles');
 
 const titles = {
-	204: mapTitles('Get updates success', 'Get updates success'),
+	204: mapTitles('Get updates success'),
 	400: mapTitles('Your email verification link has expired', 'Verification link expired'),
 	500: mapTitles('There has been a problem with our system', 'Email system problem')
 };
