e5fc6c97205e49bfedf68ffde76520611015c999
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-11-20 16:40:53 +0000

    feat: extended advice route for BO + refactor

---
+++ packages/applications-service-api/src/repositories/advice.backoffice.repository.js
@@ -0,0 +1,45 @@
+const { prismaClient } = require('../lib/prisma');
+const getAllAdviceByCaseReference = async (caseReference, offset, size, searchTerm) => {
+	const where = {
+		AND: [
+			{
+				caseReference
+			},
+			{
+				...(searchTerm ? mapSearchTermToQuery(searchTerm) : {})
+			}
+		]
+	};
+
+	const dbQuery = {
+		where,
+		orderBy: [
+			{
+				adviceDate: 'desc'
+			},
+			{
+				adviceId: 'asc'
+			}
+		],
+		skip: offset,
+		take: size
+	};
+
+	const advice = await prismaClient.advice.findMany(dbQuery);
+	const count = await prismaClient.advice.count({ where });
+	console.log({ count, advice });
+	return { count, advice };
+};
+
+const mapSearchTermToQuery = (searchTerm) => {
+	if (searchTerm) {
+		const searchStatements = ['from', 'agent', 'enquiryDetails', 'adviceDetails'].map((field) => ({
+			[field]: { contains: searchTerm }
+		}));
+		return { OR: searchStatements };
+	}
+};
+
+module.exports = {
+	getAllAdviceByCaseReference
+};
--- packages/applications-service-api/src/services/advice.service.js
+++ packages/applications-service-api/src/services/advice.service.js
@@ -1,83 +1,81 @@
-const { Op } = require('sequelize');
-
+const config = require('../lib/config');
 const db = require('../models');
+const {
+	getAllAdviceByCaseReference: getAllBackOfficeAdvice
+} = require('../repositories/advice.backoffice.repository');
+const {
+	getAllAdviceByCaseReference: getAllNIAdvice
+} = require('../repositories/advice.ni.repository');
+const { mapBackOfficeAdviceToApi } = require('../utils/advice.mapper');
 
-module.exports = {
-	async getAdvice(requestQuery) {
-		const itemsPerPage = requestQuery.itemsPerPage;
+const isBackOfficeCaseReference = (caseReference) =>
+	(config.backOfficeIntegration.advice.getAdvice.caseReferences || []).includes(caseReference);
 
-		const where = {
-			[Op.and]: [
-				{
-					...(requestQuery.caseReference ? { caseReference: requestQuery.caseReference } : {})
-				},
-				{
-					...(requestQuery.searchTerm ? mapSearchTermToQuery(requestQuery.searchTerm) : {})
-				}
-			]
-		};
+const createQueryFilters = (query) => {
+	const caseReference = query.caseRef;
+	const pageNo = parseInt(query?.page) || 1;
+	const defaultSize = 25;
+	const maxSize = 100;
+	const size = Math.min(parseInt(query?.size) || defaultSize, maxSize);
+	const offset = (pageNo - 1) * size;
+	const searchTerm = query?.searchTerm;
 
-		const dbQuery = {
-			where,
-			order: [['dateAdviceGiven', 'DESC'], ['adviceID']],
-			offset: (requestQuery.page - 1) * itemsPerPage,
-			limit: itemsPerPage
-		};
-
-		const { count, rows } = await db.Advice.findAndCountAll(dbQuery);
-
-		return {
-			count,
-			rows: rows.map((row) =>
-				row.get({
-					plain: true
-				})
-			)
-		};
-	},
-
-	async getAdviceById(adviceID) {
-		const advice = await db.Advice.findOne({
-			where: {
-				adviceID
-			}
-		});
-
-		if (!advice) return undefined;
-
-		const attachments = await db.Attachment.findAllAttachmentsWithCase(advice.caseReference, {
-			where: {
-				adviceID
-			}
-		});
-
-		const adviceDTO = advice.get({
-			plain: true
-		});
-
-		adviceDTO.attachments = attachments.map((attachment) => {
-			// eslint-disable-next-line no-unused-vars
-			const { adviceID, ...dto } = attachment.get({
-				plain: true
-			});
-			return dto;
-		});
-
-		return adviceDTO;
-	}
+	return { pageNo, size, offset, searchTerm, caseReference };
 };
 
-function mapSearchTermToQuery(searchTerm) {
-	if (searchTerm) {
-		const searchStatements = [
-			'firstName',
-			'lastName',
-			'organisation',
-			'enquiryDetail',
-			'adviceGiven'
-		].map((field) => ({
-			[field]: { [Op.like]: `%${searchTerm}%` }
-		}));
-		return { [Op.or]: searchStatements };
-	}
-}
+const gg = ({ count, advice }) => {
+	return {
+		count,
+		advice: mapBackOfficeAdviceToApi(advice)
+	};
+};
+const getAllAdvice = async (query) => {
+	const { caseReference, pageNo, size, offset, searchTerm } = createQueryFilters(query);
+
+	const { advice, count } = isBackOfficeCaseReference(caseReference)
+		? gg(await getAllBackOfficeAdvice(caseReference, offset, size, searchTerm))
+		: await getAllNIAdvice(caseReference, offset, size, searchTerm);
+
+	return {
+		advice: advice,
+		totalItems: count,
+		itemsPerPage: size,
+		totalPages: Math.ceil(Math.max(1, count) / size),
+		currentPage: pageNo
+	};
+};
+
+const getAdviceById = async (adviceID) => {
+	const advice = await db.Advice.findOne({
+		where: {
+			adviceID
+		}
+	});
+
+	if (!advice) return undefined;
+
+	const attachments = await db.Attachment.findAllAttachmentsWithCase(advice.caseReference, {
+		where: {
+			adviceID
+		}
+	});
+
+	const adviceDTO = advice.get({
+		plain: true
+	});
+
+	adviceDTO.attachments = attachments.map((attachment) => {
+		// eslint-disable-next-line no-unused-vars
+		const { adviceID, ...dto } = attachment.get({
+			plain: true
+		});
+		return dto;
+	});
+
+	return adviceDTO;
+};
+
+module.exports = {
+	getAllAdvice,
+	getAdviceById
+};
