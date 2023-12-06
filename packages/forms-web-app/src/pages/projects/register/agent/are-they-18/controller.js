94e66e17e8e4a3d9ce593d7998fa007bd5a2125b
christopher-semmens <christopher.semmens@kinandcarta.com>
2023-12-06 10:33:27 +0000

    feat: refactor are you/they over 18 pages

--- packages/forms-web-app/src/pages/projects/register/agent/are-they-18/controller.js
+++ packages/forms-web-app/src/pages/projects/register/agent/are-they-18/controller.js
@@ -1,17 +1,19 @@
-const { VIEW } = require('../../../lib/views');
+const { VIEW } = require('../../../../../lib/views');
 
-exports.getOver18 = (req, res) => {
-	res.render(VIEW.REGISTER.AGENT.REPRESENTEE_OVER_18, {
+const view = 'projects/register/agent/are-they-18/view.njk';
+
+const getRegisterAreThey18Controller = (req, res) => {
+	res.render(view, {
 		over18: req.session.behalfRegdata.representee['over-18']
 	});
 };
 
-exports.postOver18 = (req, res) => {
+const postRegisterAreThey18Controller = (req, res) => {
 	const { body } = req;
 	const over18 = body['over-18'];
 	const { errors = {}, errorSummary = [] } = body;
 	if (errors['over-18'] || Object.keys(errors).length > 0) {
-		res.render(VIEW.REGISTER.AGENT.REPRESENTEE_OVER_18, {
+		res.render(view, {
 			errors,
 			errorSummary
 		});
@@ -25,4 +27,9 @@ exports.postOver18 = (req, res) => {
 			? VIEW.REGISTER.AGENT.CHECK_YOUR_ANSWERS
 			: VIEW.REGISTER.AGENT.REPRESENTEE_ADDRESS;
 	return res.redirect(`${res.locals.baseUrl}/${redirectUrl}`);
+};
+
+module.exports = {
+	getRegisterAreThey18Controller,
+	postRegisterAreThey18Controller
 };
--- packages/forms-web-app/src/pages/projects/register/myself/router.js
+++ packages/forms-web-app/src/pages/projects/register/myself/router.js
@@ -4,14 +4,27 @@ const {
 	getRegisterAddressController,
 	postRegisterAddressController
 } = require('../_common/address/controller');
+const {
+	getRegisterAreYou18Controller,
+	postRegisterAreYou18Controller
+} = require('../_common/are-you-18/controller');
 
 const { getRegisterMyselfAddressURL } = require('./address/_utils/get-register-myself-address-url');
+const {
+	getRegisterMyselfAreYou18URL
+} = require('./are-you-18/_utils/get-register-myself-are-you-18-url');
 
 const { registerMiddleware } = require('../../../../routes/register/middleware');
+
 const { rules: addressValidationRules } = require('../../../../validators/register/myself/address');
+const {
+	rules: areYou18ValidationRules
+} = require('../../../../validators/register/myself/are-you-18-over');
+
 const { validationErrorHandler } = require('../../../../validators/validation-error-handler');
 
 const registerMyselfAddressURL = getRegisterMyselfAddressURL();
+const registerMyselfAreYou18URL = getRegisterMyselfAreYou18URL();
 
 const registerMyselfRouter = express.Router({ mergeParams: true });
 
@@ -26,6 +39,19 @@ registerMyselfRouter.post(
 	addressValidationRules(),
 	validationErrorHandler,
 	postRegisterAddressController
+);
+
+registerMyselfRouter.get(
+	registerMyselfAreYou18URL,
+	registerMiddleware,
+	getRegisterAreYou18Controller
+);
+registerMyselfRouter.post(
+	registerMyselfAreYou18URL,
+	registerMiddleware,
+	areYou18ValidationRules(),
+	validationErrorHandler,
+	postRegisterAreYou18Controller
 );
 
 module.exports = { registerMyselfRouter };
