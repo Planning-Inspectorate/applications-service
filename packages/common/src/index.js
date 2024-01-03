c7d565a08387dd756c629799f7c35d8b7e288bff
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-01-03 16:08:17 +0000

    chore: remove Prometheus package and metric route

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,11 +1,9 @@
 const functional = require('./functional');
-const prometheus = require('./prometheus');
 const utils = require('./utils');
 const constants = require('./constants');
 
 module.exports = {
 	functional,
-	prometheus,
 	utils,
 	constants
 };
