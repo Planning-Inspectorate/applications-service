2be5a4be58ddef18fc4bfa65a1a8659997145c5d
Frosty081079 <neil.frost@planninginspectorate.gov.uk>
2024-01-17 10:27:08 +0000

    test: project info page1

---
+++ packages/e2e_tests/cypress/e2e/project-information.cy.js
@@ -0,0 +1,8 @@
+describe('use can view and interact with the project informaiton page', () => {
+	it('user can visit the project information page', () => {
+		cy.clearCookies();
+		cy.visit('/projects/BC0110002');
+
+		cy.get('.govuk-caption-l').contains('Office Use Testing Application 12');
+	});
+});
