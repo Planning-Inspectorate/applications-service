61d347132799c0c3a62b0f969d5cddfc80e7d9a1
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2024-01-24 16:37:27 +0000

    feat(web/applications): get document name from it's url

---
+++ packages/forms-web-app/__tests__/unit/lib/get-file-name-from-url.test.js
@@ -0,0 +1,39 @@
+const { getFileNameFromDocumentUrl } = require('../../../src/lib/get-file-name-from-url');
+
+describe('forms-web-app/src/lib/get-file-name-from-url.js', () => {
+	describe('#getFileNameFromDocumentUrl', () => {
+		it('should return null if url is not a string', () => {
+			expect(getFileNameFromDocumentUrl([])).toEqual(null);
+			expect(getFileNameFromDocumentUrl({})).toEqual(null);
+			expect(getFileNameFromDocumentUrl(123)).toEqual(null);
+			expect(getFileNameFromDocumentUrl(null)).toEqual(null);
+			expect(getFileNameFromDocumentUrl(undefined)).toEqual(null);
+		});
+		it('should return null if function fails to extract file name', () => {
+			expect(getFileNameFromDocumentUrl('')).toEqual(null);
+			expect(getFileNameFromDocumentUrl('https://www.google.com/no-file')).toEqual(null);
+			expect(
+				getFileNameFromDocumentUrl(
+					'https://nitestaz.planninginspectorate.gov.uk/wp-content/ipc/uploads/wrong_file_name_format.doc'
+				)
+			).toEqual(null);
+		});
+		it('should extract file name from a document url', () => {
+			expect(
+				getFileNameFromDocumentUrl(
+					'https://nitestaz.planninginspectorate.gov.uk/wp-content/ipc/uploads/2021/03/2021-03-15-IPC_Decision_Notice.pdf'
+				)
+			).toEqual('IPC Decision Notice');
+			expect(
+				getFileNameFromDocumentUrl(
+					'https://nitestaz.planninginspectorate.gov.uk/wp-content/ipc/uploads/projects/TR030001/TR030001-001428-Acceptance_of_Application.pdf'
+				)
+			).toEqual('Acceptance of Application');
+			expect(
+				getFileNameFromDocumentUrl(
+					'https://test.com/mock/url/TR030001--Acceptance_of_Application.pdf'
+				)
+			).toEqual('Acceptance of Application');
+		});
+	});
+});
