5ee3ff9efb4d1e700482b875749014420158060d
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-01-30 15:41:27 +0000

    feat: add and use timetable boolean from session from api call

--- packages/forms-web-app/src/pages/projects/_middleware/middleware.js
+++ packages/forms-web-app/src/pages/projects/_middleware/middleware.js
@@ -1,22 +1,17 @@
 const { getApplicationData } = require('../_utils/get-application-data');
 const { getVerticalTabs } = require('./_utils/get-vertical-tabs');
 const logger = require('../../../lib/logger');
-const { getHasOpenTimetables } = require('../../../utils/timetables/get-timetables-state');
-const {
-	areEventsEligibleForDisplay
-} = require('../examination-timetable/_utils/events/get-events');
+const { getHasTimetables } = require('../../../utils/timetables/get-timetables-state');
 const { projectInfoProjectStages } = require('../../../utils/project-stages');
 const config = require('../../../config');
 
 async function projectsMiddleware(req, res, next) {
 	try {
-		const { params, baseUrl, path } = req;
+		const { params, baseUrl, path, session } = req;
 		const { case_ref } = params;
+
 		const applicationData = await getApplicationData(case_ref);
-		const hasOpenTimetables = await getHasOpenTimetables(case_ref);
-		const eventsEligibleForDisplay = areEventsEligibleForDisplay(
-			applicationData.dateOfNonAcceptance
-		);
+		const hasTimetables = await getHasTimetables(session, case_ref);
 
 		res.locals.projectName = applicationData.projectName;
 		res.locals.caseRef = case_ref;
@@ -24,13 +19,7 @@ async function projectsMiddleware(req, res, next) {
 		res.locals.baseUrl = baseUrl;
 		res.locals.path = path;
 		res.locals.projectStages = projectInfoProjectStages;
-		res.locals.hasOpenTimetables = hasOpenTimetables;
-		res.locals.verticalTabs = getVerticalTabs(
-			case_ref,
-			hasOpenTimetables,
-			eventsEligibleForDisplay,
-			applicationData
-		);
+		res.locals.verticalTabs = getVerticalTabs(case_ref, applicationData, hasTimetables);
 		next();
 	} catch (e) {
 		logger.error(e);
