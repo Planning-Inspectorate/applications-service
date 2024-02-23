156f1267fba6999545b1e648828734bd4521c8b3
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-02-23 12:14:04 +0000

    feat: project information content audit

--- packages/forms-web-app/src/pages/process-guide/pre-examination/config.test.js
+++ packages/forms-web-app/src/pages/process-guide/pre-examination/config.test.js
@@ -12,7 +12,7 @@ describe('pages/process-guide/pre-examination/config', () => {
 		expect(preExaminationURL).toEqual('/decision-making-process-guide/pre-examination');
 		expect(preExaminationTitle).toEqual('Pre-examination');
 		expect(preExaminationContent).toEqual([
-			'The Examining Authority is appointed and is made up of one or more inspectors. Anyone who wants to have their say needs to register at this stage.',
+			'The Examining Authority is appointed and is made up of one or more inspectors. Anyone who wants to have their say must be able to register at this stage.',
 			'The applicant must publish that the application has been accepted by us. They include when and how parties can register to get involved. The time period for registering is set by the applicant but must be no less than 28 days.',
 			'The pre-examination stage usually takes about 3 months.'
 		]);
