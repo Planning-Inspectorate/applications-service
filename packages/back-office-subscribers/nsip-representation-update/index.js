511f13da0e99116c2a2010f4b136a06481b3500c
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-04-12 16:04:14 +0100

    feat: update rep-update func

--- packages/back-office-subscribers/nsip-representation-update/index.js
+++ packages/back-office-subscribers/nsip-representation-update/index.js
@@ -1,20 +1,31 @@
 const { prismaClient } = require('../lib/prisma');
+const buildMergeQuery = require('../lib/build-merge-query');
 
 module.exports = async (context, message) => {
 	context.log(`invoking nsip-representation-update function`);
 	const { representationId, status } = message;
 
 	if (!representationId) {
-		context.log(`skipping update as representationId is missing`);
-		return;
+		throw new Error('representationId is required');
 	} else if (!status) {
-		context.log(`skipping update as status is missing`);
-		return;
+		throw new Error('status is required');
 	}
 
-	await prismaClient.representation.update({
-		where: { representationId },
-		data: { status }
-	});
-	context.log(`updated representation ${representationId} to ${status}`);
+	console.log('context.bindingData.enqueuedTimeUtc', context.bindingData.enqueuedTimeUtc);
+
+	const representation = {
+		representationId,
+		status,
+		modifiedAt: new Date()
+	};
+
+	const { statement, parameters } = buildMergeQuery(
+		'representation',
+		'representationId',
+		representation,
+		context.bindingData.enqueuedTimeUtc
+	);
+
+	await prismaClient.$executeRawUnsafe(statement, ...parameters);
+	context.log(`upserted representation with representationId ${representationId}`);
 };
