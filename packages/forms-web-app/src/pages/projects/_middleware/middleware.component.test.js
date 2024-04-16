b76a955f788e3543aa0faaa3f0b30b88dd53f7c3
Camilla Priest-Stephens <camilla.priest-stephens@planninginspectorate.gov.uk>
2024-04-16 13:28:08 +0100

    test: update unit test for projectsMiddleware

--- packages/forms-web-app/src/pages/projects/_middleware/middleware.component.test.js
+++ packages/forms-web-app/src/pages/projects/_middleware/middleware.component.test.js
@@ -1,21 +1,18 @@
 const { projectsMiddleware } = require('./middleware');
 const { getApplicationData } = require('../_utils/get-application-data');
+const { getVerticalTabs } = require('./_utils/get-vertical-tabs');
 const { getTimetables } = require('../../../lib/application-api-wrapper');
 const { fixturesTimetableResponse } = require('../../../services/__mocks__/timetable.fixtures');
 
 jest.mock('../_utils/get-application-data', () => ({
 	getApplicationData: jest.fn()
 }));
+jest.mock('./_utils/get-vertical-tabs', () => ({
+	getVerticalTabs: jest.fn()
+}));
 jest.mock('../../../lib/application-api-wrapper', () => ({
 	getTimetables: jest.fn()
 }));
-jest.mock('../../../config', () => {
-	const originalConfig = jest.requireActual('../../../config');
-	return {
-		...originalConfig,
-		featureFlag: {}
-	};
-});
 
 describe('projects _middleware', () => {
 	describe('#projectsMiddleware', () => {
@@ -34,6 +31,68 @@ describe('projects _middleware', () => {
 				DateOfRepresentationPeriodOpen: '2023-01-03',
 				DateOfRelevantRepresentationClose: '2023-01-04'
 			});
+			getVerticalTabs.mockReturnValue([
+				{
+					hidden: true,
+					id: 'project-information',
+					name: 'Project information',
+					url: '/projects/mock-case-ref'
+				},
+				{
+					hidden: true,
+					id: 'project-timeline',
+					name: 'Project timeline',
+					url: '/projects/project-timeline'
+				},
+				{
+					hidden: true,
+					id: 'project-documents',
+					name: 'Documents',
+					url: '/projects/mock-case-ref/documents'
+				},
+				{
+					hidden: true,
+					id: 'register-index',
+					name: 'Register to have your say',
+					url: '/projects/mock-case-ref/register/register-have-your-say'
+				},
+				{
+					hidden: true,
+					id: 'representations',
+					name: 'Relevant representations (registration comments)',
+					url: '/projects/mock-case-ref/representations'
+				},
+				{
+					hidden: true,
+					id: 'project-examination-timetable',
+					name: 'Examination timetable',
+					url: '/projects/mock-case-ref/examination-timetable'
+				},
+				{
+					hidden: true,
+					id: 'project-have-your-say',
+					name: 'Have your say',
+					url: '/projects/mock-case-ref/examination/have-your-say-during-examination'
+				},
+				{
+					hidden: true,
+					id: 'get-updates',
+					name: 'Get updates',
+					url: '/projects/mock-case-ref/get-updates/start'
+				},
+				{
+					hidden: true,
+					id: 'all-examination-documents',
+					name: 'All Examination documents',
+					url: '/projects/all-examination-documents'
+				},
+				{
+					hidden: true,
+					id: 'section-51',
+					name: 'Section 51 advice',
+					url: '/projects/mock-case-ref/s51advice'
+				}
+			]);
 			getTimetables.mockReturnValue(fixturesTimetableResponse);
 			projectsMiddleware(req, res, next);
 		});
