813854616b2fbe69a97fd6a0b1470c0a11ebdee7
Will Adams <Will.Adams@kcom.com>
2024-05-30 14:30:48 +0100

    test: mirgration of cookie settings and footer links

---
+++ packages/e2e_tests/cypress/e2e/cookie-settings.cy.js
@@ -0,0 +1,37 @@
+import { PO_Cookies } from '../pageObject/Cookies/PO_Cookies';
+import { BasePage } from '../pageObject/basePage';
+
+const cookies = new PO_Cookies();
+const basepage = new BasePage();
+
+beforeEach(() => {
+	cy.clearAllCookies();
+	cy.visit('/');
+	basepage.clickAllProjectsLink();
+});
+
+describe('Accept and Decline cookie settings on view cookies page', () => {
+	it('Should navigate to the view cookies page and accept cookies', () => {
+		cookies.clickOnCookieChoice('view');
+		cy.url().should('include', '/cookies');
+		basepage.locateH1ByText('Cookies on application service');
+		cookies.selectRadioChoice('yes');
+		cookies.clickSaveChangesButton();
+		cookies.cookiesUpdatedText();
+		cookies.clickGoBackToThePageLink();
+		basepage.locateH1ByText('Projects');
+		cy.url().should('include', '/project-search');
+	});
+
+	it('Should navigate to the view cookies page and decline cookies', () => {
+		cookies.clickOnCookieChoice('view');
+		cy.url().should('include', '/cookies');
+		basepage.locateH1ByText('Cookies on application service');
+		cookies.selectRadioChoice('no');
+		cookies.clickSaveChangesButton();
+		cookies.cookiesUpdatedText();
+		cookies.clickGoBackToThePageLink();
+		basepage.locateH1ByText('Projects');
+		cy.url().should('include', '/project-search');
+	});
+});
--- packages/e2e_tests/cypress/pageObject/basePage.js
+++ packages/e2e_tests/cypress/pageObject/basePage.js
@@ -1,1 +1,50 @@
-export class BasePage {}
+export class BasePage {
+	elements = {
+		homeLink: () => cy.get('a[href*="/"]'),
+		allProjectsLink: () => cy.get('a[href*="/project-search"]'),
+		detailedInformationLink: () => cy.get('a[href*="/detailed-information"]'),
+		h1: () => cy.get('h1'),
+		termsAndConditionsLink: () => cy.get('[data-cy="Terms and conditions"]'),
+		accessibliltyLink: () => cy.get('[data-cy="Accessibility statement"]'),
+		privacyNoticeLink: () => cy.get('[data-cy="Privacy"]'),
+		cookiesLink: () => cy.get('[data-cy="Cookies"]')
+	};
+
+	clickHomeLink() {
+		this.elements.homeLink().click();
+	}
+
+	clickAllProjectsLink() {
+		this.elements.allProjectsLink().click();
+	}
+
+	clickDetailedInformationLink() {
+		this.elements.detailedInformationLink().click();
+	}
+
+	locateH1ByText(pageTitle) {
+		this.elements.h1().contains(pageTitle);
+	}
+
+	clickFooterLink(linkType) {
+		switch (linkType) {
+			case 'Privacy':
+				cy.get('[data-cy="Privacy"]').click();
+				break;
+			case 'Terms and conditions':
+				cy.get('[data-cy="Terms and conditions"]').click();
+				break;
+			case 'Accessibility':
+				cy.get('[data-cy="Accessibility statement"]').click();
+				break;
+			case 'Privacy Notice':
+				cy.get('[data-cy="Privacy"]').click();
+				break;
+			case 'Cookies':
+				cy.get('[data-cy="Cookies"]').click();
+				break;
+			case 'Contact':
+				cy.get('[data-cy="Contact"]').click();
+		}
+	}
+}
