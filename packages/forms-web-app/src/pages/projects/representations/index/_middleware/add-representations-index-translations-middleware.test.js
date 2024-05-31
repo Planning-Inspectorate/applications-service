458f070c37f4b812fa5851acbfb409195a977d4b
Camilla Priest-Stephens <106540248+cprieststephens@users.noreply.github.com>
2024-05-31 15:34:20 +0100

    feat: add translations for relevant representations page (#1173)

---
+++ packages/forms-web-app/src/pages/projects/representations/index/_middleware/add-representations-index-translations-middleware.test.js
@@ -0,0 +1,55 @@
+const {
+	addRepresentationsIndexTranslationsMiddleware
+} = require('./add-representations-index-translations-middleware');
+
+jest.mock('../../../../_utils/get-translations', () => {
+	return {
+		getTranslations: () => ({
+			en: 'mock get English translations',
+			cy: 'mock get Welsh translations'
+		})
+	};
+});
+
+describe('pages/projects/representations/index/_middleware/add-representations-index-translations-middleware.js', () => {
+	describe('#addRepresentationsIndexTranslationsMiddleware', () => {
+		const req = {
+			i18n: {
+				language: null,
+				addResourceBundle: jest.fn()
+			}
+		};
+		const res = {};
+		const next = jest.fn();
+
+		describe('When the language is set to English', () => {
+			beforeEach(() => {
+				req.i18n.language = 'en';
+				addRepresentationsIndexTranslationsMiddleware(req, res, next);
+			});
+
+			it('should add the English translations', () => {
+				expect(req.i18n.addResourceBundle).toHaveBeenCalledWith(
+					'en',
+					'representationsIndex',
+					'mock get English translations'
+				);
+			});
+		});
+
+		describe('When the language is set to Welsh', () => {
+			beforeEach(() => {
+				req.i18n.language = 'cy';
+				addRepresentationsIndexTranslationsMiddleware(req, res, next);
+			});
+
+			it('should add the Welsh translations', () => {
+				expect(req.i18n.addResourceBundle).toHaveBeenCalledWith(
+					'cy',
+					'representationsIndex',
+					'mock get Welsh translations'
+				);
+			});
+		});
+	});
+});
--- packages/forms-web-app/src/pages/projects/representations/index/config.test.js
+++ packages/forms-web-app/src/pages/projects/representations/index/config.test.js
@@ -1,9 +1,15 @@
-const { representationsIndexRoute } = require('./config');
+const { representationsIndexRoute, representationsIndexI18nNamespace } = require('./config');
 
 describe('pages/projects/representations/index/config', () => {
 	describe('#representationsIndexRoute', () => {
 		it('should return the representations index route', () => {
 			expect(representationsIndexRoute).toEqual('');
+		});
+	});
+
+	describe('#representationsIndexI18nNamespace', () => {
+		it('should return the representations index namespace', () => {
+			expect(representationsIndexI18nNamespace).toEqual('representationsIndex');
 		});
 	});
 });
