6e08637ff2f38a12d5e65e0cdd024937fe2f7973
Priest-Stephens <camilla.priest-stephens@planninginspectorate.gov.uk>
2024-05-31 09:21:09 +0100

    feat: add additional translations for relevant reps page

--- packages/forms-web-app/src/pages/projects/representations/index/_translations/en.test.js
+++ packages/forms-web-app/src/pages/projects/representations/index/_translations/en.test.js
@@ -12,7 +12,14 @@ describe('pages/projects/representations/index/_translations/en.json', () => {
 			phrase3: 'of',
 			phrase4: 'representations, newest first.',
 			paragraph5:
-				'The relevant representations for this project have been archived and are no longer available on this site.'
+				'The relevant representations for this project have been archived and are no longer available on this site.',
+			linkText1: 'Clear all filters',
+			paragraph6:
+				'There are no registration comments to display. Registration comments will be published after the registration period has closed.',
+			paragraph7: 'No results were found matching your search term or filters.',
+			paragraph8:
+				'Would you like to clear your search and filters to view all relevant representations instead?',
+			linkText2: 'Clear search and filters'
 		});
 	});
 });
