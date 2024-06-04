3b5cc51b5edb7e63035510fb2d03355abc7fe1ae
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-06-04 12:32:10 +0100

    feat: add welsh fields to documents DB

---
+++ packages/applications-service-api/prisma/migrations/20240604112931_add_welsh_documents_fields/migration.sql
@@ -0,0 +1,21 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Document] ADD [authorWelsh] NVARCHAR(1000),
+[descriptionWelsh] NVARCHAR(1000),
+[filter1Welsh] NVARCHAR(1000);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
