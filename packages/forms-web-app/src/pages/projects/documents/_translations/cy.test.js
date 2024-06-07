d074aaa83b1b42d2af7af0f0441936340892ddd8
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-06-07 16:02:35 +0100

    test: update translations tests

--- packages/forms-web-app/src/pages/projects/documents/_translations/cy.test.js
+++ packages/forms-web-app/src/pages/projects/documents/_translations/cy.test.js
@@ -7,23 +7,26 @@ describe('pages/projects/documents/_translations/cy', () => {
 				documents: 'dogfennau'
 			},
 			heading1: 'Dogfennau',
+			paragraph1: "{{-link}} sy'n cynnwys rhifau cyfeirnod dogfennau",
+			paragraph1LinkText: 'Edrychwch ar lyfrgell yr archwiliad',
 			heading2: 'Chwilio dogfennau',
-			noResults: {
-				linkText1: 'Dychwelyd at Drosolwg y Prosiect',
-				paragraph1: `Nid oes dogfennau cais prosiect ar gael i'w harddangos ar hyn o bryd.`
-			},
+			paragraph2: "Chwilio yn ôl awdur, disgrifiad neu'r math o ddogfen.",
+			paragraph3:
+				"Yn dangos {{-from}} i {{-to}} o {{-total}} o ddogfennau, gyda'r rhai mwyaf newydd yn gyntaf.",
 			noResultsFound: {
-				linkText1: 'Clirio chwiliad a dewisiadau hidlo',
 				paragraph1:
 					"Ni ddaethpwyd o hyd i unrhyw ganlyniadau sy'n cyfateb i'ch term chwilio na'ch dewisiadau hidlo.",
 				paragraph2:
-					"A hoffech chi glirio'ch chwiliad a'ch dewisiadau hidlo i weld yr holl ddogfennau sydd ar gael yn lle hynny?"
+					"A hoffech chi glirio'ch chwiliad a'ch dewisiadau hidlo i weld yr holl ddogfennau sydd ar gael yn lle hynny?",
+				linkText1: 'Clirio chwiliad a dewisiadau hidlo'
 			},
-			paragraph1: "{{-link}} sy'n cynnwys rhifau cyfeirnod dogfennau",
-			paragraph1LinkText: 'Edrychwch ar lyfrgell yr archwiliad',
-			paragraph2: "Chwilio yn ôl awdur, disgrifiad neu'r math o ddogfen.",
-			paragraph3:
-				"Yn dangos {{-from}} i {{-to}} o {{-total}} o ddogfennau, gyda'r rhai mwyaf newydd yn gyntaf."
+			noResults: {
+				paragraph1: "Nid oes dogfennau cais prosiect ar gael i'w harddangos ar hyn o bryd.",
+				linkText1: 'Dychwelyd at Drosolwg y Prosiect'
+			},
+			filteredResults: {
+				heading1: "Canlyniadau wedi'u hidlo"
+			}
 		});
 	});
 });
