63e349395099e9070e982cb96619c0d4a378e528
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-06-07 16:02:35 +0100

    test: update translations tests

--- packages/forms-web-app/src/pages/projects/documents/_translations/en.test.js
+++ packages/forms-web-app/src/pages/projects/documents/_translations/en.test.js
@@ -7,21 +7,25 @@ describe('pages/projects/documents/_translations/en', () => {
 				documents: 'documents'
 			},
 			heading1: 'Documents',
-			heading2: 'Search documents',
-			noResults: {
-				linkText1: 'Return to the project overview',
-				paragraph1: 'There are no project application documents available to display at the moment.'
-			},
-			noResultsFound: {
-				linkText1: 'Clear search and filters',
-				paragraph1: 'No results were found matching your search term or filters.',
-				paragraph2:
-					'Would you like to clear your search and filters to view all available documents instead?'
-			},
 			paragraph1: '{{-link}} containing document reference numbers',
 			paragraph1LinkText: 'View examination library',
+			heading2: 'Search documents',
 			paragraph2: 'Search by author, description or document type.',
-			paragraph3: 'Showing {{-from}} to {{-to}} of {{-total}} documents, newest first.'
+			paragraph3: 'Showing {{-from}} to {{-to}} of {{-total}} documents, newest first.',
+			noResultsFound: {
+				paragraph1: 'No results were found matching your search term or filters.',
+				paragraph2:
+					'Would you like to clear your search and filters to view all available documents instead?',
+				linkText1: 'Clear search and filters'
+			},
+			noResults: {
+				paragraph1:
+					'There are no project application documents available to display at the moment.',
+				linkText1: 'Return to the project overview'
+			},
+			filteredResults: {
+				heading1: 'Filtered results'
+			}
 		});
 	});
 });
