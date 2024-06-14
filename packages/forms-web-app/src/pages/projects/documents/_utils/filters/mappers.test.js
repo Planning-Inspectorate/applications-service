c7589a8298bbbbec67f00a9fbe0e212007d51d9a
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-06-14 09:58:49 +0100

    test: update test for mapFilterTypeToCheckbox to also pass i18n argument

--- packages/forms-web-app/src/pages/projects/documents/_utils/filters/mappers.test.js
+++ packages/forms-web-app/src/pages/projects/documents/_utils/filters/mappers.test.js
@@ -1,11 +1,13 @@
 const { mapFilterTypeToCheckBox } = require('./mappers');
+const { mockI18n } = require('../../../../_mocks/i18n');
+
 describe('#mapFilterTypeToCheckBox', () => {
 	describe('When mapping a list of filters to a list of checkboxes', () => {
 		const items = [
 			{ value: 'mock value', count: '1' },
 			{ value: 'mock value second', count: '2' }
 		];
-		const result = mapFilterTypeToCheckBox(items);
+		const result = mapFilterTypeToCheckBox(mockI18n, items);
 		it('should return an array of checkboxes', () => {
 			expect(result).toEqual([
 				{
