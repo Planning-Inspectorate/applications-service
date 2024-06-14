51bb9ecc430e08f54068bf7671c07c0d01e51163
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-06-14 09:55:18 +0100

    feat: render Welsh when available for filters inside accordions

--- packages/forms-web-app/src/pages/projects/documents/_utils/filters/mappers.js
+++ packages/forms-web-app/src/pages/projects/documents/_utils/filters/mappers.js
@@ -1,7 +1,18 @@
 const { checkBoxMapper } = require('./checkBoxMapper');
+const { isLangWelsh } = require('../../../../_utils/is-lang-welsh');
 
-const mapFilterTypeToCheckBox = (types) =>
-	types.map((type) => checkBoxMapper(`${type.value} (${type.count})`, type.value, false));
+const mapFilterTypeToCheckBox = (i18n, types) =>
+	types.map((type) => {
+		const value = (() => {
+			if (isLangWelsh(i18n.language) && type.value_cy) {
+				return type.value_cy;
+			}
+
+			return type.value;
+		})();
+
+		return checkBoxMapper(`${value} (${type.count})`, type.value, false);
+	});
 
 module.exports = {
 	mapFilterTypeToCheckBox
