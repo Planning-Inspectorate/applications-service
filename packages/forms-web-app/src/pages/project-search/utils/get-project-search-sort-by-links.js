d1f2283a2b965892ca8e90d5316db1a94f45bfde
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-06-24 12:35:52 +0100

    feat: add project search translations

--- packages/forms-web-app/src/pages/project-search/utils/get-project-search-sort-by-links.js
+++ packages/forms-web-app/src/pages/project-search/utils/get-project-search-sort-by-links.js
@@ -1,17 +1,17 @@
 const { getSortByLinks } = require('../../_utils/get-sort-by-links');
 
-const getProjectSearchSortByLinks = (query) =>
+const getProjectSearchSortByLinks = (i18n, query) =>
 	getSortByLinks(query, [
 		{
-			name: 'Project name',
+			name: i18n.t('projectSearch.sortByLinks.projectName'),
 			value: 'ProjectName'
 		},
 		{
-			name: 'Applicant',
+			name: i18n.t('projectSearch.sortByLinks.promoterName'),
 			value: 'PromoterName'
 		},
 		{
-			name: 'Stage',
+			name: i18n.t('projectSearch.sortByLinks.stage'),
 			value: 'Stage'
 		}
 	]);
