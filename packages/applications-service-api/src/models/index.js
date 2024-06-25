10a6c7045d1cdf3df7069a92349d157eee923e44
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-06-25 12:53:40 +0100

    fix: bypass ni sequelize connection on training env

--- packages/applications-service-api/src/models/index.js
+++ packages/applications-service-api/src/models/index.js
@@ -1,27 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-
+const {
+	backOfficeIntegration: { getAllApplications }
+} = require('../lib/config');
 const basename = path.basename(__filename);
 const config = require(`../database/config/config`);
 const db = {};
+const SequelizeMock = require('sequelize-mock');
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize;
 
-fs.readdirSync(__dirname)
-	.filter((file) => {
-		return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-	})
-	.forEach((file) => {
-		const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-		db[model.name] = model;
+// Training env will only use BO and cannot connect to NI, so we use a mock DB to avoid connection errors
+const shouldMockDB = getAllApplications === 'BO';
+if (shouldMockDB) {
+	console.log('Training environment - using mock DB for NI');
+	sequelize = new SequelizeMock();
+} else {
+	sequelize = new Sequelize(config.database, config.username, config.password, config);
+	fs.readdirSync(__dirname)
+		.filter((file) => {
+			return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+		})
+		.forEach((file) => {
+			const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+			db[model.name] = model;
+		});
+
+	Object.keys(db).forEach((modelName) => {
+		if (db[modelName].associate) {
+			db[modelName].associate(db);
+		}
 	});
-
-Object.keys(db).forEach((modelName) => {
-	if (db[modelName].associate) {
-		db[modelName].associate(db);
-	}
-});
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
