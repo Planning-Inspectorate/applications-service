2668b1f90685cb75938a8854ec9135a695905261
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-06-28 11:20:48 +0100

    feat: create a custom toHaveSameKeys Jest matcher

--- packages/forms-web-app/__tests__/setupTests.js
+++ packages/forms-web-app/__tests__/setupTests.js
@@ -3,3 +3,16 @@ require('@testing-library/jest-dom');
 const fetchMock = require('jest-fetch-mock');
 
 fetchMock.enableMocks();
+
+function toHaveSameKeys(a, b) {
+	const aKeys = Object.keys(a).sort();
+	const bKeys = Object.keys(b).sort();
+
+	return {
+		pass: this.equals(aKeys, bKeys),
+		message: () =>
+			`Expected: ${this.utils.printExpected(aKeys)}\nReceived: ${this.utils.printReceived(bKeys)}`
+	};
+}
+
+expect.extend({ toHaveSameKeys });
