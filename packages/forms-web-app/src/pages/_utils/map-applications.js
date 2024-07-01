551835e9a887d4675f190554828a21a88a6406a6
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-07-01 10:54:26 +0100

    feat: all projects dynamic welsh

--- packages/forms-web-app/src/pages/_utils/map-applications.js
+++ packages/forms-web-app/src/pages/_utils/map-applications.js
@@ -1,18 +1,28 @@
 const dayjs = require('dayjs');
 const { projectStagesTranslations } = require('../../utils/project-stages');
 const { getProjectsIndexURL } = require('../projects/index/_utils/get-projects-index-url');
+const { isLangWelsh } = require('./is-lang-welsh');
 
 const formatDate = (date, format = 'DD MMM YYYY') => (date ? dayjs(date).format(format) : '');
 
 const mapApplications = ({ language }, applications) =>
-	applications.map((application) => ({
-		applicant: application.PromoterName,
-		applicationDate: formatDate(application.DateOfDCOSubmission),
-		decisionDate: formatDate(application.ConfirmedDateOfDecision),
-		location: application.ProjectLocation,
-		pageURL: getProjectsIndexURL(application.CaseReference),
-		projectName: application.ProjectName,
-		stage: projectStagesTranslations[application.Stage][language]
-	}));
+	applications.map((application) => {
+		const langIsWelsh = isLangWelsh(language);
+		return {
+			applicant: application.PromoterName,
+			applicationDate: formatDate(application.DateOfDCOSubmission),
+			decisionDate: formatDate(application.ConfirmedDateOfDecision),
+			location:
+				langIsWelsh && application.ProjectLocationWelsh
+					? application.ProjectLocationWelsh
+					: application.ProjectLocation,
+			pageURL: getProjectsIndexURL(application.CaseReference),
+			projectName:
+				langIsWelsh && application.ProjectNameWelsh
+					? application.ProjectNameWelsh
+					: application.ProjectName,
+			stage: projectStagesTranslations[application.Stage][language]
+		};
+	});
 
 module.exports = { mapApplications };
--- packages/forms-web-app/src/pages/_utils/map-applications.unit.test.js
+++ packages/forms-web-app/src/pages/_utils/map-applications.unit.test.js
@@ -8,38 +8,75 @@ const i18n = mockI18n();
 
 describe('_utils/map-applications', () => {
 	describe('#mapApplications', () => {
-		it('should take applications API response and returned correctly mapped data', () => {
-			const result = mapApplications(i18n, getApplicationsFixture.data.applications);
+		describe('should take applications API response and returned correctly mapped data', () => {
+			it('when the language is English', () => {
+				const result = mapApplications(i18n, getApplicationsFixture.data.applications);
+				expect(result).toEqual([
+					{
+						applicant: 'EDF',
+						applicationDate: '01 Jan 2018',
+						decisionDate: '',
+						location: 'Somerset - Monday PM 23/12',
+						pageURL: '/projects/TR010001',
+						projectName: 'Accessibility Test',
+						stage: 'Examination'
+					},
+					{
+						applicant: 'John Agent Burke',
+						applicationDate: '',
+						decisionDate: '',
+						location: 'Bristol',
+						pageURL: '/projects/TR023024',
+						projectName: 'April 7 2020',
+						stage: 'Pre-application'
+					},
+					{
+						applicant: 'EDF',
+						applicationDate: '01 Jan 2018',
+						decisionDate: '',
+						location: 'Somerset - cache test 03-02 15:44',
+						pageURL: '/projects/tr033005',
+						projectName: 'Azure Performance Test',
+						stage: 'Acceptance (review of the application)'
+					}
+				]);
+			});
+			it('when the language is Welsh', () => {
+				const result = mapApplications(
+					{ language: 'cy' },
+					getApplicationsFixture.data.applications
+				);
 
-			expect(result).toEqual([
-				{
-					applicant: 'EDF',
-					applicationDate: '01 Jan 2018',
-					decisionDate: '',
-					location: 'Somerset - Monday PM 23/12',
-					pageURL: '/projects/TR010001',
-					projectName: 'Accessibility Test',
-					stage: 'Examination'
-				},
-				{
-					applicant: 'John Agent Burke',
-					applicationDate: '',
-					decisionDate: '',
-					location: 'Bristol',
-					pageURL: '/projects/TR023024',
-					projectName: 'April 7 2020',
-					stage: 'Pre-application'
-				},
-				{
-					applicant: 'EDF',
-					applicationDate: '01 Jan 2018',
-					decisionDate: '',
-					location: 'Somerset - cache test 03-02 15:44',
-					pageURL: '/projects/tr033005',
-					projectName: 'Azure Performance Test',
-					stage: 'Acceptance (review of the application)'
-				}
-			]);
+				expect(result).toEqual([
+					{
+						applicant: 'EDF',
+						applicationDate: '01 Jan 2018',
+						decisionDate: '',
+						location: 'Somerset - Monday PM 23/12 Welsh',
+						pageURL: '/projects/TR010001',
+						projectName: 'Accessibility Test Welsh',
+						stage: 'Archwiliad'
+					},
+					{
+						applicant: 'John Agent Burke',
+						applicationDate: '',
+						decisionDate: '',
+						location: 'Bristol Welsh',
+						pageURL: '/projects/TR023024',
+						projectName: 'April 7 2020 Welsh',
+						stage: 'Cyn-ymgeisio'
+					},
+					{
+						applicant: 'EDF',
+						applicationDate: '01 Jan 2018',
+						decisionDate: '',
+						location: 'Somerset - cache test 03-02 15:44 Welsh',
+						pageURL: '/projects/tr033005',
+						projectName: 'Azure Performance Test Welsh',
+						stage: 'Acceptance (review of the application)'
+					}
+				]);
+			});
 		});
 	});
 });
