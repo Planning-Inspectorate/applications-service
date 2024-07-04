8674fd6f412ffa960f9bc63cf81f0abad0111b1e
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-07-04 10:02:23 +0100

    feat: add remaining Welsh translations to unsubscribe/d

--- packages/forms-web-app/src/pages/projects/get-updates/unsubscribed/controller.js
+++ packages/forms-web-app/src/pages/projects/get-updates/unsubscribed/controller.js
@@ -1,11 +1,12 @@
 const logger = require('../../../../lib/logger');
 const { getGetUpdatesUnsubscribedSession } = require('../_session');
+const { isLangWelsh } = require('../../../_utils/is-lang-welsh');
 
 const view = 'projects/get-updates/unsubscribed/view.njk';
 
 const getUpdatesUnsubscribedController = (req, res, next) => {
 	try {
-		const { session } = req;
+		const { session, i18n } = req;
 
 		if (!getGetUpdatesUnsubscribedSession(session)) {
 			throw new Error('Get updates is not unsubscribed');
@@ -13,7 +14,8 @@ const getUpdatesUnsubscribedController = (req, res, next) => {
 
 		return res.render(view, {
 			pageHeading: 'Unsubscribe success',
-			pageTitle: 'Successfully unsubscribed'
+			pageTitle: 'Successfully unsubscribed',
+			isWelsh: isLangWelsh(i18n.language)
 		});
 	} catch (error) {
 		logger.error(error);
