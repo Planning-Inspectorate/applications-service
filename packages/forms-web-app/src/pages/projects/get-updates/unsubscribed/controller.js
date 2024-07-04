01253ddadf5e2b755786c8bb94fc952cb23691c7
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-07-04 09:56:47 +0100

    refactor: remove getPageData function from subscribed controller
    Since no arguments are passed to this function -- it just returns a
    static value -- it only creates unnecessary indirection.

--- packages/forms-web-app/src/pages/projects/get-updates/unsubscribed/controller.js
+++ packages/forms-web-app/src/pages/projects/get-updates/unsubscribed/controller.js
@@ -1,16 +1,20 @@
 const logger = require('../../../../lib/logger');
 const { getGetUpdatesUnsubscribedSession } = require('../_session');
 
-const { getPageData } = require('./utils/get-page-data');
-
 const view = 'projects/get-updates/unsubscribed/view.njk';
 
 const getUpdatesUnsubscribedController = (req, res, next) => {
 	try {
 		const { session } = req;
 
-		if (getGetUpdatesUnsubscribedSession(session)) return res.render(view, getPageData());
-		else throw new Error('Get updates is not unsubscribed');
+		if (!getGetUpdatesUnsubscribedSession(session)) {
+			throw new Error('Get updates is not unsubscribed');
+		}
+
+		return res.render(view, {
+			pageHeading: 'Unsubscribe success',
+			pageTitle: 'Successfully unsubscribed'
+		});
 	} catch (error) {
 		logger.error(error);
 		next(error);
