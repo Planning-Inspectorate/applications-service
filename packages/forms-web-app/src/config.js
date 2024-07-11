6673afba94922c171483fcafdb52063f9c4d402c
Priest-Stephens <camilla.priest-stephens@planninginspectorate.gov.uk>
2024-07-11 13:18:46 +0100

    chore: remove project timeline logic from codebase

--- packages/forms-web-app/src/config.js
+++ packages/forms-web-app/src/config.js
@@ -49,7 +49,6 @@ module.exports = {
 		useRedisSessionStore: process.env.FEATURE_REDIS_SESSION_STORE === 'true',
 		allowSaveAndExitOption: process.env.FEATURE_SAVE_AND_EXIT_OPTION === 'true',
 		showAffectedAreaSection: process.env.FEATURE_SHOW_AFFECTED_AREA_SECTION === 'true',
-		hideProjectTimelineLink: process.env.FEATURE_PROJECT_TIMELINE_LINK === 'true',
 		contentSecurityPolicy: process.env.FEATURE_ENABLED_CONTENT_SECURITY_POLICY === 'true',
 		allowProjectInformation: process.env.FEATURE_PROJECT_INFORMATION === 'true',
 		generalisedFormSanitisation:
