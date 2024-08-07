ea8b0ffa4a0829b331b02f2d54eb7b85e63cd692
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-08-07 11:27:11 +0100

    docs: add note to explain middleware infinite redirect fix

--- packages/forms-web-app/src/pages/examination/_middleware/submission.middleware.js
+++ packages/forms-web-app/src/pages/examination/_middleware/submission.middleware.js
@@ -20,6 +20,11 @@ const isProcessingSubmission = (req, res, next) => {
 	if (allowableRoutes.includes(path.replace('/', ''))) return next();
 
 	if (getExaminationSubmissionComplete(session)) {
+		/*
+			Rebuild queryString to main lang value to avoid conflict with i18n middleware which causes redirect loop
+			* This happens due to i18n middleware detecting the absence of a lang value but the cookie value being set to cy so a redirect is
+			made to the requested URL with a query string which again hit the examination middleware causing a loop.
+		*/
 		const queryString = Object.keys(query).length ? buildQueryString(query) : '';
 		return res.redirect(`${submissionComplete.route}${queryString}`);
 	}
