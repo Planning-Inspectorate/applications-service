c91b91b9d8c7e2b403ad3af0ccbef1500f46f13c
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-08-08 12:51:42 +0100

    test: add date util tests to demonstrate timezone issues

--- packages/forms-web-app/__tests__/unit/utils/date-utils.test.js
+++ packages/forms-web-app/__tests__/unit/utils/date-utils.test.js
@@ -1,13 +1,50 @@
-const { isNullSQLDate } = require('../../../src/utils/date-utils');
+const { isNullSQLDate, setTimeToStartOfDay, formatDate } = require('../../../src/utils/date-utils');
 
 describe('date-utils Test Cases', () => {
-	it('return false when date passed to isNullSQLDate', () => {
-		expect(isNullSQLDate(new Date())).toBe(false);
+	describe('isNullSQLDate', () => {
+		it('return false when date passed to isNullSQLDate', () => {
+			expect(isNullSQLDate(new Date())).toBe(false);
+		});
+		it('return true when date is SQL null date', () => {
+			expect(isNullSQLDate(new Date('0001-01-01 00:00:00'))).toBe(true);
+		});
+		it('return false when invalid date passed to isNullSQLDate', () => {
+			expect(isNullSQLDate(NaN)).toBe(false);
+		});
 	});
-	it('return true when date is SQL null date', () => {
-		expect(isNullSQLDate(new Date('0001-01-01 00:00:00'))).toBe(true);
+
+	describe('formatDate', () => {
+		it('should format date in Europe/London', () => {
+			// BST
+			expect(formatDate('2024-08-08T13:00Z')).toBe('8 August 2024');
+			expect(formatDate('2024-08-07T23:00Z')).toBe('8 August 2024');
+			// GMT
+			expect(formatDate('2024-01-08T23:00Z')).toBe('8 January 2024');
+		});
 	});
-	it('return false when invalid date passed to isNullSQLDate', () => {
-		expect(isNullSQLDate(NaN)).toBe(false);
+
+	describe('setTimeToStartOfDay', () => {
+		// is this util meant to be setting the start of day in Europe/London or UTC?
+		// behaviour will be differently locally to on the server
+
+		it('should set start of date in Europe/London', () => {
+			const isoString = (value) => {
+				return new Date(value).toISOString();
+			};
+			// BST
+			expect(isoString(setTimeToStartOfDay('2024-08-08T13:00Z'))).toBe('2024-08-07T23:00:00.000Z');
+			// GMT
+			expect(isoString(setTimeToStartOfDay('2024-01-08T13:00Z'))).toBe('2024-01-08T00:00:00.000Z');
+		});
+
+		it('should set start of date in UTC', () => {
+			const isoString = (value) => {
+				return new Date(value).toISOString();
+			};
+			// BST
+			expect(isoString(setTimeToStartOfDay('2024-08-08T13:00Z'))).toBe('2024-08-08T00:00:00.000Z');
+			// GMT
+			expect(isoString(setTimeToStartOfDay('2024-01-08T13:00Z'))).toBe('2024-01-08T00:00:00.000Z');
+		});
 	});
 });
