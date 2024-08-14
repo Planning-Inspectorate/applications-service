63b9ed341837a161ce0474f64df9d200427ac2c9
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-08-14 12:13:44 +0100

    fix: bypass the case references feature flag when in training env

--- packages/applications-service-api/src/utils/is-backoffice-case-reference.js
+++ packages/applications-service-api/src/utils/is-backoffice-case-reference.js
@@ -1,6 +1,7 @@
 const config = require('../lib/config');
-const isBackOfficeCaseReference = (caseReference) => {
-	return config.backOfficeIntegration.caseReferences.includes(caseReference);
-};
+
+const isBackOfficeCaseReference = (caseReference) =>
+	config.backOfficeIntegration.getAllApplications === 'BO' ||
+	config.backOfficeIntegration.caseReferences.includes(caseReference);
 
 module.exports = { isBackOfficeCaseReference };
