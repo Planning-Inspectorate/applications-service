ec8840e5aab8de7c46a29a6d9c9bbaebbc8bdf4c
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-08-21 16:02:51 +0100

    feat: add redirect middleware to examination journey if no examination session

---
+++ packages/forms-web-app/src/pages/examination/_middleware/examination.middleware.test.js
@@ -0,0 +1,74 @@
+const { examinationMiddleware } = require('./examination.middleware');
+
+describe('pages/examination/_middleware/examination.middleware', () => {
+	describe('#examinationMiddleware', () => {
+		describe('When user is viewing the have your say page', () => {
+			const req = {
+				path: '/have-your-say-during-examination',
+				params: {
+					case_ref: 'ABC-123'
+				}
+			};
+			const res = {};
+			const next = jest.fn();
+
+			beforeEach(() => {
+				examinationMiddleware(req, res, next);
+			});
+
+			it('should go to the next middleware', () => {
+				expect(next).toHaveBeenCalled();
+			});
+		});
+
+		describe('When user does not have an examination session', () => {
+			const req = {
+				path: '/have-an-interested-party-number',
+				params: {
+					case_ref: 'ABC-123'
+				},
+				query: {},
+				session: {}
+			};
+			const res = {
+				redirect: jest.fn()
+			};
+			const next = jest.fn();
+
+			beforeEach(() => {
+				examinationMiddleware(req, res, next);
+			});
+
+			it('should go to the next middleware', () => {
+				expect(res.redirect).toHaveBeenCalledWith(
+					'/projects/ABC-123/examination/have-your-say-during-examination'
+				);
+			});
+		});
+
+		describe('When the user is viewing a examination journey page and does have an examination session', () => {
+			const req = {
+				path: '/have-an-interested-party-number',
+				params: {
+					case_ref: 'ABC-123'
+				},
+				query: {},
+				session: {
+					examination: {}
+				}
+			};
+			const res = {
+				redirect: jest.fn()
+			};
+			const next = jest.fn();
+
+			beforeEach(() => {
+				examinationMiddleware(req, res, next);
+			});
+
+			it('should go to the next middleware', () => {
+				expect(next).toHaveBeenCalled();
+			});
+		});
+	});
+});
