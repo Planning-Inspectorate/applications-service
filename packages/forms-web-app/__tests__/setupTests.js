98247a6f69ca441704e371c32d12ea30381b1306
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-09-04 15:22:33 +0100

    refactor: assign new object for comparison

--- packages/forms-web-app/__tests__/setupTests.js
+++ packages/forms-web-app/__tests__/setupTests.js
@@ -5,20 +5,21 @@ const fetchMock = require('jest-fetch-mock');
 fetchMock.enableMocks();
 
 const clearAllObjValues = (translations) =>
-	Object.keys(translations).forEach((key) => {
-		if (typeof translations[key] === 'object') {
-			clearAllObjValues(translations[key]);
-		} else translations[key] = '';
-	});
+	Object.keys(translations).reduce((acc, key) => {
+		if (typeof translations[key] === 'object') acc[key] = clearAllObjValues(translations[key]);
+		else acc[key] = '';
+
+		return acc;
+	}, {});
 
 function toHaveSameKeys(a, b) {
-	clearAllObjValues(a);
-	clearAllObjValues(b);
+	const aKeys = clearAllObjValues(a);
+	const bKeys = clearAllObjValues(b);
 
 	return {
-		pass: this.equals(a, b),
+		pass: this.equals(aKeys, bKeys),
 		message: () =>
-			`Expected: ${this.utils.printExpected(a)}\nReceived: ${this.utils.printReceived(b)}`
+			`Expected: ${this.utils.printExpected(aKeys)}\nReceived: ${this.utils.printReceived(bKeys)}`
 	};
 }
 
