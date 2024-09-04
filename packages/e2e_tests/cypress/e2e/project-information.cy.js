e2767f4e08adf7847c59610c08a40ce0a9a5e986
willadams332 <will.adams@kinandcarta.com>
2024-09-04 12:09:23 +0100

    test(refactor and adding project information test): refactor and adding project information test

--- packages/e2e_tests/cypress/e2e/project-information.cy.js
+++ packages/e2e_tests/cypress/e2e/project-information.cy.js
@@ -1,8 +1,63 @@
-describe('use can view and interact with the project informaiton page', () => {
-	it('user can visit the project information page', () => {
-		cy.clearCookies();
-		cy.visit('/projects/BC0110002');
+import { BasePage } from '../pageObject/basePage';
 
-		cy.get('.govuk-caption-l').contains('Office Use Testing Application 2');
+const basePage = new BasePage();
+
+before(() => {
+	cy.clearCookies();
+	cy.visit('/projects/BC0910150');
+});
+
+describe('use can view and interact with the project informaiton page', () => {
+	it('should verify the user is on the project information page', () => {
+		cy.url().should('include', 'projects/BC0910150');
+		basePage.locateH1ByText('Project information');
+	});
+
+	it('should verify a user is returned to the project information section', () => {
+		basePage.clickProjectInformationMenuLink('have-your-say');
+		cy.url().should('include', 'have-your-say-during-examination');
+		basePage.clickProjectInformationMenuLink('projects');
+		basePage.locateH1ByText('Project information');
+	});
+
+	it('should verify about the project section exists', () => {
+		basePage.locateH2ByText('About the project');
+		basePage.visibleGovBody('Type of application');
+		basePage.visibleGovBody('Name of applicant');
+	});
+
+	it('should verify the project stage section exists', () => {
+		basePage.locateH2ByText('Project stage');
+		basePage.visibleGovBody('This project is at');
+		basePage.clickGovBtn('Register to have your say');
+		cy.url().should('include', 'register-have-your-say');
+		basePage.clickProjectInformationMenuLink('projects');
+		cy.url().should('include', 'projects/BC0910150');
+	});
+
+	it('should verify the project location section exists', () => {
+		basePage.locateH2ByText('Project location');
+		basePage.visibleGovMap();
+	});
+
+	it('should verify the get updates section exists', () => {
+		basePage.locateH2ByText('Get updates');
+		basePage.visibleGovBody('Enter your email address to receive:');
+		basePage.clickGovInset(
+			'Read the privacy notice to see how we handle your information',
+			'/customer-privacy-notice'
+		);
+		basePage.clickGovBtn('Get updates');
+		cy.url().should('include', 'get-updates/start');
+		basePage.clickProjectInformationMenuLink('projects');
+		cy.url().should('include', 'projects/BC0910150');
+	});
+
+	it('should verify the contact us section exists', () => {
+		basePage.locateH2ByText('Contact us');
+		basePage.locateH3ByText('Telephone');
+		basePage.visibleGovBody('Telephone: 0303 444 5000');
+		basePage.locateH3ByText('Email');
+		basePage.locateH3ByText('Alternative formats');
 	});
 });
--- packages/e2e_tests/cypress/pageObject/basePage.js
+++ packages/e2e_tests/cypress/pageObject/basePage.js
@@ -5,14 +5,23 @@ export class BasePage {
 		detailedInformationLink: () => cy.get('a[href*="/detailed-information"]'),
 		h1: () => cy.get('h1'),
 		h2: () => cy.get('h2'),
+		h3: () => cy.get('h3'),
 		termsAndConditionsLink: () => cy.get('[data-cy="Terms and conditions"]'),
 		accessibliltyLink: () => cy.get('[data-cy="Accessibility statement"]'),
 		privacyNoticeLink: () => cy.get('[data-cy="Privacy"]'),
 		cookiesLink: () => cy.get('[data-cy="Cookies"]'),
 		saveAndContinueBtn: () => cy.get('[data-cy="button-accept-and-regoster"]'),
+		contiuneBtn: () => cy.get('[data-cy="button-submit-and-continue"]'),
 		checkBox: () => cy.get('[type="checkbox"]'),
 		govLink: () => cy.get('.govuk-link'),
-		govInput: () => cy.get('.govuk-input')
+		govInput: () => cy.get('.govuk-input'),
+		govLink: () => cy.get('.govuk-link'),
+		projectInformationMenu: () => cy.get('nav[aria-label="Project navigation"]'),
+		govRadioBtn: () => cy.get('[type="radio"]'),
+		govBody: () => cy.get('.govuk-body'),
+		govBtn: () => cy.get('.govuk-button'),
+		govMap: () => cy.get('.pins-map'),
+		govInset: () => cy.get('.govuk-inset-text')
 	};
 
 	clickSaveAndContinueBtn() {
@@ -35,8 +44,8 @@ export class BasePage {
 		this.elements.h1().contains(pageTitle);
 	}
 
-	locateH2ByText(text) {
-		this.elements.h2().contains(text);
+	locateH2ByText(string) {
+		this.elements.h2().contains(string);
 	}
 
 	clickFooterLink(linkType) {
@@ -90,5 +99,68 @@ export class BasePage {
 
 	govInputType(string) {
 		this.elements.govInput().type(string);
+	}
+
+	clickContiuneBtn() {
+		this.elements.contiuneBtn().click();
+	}
+
+	checkGovRadioBtn(string) {
+		this.elements.govRadioBtn().check(string);
+	}
+
+	clickProjectInformationMenuLink(link) {
+		this.elements.projectInformationMenu().within(() => {
+			switch (link) {
+				case 'documents':
+					cy.get('a[href*="/documents"]').click();
+					break;
+				case 'representations':
+					cy.get('a[href*="/representations"]').click();
+					break;
+				case 'examination-timetable':
+					cy.get('a[href*="/examination-timetable"]').click();
+					break;
+				case 'have-your-say':
+					cy.get('a[href*="/have-your-say-during-examination"]').click();
+					break;
+				case 'get-updates':
+					cy.get('a[href*="/start"]').click();
+					break;
+				case 's51':
+					cy.get('a[href*="/s51advice"]').click();
+					break;
+				case 'projects':
+					cy.get('a[href*="/projects/"]').contains('Project information').click();
+					break;
+				default:
+					throw new Error(`No matching case found for ${urlEnding}`);
+			}
+		});
+	}
+
+	visibleGovBody(string) {
+		this.elements.govBody().contains(string).should('be.visible');
+	}
+
+	clickGovBtn(string) {
+		this.elements.govBtn().contains(string).click();
+	}
+
+	visibleGovMap() {
+		this.elements.govMap().should('be.visible');
+	}
+
+	clickGovInset(expectedText, expectedHref) {
+		this.elements
+			.govInset()
+			.contains(expectedText)
+			.find('a')
+			.should('have.attr', 'href')
+			.and('include', expectedHref);
+	}
+
+	locateH3ByText(string) {
+		this.elements.h3().contains(string);
 	}
 }
