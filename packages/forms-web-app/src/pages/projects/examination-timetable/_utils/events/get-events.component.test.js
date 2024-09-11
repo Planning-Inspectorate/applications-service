4b99edc92ac8173852eb2e1263d51003aed265a0
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-09-11 14:20:38 +0100

    feat: update tag colours

--- packages/forms-web-app/src/pages/projects/examination-timetable/_utils/events/get-events.component.test.js
+++ packages/forms-web-app/src/pages/projects/examination-timetable/_utils/events/get-events.component.test.js
@@ -63,7 +63,7 @@ describe('controllers/projects/examination-timetable/utils/events/get-events', (
 								state: {
 									isSubmissionOpen: false,
 									tag: {
-										classes: '',
+										classes: 'govuk-tag govuk-tag--blue',
 										text: 'Closed'
 									}
 								},
