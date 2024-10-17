74d24d1dbeb7006f5bf842cab34fd2ef89a00543
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2024-10-17 15:48:37 +0100

    chore(web): unregister old service worker before registering new one

--- packages/forms-web-app/src/scripts/service-worker/register-service-worker.js
+++ packages/forms-web-app/src/scripts/service-worker/register-service-worker.js
@@ -1,5 +1,8 @@
+const { unregisterDuplicateServiceWorker } = require('./unregister-duplicate-service-worker');
+
 (async function () {
 	try {
+		await unregisterDuplicateServiceWorker();
 		await navigator.serviceWorker.register('/public/scripts/service-worker.script.js');
 	} catch (error) {
 		console.error(`SW Registration failed with ${error}`);
