380ebe1d2192cb4daf98d7cb8b5b12a4ea82d00c
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2024-10-25 15:05:45 +0100

    fix(web): lowercase allowedParamsArr to account for camelcased query parameters

--- packages/forms-web-app/src/pages/_utils/filter-allowed-params.js
+++ packages/forms-web-app/src/pages/_utils/filter-allowed-params.js
@@ -1,8 +1,9 @@
 const filterAllowedParams = (paramsArr, allowedParamsArr) => {
+	const lowerCaseAllowedParamsArr = allowedParamsArr.map((param) => param.toLowerCase());
 	const getOnlyAllowedParam = (param) => {
 		const stageRegEx = /^stage-[1-8]$/i; //stage-1, stage-2...stage-8
 
-		return stageRegEx.test(param) || allowedParamsArr.includes(param.toLowerCase());
+		return stageRegEx.test(param) || lowerCaseAllowedParamsArr.includes(param.toLowerCase());
 	};
 
 	return paramsArr.filter(getOnlyAllowedParam);
