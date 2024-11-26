384f38421f3757f87f5e418ef00dd21dedee4a4d
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-11-26 13:56:53 +0000

    feat: improve ni redirect setup process

--- packages/ni-redirects/src/index.js
+++ packages/ni-redirects/src/index.js
@@ -2,13 +2,19 @@ import { getDocumentRedirect, isDocumentLink } from './documents';
 import { getProjectRedirect, isProjectLink } from './projects';
 import testConfig from './config.test.json';
 import prodConfig from './config.prod.json';
+import testGeneratedConfig from './config.generated.test.json';
+import prodGeneratedConfig from './config.generated.prod.json';
 
 /**
  * @typedef {Object} EnvConfig
  * @property {string} blobStoreUrl
  * @property {string} frontOfficeUrl
- * @property {string[]} documentRedirectCaseReferences
  * @property {Object<string, string>} projectRedirects
+ */
+
+/**
+ * @typedef {Object} EnvGeneratedConfig
+ * @property {string[]} redirectAllowedCaseReferences - An array of allowed case references for redirection.
  */
 
 export default {
@@ -19,14 +25,24 @@ export default {
 	 * @returns {Promise<Response>}
 	 */
 	async fetch(request, env) {
+		const isProd = env.CONFIG === 'prod';
+
 		/** @type {EnvConfig} */
-		const config = env.CONFIG === 'prod' ? prodConfig : testConfig;
+		const config = isProd ? prodConfig : testConfig;
+		/** @type {EnvGeneratedConfig} */
+		const generatedConfig = isProd ? prodGeneratedConfig : testGeneratedConfig;
+		const redirectAllowedCaseReferences = generatedConfig.redirectAllowedCaseReferences;
+		// Don't log in prod for performance
+		if (!isProd) {
+			console.log(`Enabling redirection for the following cases: ${redirectAllowedCaseReferences}`);
+		}
+
 		const requestURL = new URL(request.url);
 		const path = requestURL.pathname;
 
 		if (isDocumentLink(path)) {
 			// redirect documents
-			const redirectTo = getDocumentRedirect(path, config);
+			const redirectTo = getDocumentRedirect(path, config, redirectAllowedCaseReferences);
 
 			if (redirectTo) {
 				return Response.redirect(redirectTo, 301);
@@ -34,7 +50,7 @@ export default {
 			// else fallthrough - no redirect
 		} else if (isProjectLink(path)) {
 			// redirect projects
-			const redirectTo = getProjectRedirect(path, config);
+			const redirectTo = getProjectRedirect(path, config, redirectAllowedCaseReferences);
 
 			if (redirectTo) {
 				return Response.redirect(redirectTo, 301);
