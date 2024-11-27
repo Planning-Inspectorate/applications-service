d4f8e89c53dfa00a8ecb19b57eb2c417e2ea269f
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-11-27 14:11:45 +0000

    feat: project-updates welsh translation

--- packages/forms-web-app/src/pages/projects/all-updates/controller.js
+++ packages/forms-web-app/src/pages/projects/all-updates/controller.js
@@ -6,10 +6,14 @@ const getProjectsAllUpdatesController = async (req, res, next) => {
 	try {
 		const { locals } = res;
 		const { caseRef } = locals;
+		const { i18n } = req;
 
 		const projectUpdates = await getProjectUpdatesData(caseRef);
 
-		return res.render('projects/all-updates/view.njk', getPageData(locals, projectUpdates));
+		return res.render(
+			'projects/all-updates/view.njk',
+			getPageData(locals, projectUpdates, i18n.language)
+		);
 	} catch (error) {
 		logger.error(error);
 		next(error);
