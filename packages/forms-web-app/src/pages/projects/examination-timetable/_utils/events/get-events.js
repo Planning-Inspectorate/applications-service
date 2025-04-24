e3bfbc04ee5bcb214f63abffa0c0bb6a8533f7bf
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2025-04-24 11:37:31 +0100

    fix(web): localise CBOS exam timetable dates

--- packages/forms-web-app/src/pages/projects/examination-timetable/_utils/events/get-events.js
+++ packages/forms-web-app/src/pages/projects/examination-timetable/_utils/events/get-events.js
@@ -6,18 +6,35 @@ const {
 	getOpenEventDeadlineTimetables
 } = require('../../../../../utils/timetables/get-timetables-state');
 const { getPastTimetables } = require('../../../../../utils/timetables/get-timetables-state');
+const { localiseDate } = require('../../../../../utils/date-utils');
 
 const getEvents = async (appData, i18n) => {
 	const { data } = await getTimetables(getProjectCaseRef(appData));
 
 	const timetables = data?.timetables || [];
 
+	//(APPLICS-1508)Due to timetable data coming from two sources (NI and CBOS; and NI dates being saved in local time, while CBOS dates are saved in UTC),
+	//we will need to localise the CBOS dates here, where we can still differentiate between the two sources (by sourceSystem), before it is being passed deeper
+	//TODO: When the migration is completed, we should remove this and probably only localise the dates in the views, and keep the date logic in UTC
+
+	//go over the timetables and localise the CBOS dates
+	const localisedTimetableData = timetables.map((timetable) => {
+		if (timetable.sourceSystem === 'BACK_OFFICE') {
+			return {
+				...timetable,
+				dateOfEvent: localiseDate(timetable.dateOfEvent),
+				dateTimeDeadlineStart: localiseDate(timetable.dateTimeDeadlineStart)
+			};
+		}
+		return timetable;
+	});
+
 	const sortedTimetables = {
-		past: getPastTimetables(timetables),
-		upcoming: getUpcomingTimetables(timetables)
+		past: getPastTimetables(localisedTimetableData),
+		upcoming: getUpcomingTimetables(localisedTimetableData)
 	};
 
-	const hasOpenTimetables = getOpenEventDeadlineTimetables(timetables).length > 0;
+	const hasOpenTimetables = getOpenEventDeadlineTimetables(localisedTimetableData).length > 0;
 
 	return { ...eventsViewModel(sortedTimetables, i18n), hasOpenTimetables };
 };
