75443505ecb3f53a7fa68cdcc7d842c88a78c85f
Camilla Priest-Stephens <camilla.priest-stephens@planninginspectorate.gov.uk>
2025-06-11 09:16:32 +0100

    fix(web): replace angle brackets with html entities in comments (applics-1502)

--- packages/forms-web-app/src/pages/projects/representations/index/_utils/get-representations-view-model.js
+++ packages/forms-web-app/src/pages/projects/representations/index/_utils/get-representations-view-model.js
@@ -3,7 +3,15 @@ const { titleCase } = require('../../../../../utils/string-case');
 const { getRepresentationURL } = require('../../representation/_utils/get-representation-url');
 const { formatDate } = require('../../../../../utils/date-utils');
 
-const getFormattedComment = (comment) => (comment ? marked.parse(comment).trim() : '');
+const getCommentWithHtmlEntities = (comment) => {
+	if (!comment) return '';
+	return comment.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+};
+
+const getFormattedComment = (comment) => {
+	const commentWithHtmlEntities = getCommentWithHtmlEntities(comment);
+	return commentWithHtmlEntities ? marked.parse(commentWithHtmlEntities).trim() : '';
+};
 
 const getRepresentationViewModel = (
 	{
