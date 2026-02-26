7275909db881af1134eb3910df19d012b758e556
ypk <Prasanna.Yalala.fa@planninginspectorate.gov.uk>
2026-02-26 11:48:58 +0000

    Revert "feat(IDAS-40): implement project maps with OS Maps integration and UK pan restrictions"
    This reverts commit fd4a24c59b796b228ed061d74be7722fe036a00d.

--- packages/forms-web-app/src/pages/project-search/utils/get-page-data.js
+++ packages/forms-web-app/src/pages/project-search/utils/get-page-data.js
@@ -6,7 +6,6 @@ const { getPagination } = require('../../projects/_utils/pagination/pagination')
 const { getFilters } = require('./filters/get-filters');
 const { getProjectSearchSortByLinks } = require('./get-project-search-sort-by-links');
 const { getRelatedContentLinks } = require('./get-related-content-links');
-const { getProjectsMapURL } = require('../../projects-map/utils/get-projects-map-url');
 
 const getPageData = (i18n, query, applications, filters, pagination) => ({
 	...getFilters(i18n, query, filters),
@@ -15,7 +14,6 @@ const getPageData = (i18n, query, applications, filters, pagination) => ({
 	totalApplicationsWithoutFilters: pagination.totalItemsWithoutFilters,
 	pagination: getPagination(pagination),
 	paginationQueryString: buildPaginationQueryString(query),
-	projectsMapURL: getProjectsMapURL(),
 	query,
 	resultsPerPage: documentsPerPage(query),
 	sortByLinks: getProjectSearchSortByLinks(i18n, query),
