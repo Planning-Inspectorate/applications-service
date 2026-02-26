aa68010d1b1dfaa04553f708e531036108b13b86
ypk <Prasanna.Yalala.fa@planninginspectorate.gov.uk>
2026-02-26 14:29:18 +0000

    feat(packages/forms-web-app): add projects-map page controller (idas-40)
    - added src/pages/projects-map/controller.js with server-side token fetch
    - added src/pages/projects-map/utils/get-projects-map-url.js
    - added controller.test.js (3 tests passing)

---
+++ packages/forms-web-app/src/pages/projects-map/controller.js
@@ -0,0 +1,35 @@
+const logger = require('../../lib/logger');
+const { getApplications } = require('../../services/applications.service');
+const { getMapAccessToken } = require('../_services');
+const { getFilters } = require('../project-search/utils/filters/get-filters');
+const {
+	getProjectSearchQueryString
+} = require('../project-search/utils/get-project-search-query-string');
+const { getProjectSearchURL } = require('../project-search/utils/get-project-search-url');
+const { getRelatedContentLinks } = require('../project-search/utils/get-related-content-links');
+
+const view = 'projects-map/view.njk';
+
+const getProjectsMapController = async (req, res, next) => {
+	try {
+		const { i18n, query } = req;
+
+		const [{ filters }, mapAccessToken] = await Promise.all([
+			getApplications(getProjectSearchQueryString(query)),
+			getMapAccessToken()
+		]);
+
+		res.render(view, {
+			...getFilters(i18n, query, filters),
+			mapAccessToken,
+			projectSearchURL: getProjectSearchURL(),
+			relatedContentLinks: getRelatedContentLinks(i18n),
+			query
+		});
+	} catch (error) {
+		logger.error(error);
+		next(error);
+	}
+};
+
+module.exports = { getProjectsMapController };
