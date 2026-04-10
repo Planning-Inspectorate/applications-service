7f59fc08376b00362b4be0ea615808c08c849477
james.barnes <james.barnes@solirius.com>
2026-04-10 12:41:48 +0100

    feat: redirect from form to existing email confirmation functionality

--- packages/forms-web-app/src/pages/projects/get-updates/router.js
+++ packages/forms-web-app/src/pages/projects/get-updates/router.js
@@ -6,7 +6,15 @@ import { projectsMiddleware } from '../_middleware/middleware.js';
 import { addGetUpdatesIndexTranslationsMiddleware } from './index/_middleware/add-get-updates-index-translations-middleware.js';
 
 import { getGetUpdatesIndexController } from './index/controller.js';
+import { getUpdatesConfirmYourEmailController } from './confirm-your-email/controller.js';
+import { getUpdatesSubscribedController } from './subscribed/controller.js';
+import {
+	getGetUpdatesUnsubscribeController,
+	postGetUpdatesUnsubscribeController
+} from './unsubscribe/controller.js';
+import { getUpdatesUnsubscribedController } from './unsubscribed/controller.js';
 
+import { getUpdatesMiddleware } from './_middleware/get-updates-middleware.js';
 /*
 // SO all these middlewares do the same thing functionally, the only difference is they load the translations in the _translations folder of their respective folder
 // TODO - set up the middleware later
@@ -14,7 +22,6 @@ import { addGetUpdatesEmailTranslationsMiddleware } from './email/_middleware/ad
 import { addGetUpdatesHowOftenTranslationsMiddleware } from './how-often/_middleware/add-get-updates-how-often-translations-middleware';
 import { addGetUpdatesConfirmYourEmailTranslationsMiddleware } from './confirm-your-email/_middleware/add-get-updates-confirm-your-email-translations-middleware';
 import { addGetUpdatesSubscribedTranslationsMiddleware } from './subscribed/_middleware/add-get-updates-subscribed-translations-middleware';
-import { getUpdatesMiddleware } from './_middleware/get-updates-middleware';
 import { getUpdatesUnsubscribedTranslationsMiddleware } from './unsubscribed/_middleware/unsubscribed-translations-middleware';
 import { getUpdatesUnsubscribeTranslationsMiddleware } from './unsubscribe/_middleware/unsubscribe-translations-middleware';
 
@@ -26,11 +33,19 @@ import { createRoutes as getUpdatesFormRoutes } from './form/router.js';
 
 import { getUpdatesIndexURL } from './index/utils/get-updates-index-url.js';
 import { getUpdatesFormURL } from './form/utils/get-updates-form-url.js';
+import { getUpdatesConfirmYourEmailURL } from './confirm-your-email/utils/get-updates-confirm-your-email-url.js';
+import { getUpdatesSubscribedURL } from './subscribed/utils/get-updates-subscribed-url.js';
+import { getUpdatesUnsubscribeURL } from './unsubscribe/utils/get-updates-unsubscribe-url.js';
+import { getUpdatesUnsubscribedURL } from './unsubscribed/utils/get-updates-unsubscribed-url.js';
 
 const getUpdatesRouter = express.Router();
 
 const updatesIndexURL = getUpdatesIndexURL();
 const updatesFormURL = getUpdatesFormURL();
+const updatesConfirmYourEmailURL = getUpdatesConfirmYourEmailURL();
+const updatesSubscribedURL = getUpdatesSubscribedURL();
+const updatesUnsubscribeURL = getUpdatesUnsubscribeURL();
+const updatesUnsubscribedURL = getUpdatesUnsubscribedURL();
 
 getUpdatesRouter.use(addCommonTranslationsMiddleware);
 
@@ -42,5 +57,19 @@ getUpdatesRouter.get(
 );
 
 getUpdatesRouter.use(updatesFormURL, getUpdatesFormRoutes());
+
+getUpdatesRouter.get(
+	updatesConfirmYourEmailURL,
+	projectsMiddleware,
+	getUpdatesMiddleware,
+	getUpdatesConfirmYourEmailController
+);
+
+getUpdatesRouter.get(updatesSubscribedURL, projectsMiddleware, getUpdatesSubscribedController);
+
+getUpdatesRouter.get(updatesUnsubscribeURL, projectsMiddleware, getGetUpdatesUnsubscribeController);
+getUpdatesRouter.post(updatesUnsubscribeURL, postGetUpdatesUnsubscribeController);
+
+getUpdatesRouter.get(updatesUnsubscribedURL, projectsMiddleware, getUpdatesUnsubscribedController);
 
 export { getUpdatesRouter };
