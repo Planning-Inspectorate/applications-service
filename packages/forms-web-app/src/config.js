e2fa290eff4bf1e83a66c750c50eb3d6959230b9
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-04-20 17:39:09 +0100

    chore(app): remove migration code from forms web app
    chore(app): update tests to reflect change to examinationTimetableData source

--- packages/forms-web-app/src/config.js
+++ packages/forms-web-app/src/config.js
@@ -66,7 +66,6 @@ module.exports = {
 		openRegistrationCaseReferences: splitStringToArray(
 			process.env.OPEN_REGISTRATION_CASE_REFERENCES
 		),
-		allowApplicationsPagination: process.env.BACK_OFFICE_INTEGRATION_GET_APPLICATIONS !== 'MERGE',
 		allowWelshTranslation: process.env.FEATURE_ALLOW_WELSH_TRANSLATION === 'true',
 		allowWelshCases: process.env.FEATURE_ALLOW_WELSH_CASES === 'true',
 		enableProjectsMap: process.env.FEATURE_ENABLE_PROJECTS_MAP === 'true',
