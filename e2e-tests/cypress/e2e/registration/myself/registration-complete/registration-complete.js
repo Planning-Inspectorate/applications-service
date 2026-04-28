ab728008c4da6a64a19c4d03b045e386f4256705
kostaspins <Kostas.Papanikolaou.b5@planninginspectorate.gov.uk>
2026-04-28 15:07:10 +0100

    fix(web): clarify registration complete comments publishing (IDAS-568)

--- e2e-tests/cypress/e2e/registration/myself/registration-complete/registration-complete.js
+++ e2e-tests/cypress/e2e/registration/myself/registration-complete/registration-complete.js
@@ -1,37 +1,33 @@
-import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then, And } from 'cypress-cucumber-preprocessor/steps';
 import PO_FullName from '../full-name/PageObjects/PO_FullName';
 import PO_AddressDetails from '../uk-address-details/PageObjects/PO_AddressDetails';
 import PO_EmailAddress from '../what-is-your-email-address/PageObjects/PO_EmailAddress';
 import PO_TeleNumber from '../what-is-your-telephone-number/PageObjects/PO_TeleNumber';
 import PO_TellAboutProject from '../what-do-you-want-to-tell-about-project/PageObjects/PO_TellAboutProject';
-import PO_RegComplete from './PageObjects/PO_RegComplete';
-import PO_WhoYouRegisterFor from '../../who-are-you-registering-for/PageObjects/PO_WhoYouRegisterFor';
-import {
-	registerAddAnotherCommentRadioStep,
-	registerTopicFieldStep
-} from '../../shared/registerCommentPageSteps';
 
 const fullNamePage = new PO_FullName();
 const addressDetails = new PO_AddressDetails();
 const emailAddressPage = new PO_EmailAddress();
 const teleNumberPage = new PO_TeleNumber();
 const tellAboutProject = new PO_TellAboutProject();
-const registrationComplete = new PO_RegComplete();
-const whoYouRegisterForPage = new PO_WhoYouRegisterFor();
 
 Given('I navigate to UK address details page', () => {
-	whoYouRegisterForPage.navigatetoTypeOfPartyPage();
-	whoYouRegisterForPage.selectPartyAndContinue('Myself');
+	cy.visit('/project-search');
+	cy.clickProjectLink('North Lincolnshire Green Energy Park');
+	cy.clickOnHref('/register-have-your-say');
+	cy.clickOnHref('who-registering-for');
+	cy.selectRadioOption('Myself');
+	cy.clickSaveAndContinue();
 	fullNamePage.enterTextIntoFullNameField('TestFirstName TestMiddleName TestLastName');
-	fullNamePage.clickSaveAndContinue();
-	emailAddressPage.selectRadioYesOrNo('Yes');
-	emailAddressPage.clickSaveAndContinue();
+	cy.clickSaveAndContinue();
+	cy.selectRadioYesOrNo('Yes');
+	cy.clickSaveAndContinue();
 	emailAddressPage.enterTextIntoEmailField('test@gmail.com');
-	emailAddressPage.clickSaveAndContinue();
+	cy.clickSaveAndContinue();
 });
 
 And('User clicks on continue button', () => {
-	addressDetails.clickSaveAndContinue();
+	cy.clickSaveAndContinue();
 });
 
 And('I enter below data into address details page', function (table) {
@@ -39,7 +35,7 @@ And('I enter below data into address details page', function (table) {
 });
 
 Then('I am on the {string} page', (pageName) => {
-	registrationComplete.assertOnPage(pageName);
+	cy.assertUserOnThePage(pageName);
 });
 
 And('I enter {string} into email address field', (dataInput) => {
@@ -55,27 +51,24 @@ And('I enter {string} into comments field', (dataInput) => {
 });
 
 And('User clicks on accept and continue button for {string}', (linkType) => {
-	registrationComplete.clickDeclarationLink(linkType);
+	switch (linkType) {
+		case 'myself':
+			cy.clickOnHref('/register/myself/declaration');
+			break;
+		case 'organisation':
+			cy.clickOnHref('/register/organisation/declaration');
+			break;
+	}
 });
 
 And('User clicks on accept and register button', () => {
-	registrationComplete.clickAcceptAndRegister();
+	cy.get('[data-cy="button-accept-and-register"]').click();
 });
 
-And(
-	'I click on find out more about having your say during the Examination of the application link',
-	() => {
-		registrationComplete.clickExaminationGuideLink();
-	}
-);
-
-registerTopicFieldStep(And, tellAboutProject);
-registerAddAnotherCommentRadioStep(When, tellAboutProject);
-
 Then('I click on feedback link', () => {
-	registrationComplete.clickOnProvideFeedbackLink();
+	cy.get('[data-cy="Feedback"]').first().click();
 });
 
 Then('I click on go back to project page link', () => {
-	registrationComplete.clickProjectLink();
+	cy.get('#project-link').click();
 });
