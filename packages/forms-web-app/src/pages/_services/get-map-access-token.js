38bc34ff094684d5dea0e4a82c7e0e9b686c9a15
ypk <Prasanna.Yalala.fa@planninginspectorate.gov.uk>
2026-06-02 07:05:25 +0100

    feat(web): display project boundaries from geo json (idas-131)
    - cleaned up unit tests

--- packages/forms-web-app/src/pages/_services/get-map-access-token.js
+++ packages/forms-web-app/src/pages/_services/get-map-access-token.js
@@ -4,7 +4,6 @@ const logger = require('../../lib/logger');
 
 const getMapAccessToken = async () => {
 	try {
-		logger.info('[map-token] Starting OS Maps token fetch');
 		const authString = Buffer.from(maps.osMapsApiKey + ':' + maps.osMapsApiSecret).toString(
 			'base64'
 		);
@@ -12,7 +11,6 @@ const getMapAccessToken = async () => {
 		const params = new URLSearchParams();
 		params.append('grant_type', 'client_credentials');
 
-		logger.info('[map-token] Calling OS Maps OAuth API');
 		const authResponse = await fetch('https://api.os.uk/oauth2/token/v1', {
 			method: 'POST',
 			body: params,
@@ -21,19 +19,13 @@ const getMapAccessToken = async () => {
 			}
 		});
 
-		logger.info(`[map-token] OS Maps OAuth API response status: ${authResponse.status}`);
-
 		const { access_token } = await authResponse.json();
 
-		if (!access_token) {
-			logger.error('[map-token] Map access token is undefined, API response may have failed');
-			throw new Error('Map access token is undefined');
-		}
+		if (!access_token) throw new Error('Map access token is undefined');
 
-		logger.info('[map-token] Successfully obtained OS Maps access token');
 		return access_token;
 	} catch (error) {
-		logger.error('[map-token] Failed to get map access token:', error.message, error.stack);
+		logger.error(error);
 	}
 };
 
