2f019cb9e2b90a02ed9aec13d71b8d28d74446f7
ypk <Prasanna.Yalala.fa@planninginspectorate.gov.uk>
2026-06-02 21:56:42 +0100

    feat(web): display project boundaries from geo json (idas-131)
    - changed templates to guard appscript loading for project-maps and projects page

--- packages/forms-web-app/src/pages/projects-map/config.js
+++ packages/forms-web-app/src/pages/projects-map/config.js
@@ -1,13 +1,8 @@
 const projectsMapRoute = 'projects-map';
-
 const projectsMapViewPath = 'projects-map/view.njk';
-
 const projectsMapI18nNamespace = 'projectsMap';
-
 const masterGeoJsonDownloadRoute = 'download-boundaries';
-
 const masterGeoJsonMapDisplayRoute = 'geojson';
-
 const geoJsonMimeType = 'application/geo+json';
 
 module.exports = {
